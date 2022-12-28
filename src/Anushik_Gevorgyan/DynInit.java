85544b4221900b0d453244668dc3ea0b535797fb
Anushik04 <anushikgevorgyan19@gmail.com>
2022-12-28 23:26:02 +0400

    I have done homework 1

---
+++ src/Anushik_Gevorgyan/DynInit.java
@@ -0,0 +1,12 @@
+package Anushik_Gevorgyan;
+
+public class DynInit {
+    public static void main(String[] args) {
+        double a = 3.0, b = 4.0;
+
+        double c = Math.sqrt(a * a + b * b);
+
+
+        System.out.println("Hypotenuse is " + c);
+    }
+}
---
+++ src/Anushik_Gevorgyan/ScopeErr.java
@@ -0,0 +1,10 @@
+package Anushik_Gevorgyan;
+
+public class ScopeErr {
+    public static void main(String[]args){
+        int bar = 1;{
+
+            int ba = 2;
+        }
+    }
+}
