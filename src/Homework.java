590109b32849ed555152606423e9a8649525a6aa
mkrtichstepanyan <32538476+mkrtichstepanyan@users.noreply.github.com>
2023-01-09 21:17:53 +0400

    fixed homework

--- src/Homework.java
+++ src/Homework.java
@@ -3,7 +3,7 @@ public class Homework {
       /* Homework
 
         Create package with your <name_surname> in src package (see example with my name )
-        Write all book examples
+        Write all book examples in Chapter 3
         Create separate classes if there is need
         Create new branch <homework_1_name_surname> -> example homework_1_mkrtich_stepanyan
         commit and push into new branch
