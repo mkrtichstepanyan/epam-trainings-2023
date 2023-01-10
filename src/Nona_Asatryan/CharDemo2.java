38b534d678f5481b9263d46edf556a1d86a92d57
nona.asatryan <nonadev@gmail.com>
2023-01-10 23:36:50 +0400

    Initial commit

---
+++ src/Nona_Asatryan/CharDemo2.java
@@ -0,0 +1,13 @@
+package Nona_Asatryan;
+
+public class CharDemo2 {
+    public static void main(String[] args) {
+        char ch1;
+
+        ch1 = 'X';
+        System.out.println("ch1 contains " + ch1);
+
+        ch1++;
+        System.out.println("ch1 is now " + ch1);
+    }
+}
