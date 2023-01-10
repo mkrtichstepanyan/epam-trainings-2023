4630f30b7b56458137d2e30a942508f74bd6de52
garik <garik.garibyan.1990@gmail.com>
2023-01-10 16:00:48 +0400

    create CharDemo and BoolTest classes

---
+++ src/Garik_Gharibyan/BoolTest.java
@@ -0,0 +1,18 @@
+package Garik_Gharibyan;
+
+public class BoolTest {
+    public static void main(String[] args) {
+
+        boolean b = false;
+        System.out.println("Boolean b is - " + b);
+
+        b = true;
+        System.out.println("Boolean b is - " +b);
+
+        if(b){
+            System.out.println("This is executes.");
+        }
+
+        System.out.println("10>9 result is - " + (10>5));
+    }
+}
---
+++ src/Garik_Gharibyan/CharDemo.java
@@ -0,0 +1,13 @@
+package Garik_Gharibyan;
+
+public class CharDemo {
+    public static void main(String[] args) {
+
+        char char1 = 100;
+        char char2 = 's';
+        System.out.println("char1: " + char1 + ", char2: "  + char2);
+
+        char2++;
+        System.out.println("char3: " + char2);
+    }
+}
