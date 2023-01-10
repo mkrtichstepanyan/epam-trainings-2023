74d76a226be0faa78c929325d138ff98637dd639
AraksyaGhazaryan <araksya.ghazaryan.im@gmail.com>
2023-01-10 16:41:53 +0400

    added homework 1

---
+++ src/Araksya_Ghazaryan/AutoArray.java
@@ -0,0 +1,8 @@
+package Araksya_Ghazaryan;
+
+public class AutoArray {
+    public static void main(String[] args) {
+        int[] month_days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        System.out.print(" April has " + month_days [3] + " days");
+    }
+}
