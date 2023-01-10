0c2f7e2357166b0f665b08530e036328377fe676
Vazgen Harutyunyan <vazgenharutyunyan38@gmail.com>
2023-01-10 09:28:40 -0800

    homework_1 is done

---
+++ src/Vazgen_Harutyunyan/chapter3/characters/CharDemo2.java
@@ -0,0 +1,13 @@
+package Vazgen_Harutyunyan.chapter3.characters;
+
+public class CharDemo2 {
+    public static void main(String[] args) {
+        char ch1;
+
+        ch1 = 'X';
+        System.out.println("ch1 contains " + ch1);
+
+        ch1++; //increment ch1
+        System.out.println("ch1 is now " + ch1);
+    }
+}
