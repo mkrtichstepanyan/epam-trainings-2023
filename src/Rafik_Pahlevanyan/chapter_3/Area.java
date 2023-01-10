4ca1bf6888104ab274d5e926e5badc56ea2ba620
rafpahlevanyan <rafpahlevanyan@gmail.com>
2023-01-10 12:52:50 +0400

    Added chapters 2 and 3 to the lessons section.

---
+++ src/Rafik_Pahlevanyan/chapter_3/Area.java
@@ -0,0 +1,14 @@
+package Rafik_Pahlevanyan.chapter_3;
+
+//Compute the area of a circle
+public class Area {
+    public static void main(String[] args) {
+        double pi, r, a;
+        r = 10.8; //radius of circle
+        pi = 3.1416; //pi, approximately
+        a = pi * r * r;//compute area
+
+        System.out.println("Area of circle is " + a);
+
+    }
+}
