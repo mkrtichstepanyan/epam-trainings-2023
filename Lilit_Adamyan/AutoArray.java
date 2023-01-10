fdcb3653ca74a6cc82b53a4c57d08e53a16c66e1
LAdamyan <ladamyan@bk.ru>
2023-01-10 16:55:47 +0400

    Delete wrong another Lilit_Adamyan folder out of src

--- Lilit_Adamyan/AutoArray.java
+++
@@ -1,9 +0,0 @@
-package Lilit_Adamyan;
-
-public class AutoArray {
-    public static void main(String[] args) {
-        int[]month_days = {31,28,31,30,31,30,31,31,30,31,30,31};
-
-        System.out.println("September  has " + month_days[8] + " days.");
-    }
-}
