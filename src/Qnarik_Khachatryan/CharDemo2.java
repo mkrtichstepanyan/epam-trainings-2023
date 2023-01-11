6955cce3c305f64b6c6fea7d950525568480b864
Knarik8 <k_armina@mail.ru>
2023-01-11 21:38:55 +0300

    homework_1_qnarik_khachatryan

---
+++ src/Qnarik_Khachatryan/CharDemo2.java
@@ -0,0 +1,13 @@
+package Qnarik_Khachatryan;
+
+class CharDemo2 {
+    public static void main(String[] args) {
+        char ch1;
+
+        ch1 = 'X';
+        System.out.println("ch1 contains " + ch1);
+
+        ch1++;
+        System.out.println("ch1 is now " + ch1);
+    }
+}
