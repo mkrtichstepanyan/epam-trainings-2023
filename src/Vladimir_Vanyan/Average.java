313f184e8b26ac41192aebecb6f548c38124d8ae
vladvanyan95 <vlovvayan95@gmail.com>
2023-01-11 22:02:23 +0400

    homework 1

---
+++ src/Vladimir_Vanyan/Average.java
@@ -0,0 +1,13 @@
+package Vladimir_Vanyan;
+
+public class Average {
+    public static void main(String[] args) {
+        double[] nums = {10.1, 11.2, 12.3, 13.4, 14.5};
+        double resault = 0;
+        int i;
+        for (i = 0; i < 5; i++) {
+            resault = resault + nums[i];
+        }
+        System.out.println("Average is " + resault / 5);
+    }
+}
