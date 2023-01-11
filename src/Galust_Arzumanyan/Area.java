62138ca264789b31965f5124b20a933481cb9be5
arzumanyan91 <arzumanyan100991@gmail.com>
2023-01-11 17:23:38 +0400

    Added java classes from book (chapter3)

---
+++ src/Galust_Arzumanyan/Area.java
@@ -0,0 +1,13 @@
+package Galust_Arzumanyan;
+
+//This method is used to calculate the area of circle
+class Area {
+    public static void main(String[] args) {
+        double radius;
+        double pi, area;
+        radius = 15.0; // radius of circle
+        pi = 3.1416; // constant number
+        area = pi * radius * radius; // area of circle
+        System.out.println("Area of circle is " + area);
+    }
+}
