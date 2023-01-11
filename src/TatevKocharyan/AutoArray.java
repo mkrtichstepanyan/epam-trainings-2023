fa8ea5cec26dd903b968b7442e03dbbc52f209ba
TatevTatev <fcb.tatev@gmail.com>
2023-01-12 01:27:07 +0400

    Homework.The examples of the book.

---
+++ src/TatevKocharyan/AutoArray.java
@@ -0,0 +1,9 @@
+package TatevKocharyan;
+
+public class AutoArray {
+    public static void main(String[] args) {
+        int[] monthDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31,
+                30, 31};
+        System.out.println("April has " + monthDays[3] + " days.");
+    }
+}
---
+++ src/TatevKocharyan/ScopeErr.java
@@ -0,0 +1,14 @@
+package TatevKocharyan;
+
+public class ScopeErr {
+    public static void main(String[] args) {
+        int bar=1;
+      /*
+
+       {
+            int bar=2; //Compile-time error-bar already defined
+        }
+
+        */
+    }
+}
