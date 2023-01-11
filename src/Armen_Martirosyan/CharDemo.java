8c0aa480bd2542009fcb771035561e201d269cb4
armenmartirosyan <armmartirosyan@mail.ru>
2023-01-11 23:22:00 +0400

    I done my Homework_1

---
+++ src/Armen_Martirosyan/CharDemo.java
@@ -0,0 +1,16 @@
+package Armen_Martirosyan;
+
+public class CharDemo {
+    public static void main(String[] args) {
+        char ch1 = 88;
+        char ch2 = 'Y';
+
+        System.out.println("ch1 and ch2 " + ch1 + " " + ch2);
+
+        ch1='X';
+        System.out.println("ch1 contains "+ ch1);
+
+        ch1++;
+        System.out.println("ch1 is now " + ch1);
+    }
+}
---
+++ src/Armen_Martirosyan/TwoDArray.java
@@ -0,0 +1,44 @@
+package Armen_Martirosyan;
+
+public class TwoDArray {
+    public static void main(String[] args) {
+        int[][] twoD = new int[4][5];
+        int[][] twoD1 = new int[4][];
+        twoD1[0] = new int[1];
+        twoD1[1] = new int[2];
+        twoD1[2] = new int[3];
+        twoD1[3] = new int[4];
+
+        int i, j, k = 0;
+        for (i = 0; i < 4; i++) {
+            for (j = 0; j < 5; j++) {
+                twoD[i][j] = k;
+                k++;
+            }
+        }
+
+        for (i = 0; i < 4; i++) {
+            for (j = 0; j < 5; j++) {
+                System.out.print(twoD[i][j] + " ");
+            }
+            System.out.println();
+        }
+        i=0;
+        j=0;
+         k=0;
+
+        for (i = 0; i < 4; i++) {
+            for (j = 0; j < i + 1; j++) {
+                twoD1[i][j] = k;
+                k++;
+            }
+        }
+
+        for (i = 0; i < 4; i++) {
+            for (j = 0; j < i+1; j++) {
+                System.out.print(twoD1[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
