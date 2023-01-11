3110411c4eaf12242ec364634a2f95a576831ea4
karsargsyan2001 <karsargsyan40@gmail.com>
2023-01-11 20:29:41 +0400

    Added new homewokr_1_karen_sargsyan

--- src/Karen_Sargsyan/ScopeErr.java
+++ src/Karen_Sargsyan/ScopeErr.java
@@ -1,2 +1,8 @@
-package Karen_Sargsyan;public class ScopeErr {
+package Karen_Sargsyan;
+
+public class ScopeErr {
+    public static void main(String[] args) {
+        int bar = 1;
+//        int bar = 2; // Compile-time error - bar already defined.
+    }
 }
