5fb911c0eb593acd4ee812e5c313e8fc8977c7f7
Samhakobyan1 <samohakobyan35@gmail.com>
2023-01-11 16:01:14 +0400

    HomeWork

--- src/Samvel_Hakobyan/BoolTest.java
+++ src/Samvel_Hakobyan/BoolTest.java
@@ -3,14 +3,15 @@ package Samvel_Hakobyan;
 public class BoolTest {
     public static void main(String[] args) {
 
-        boolean b;
-        b = false;
-        System.out.println(" 5/ " + " b equals " + b);
+boolean b;
+b = false;
+        System.out.println(" b is " + b);
         b = true;
-        System.out.println(" b equals " + b);
-        if (b);System.out.println(" This is executed ");
+        System.out.println(" b is " + b);
+        if (b) System.out.println(" This is executed ");
         b = false;
-        System.out.println(" 10 > 9 equals " + ( 10 > 9 ) );
+        if (b) System.out.println(" This is not executed ");
+        System.out.println(" 10 > 9 " + (10 > 9));
 
     }
 }
