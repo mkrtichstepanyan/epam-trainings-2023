74cf5805f6039fd6fe83d3bdc4a956888240253f
Argisht <100313895+Argisht25@users.noreply.github.com>
2023-01-12 19:15:31 +0400

    chapter 3 examples (#66)
    * chapter 3 examples

---
+++ src/Argishti Mesropyan/BoolTest.java
@@ -0,0 +1,15 @@
+public class BoolTest {
+    public static void main(String[] args) {
+        boolean b;
+        b = false;
+        System.out.println("b is " + b);
+
+        b = true;
+        System.out.println("b is " + b);
+        if (b) System.out.println("This is executed.");
+
+        b = false;
+        if (b) System.out.println("this is not executed.");
+        System.out.println("10 > 9 is " + (10 > 9));
+    }
+}
