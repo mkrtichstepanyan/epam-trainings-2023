8e287d40a782b025a6090ebea24d4c22ae51183b
vardanmkrtchyann <vardanmkrtchyan296@gmail.com>
2023-01-12 19:31:28 +0400

    Homework 1 completed

---
+++ src/Vardan_Mkrtchyan/ScopeErr.java
@@ -0,0 +1,13 @@
+package Vardan_Mkrtchyan;
+
+public class ScopeErr {
+    public static void main(String[] args) {
+        int bar = 1;
+        {
+            //int bar = 2;
+            int x = 8;
+        }
+        int x = 9;
+        System.out.println(x);
+    }
+}
