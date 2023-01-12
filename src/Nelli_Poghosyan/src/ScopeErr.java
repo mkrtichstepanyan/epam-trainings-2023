2a7d110a7fefbdeee55c2ebff9f2ac7dc58bf00e
DavitPoghosyan177 <davit.poghosyan1.d@gmail.com>
2023-01-13 01:19:05 +0400

    Homework 1: Chapter 3 examples.

---
+++ src/Nelli_Poghosyan/src/ScopeErr.java
@@ -0,0 +1,11 @@
+//this programm will not compile
+/*
+public class ScopeErr {
+    public static void main(String[] args) {
+        int bar = 1;
+        {
+            int bar = 2;
+        }
+    }
+}
+*/
