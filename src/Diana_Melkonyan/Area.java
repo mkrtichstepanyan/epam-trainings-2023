23fb33560a54f31be626587c1490fed5b43420d3
DianaMelkonyan84 <dianamelkonyanit@gmail.com>
2023-01-12 16:41:09 +0400

    Added Area.java

--- src/Diana_Melkonyan/Area.java
+++ src/Diana_Melkonyan/Area.java
@@ -1,7 +1,9 @@
 package Diana_Melkonyan;
 
 public class Area {
-    public static void Arr(){
+    public static void main(String[] args) {
+
+    }{
         double r = 10.8;
         double pi = 3.1416;
         double a = pi * r * r;
