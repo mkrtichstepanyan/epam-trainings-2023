e5580e2b52e132fa13a37d4d516ef04cef589dd7
Ani Kovalenko <ani.kovalenko97@gmail.com>
2023-01-12 15:33:53 +0400

    Added 1st homework.*updated. Ani Kovalenko

--- src/Ani_Kovalenko/Scope_Error.java
+++ src/Ani_Kovalenko/Scope_Error.java
@@ -4,7 +4,7 @@ public class Scope_Error {
     public static void main(String [] args){
         int bar = 1;
         /* { int bar = 2;
-            System.out.print (bar);
+            System.out.print (bar);           bar is already defined as 1 > compile error
         } */
     }
 }
