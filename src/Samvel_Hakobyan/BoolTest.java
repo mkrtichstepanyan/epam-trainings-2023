6bfe45d7b63864a79b7b6930f8d81959cac1be46
Samhakobyan1 <samohakobyan35@gmail.com>
2023-01-12 13:04:15 +0400

    Home_Work

---
+++ src/Samvel_Hakobyan/BoolTest.java
@@ -0,0 +1,16 @@
+package Samvel_Hakobyan;
+
+public class BoolTest {
+    public static void main(String[] args) {
+
+        boolean b;
+        b = false;
+        System.out.println(" b equals " + b);
+        b = true;
+        System.out.println(" b equals " + b);
+        if (b);System.out.println(" This is  excuted ");
+        b = false;
+        if (b);System.out.println(" This is not excuted ");
+        System.out.println(" 10 > 9 equals " + ( 10 > 9 ) );
+    }
+}
