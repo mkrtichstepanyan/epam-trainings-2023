b6baaead2dc2924dc3d1f0bfa450491fa0b80388
DianaMelkonyan84 <dianamelkonyanit@gmail.com>
2023-01-12 14:45:43 +0400

    added new homework

---
+++ src/Diana_Melkonyan/Area.java
@@ -0,0 +1,10 @@
+package Diana_Melkonyan;
+
+public class Area {
+    public static void Arr(){
+        double r = 10.8;
+        double pi = 3.1416;
+        double a = pi * r * r;
+        System.out.println("Area of circle is" + a);
+    }
+}
