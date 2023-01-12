cf0c1c930b206906f47decabd0fdc2ebe37857b9
DavitPoghosyan177 <davit.poghosyan1.d@gmail.com>
2023-01-12 22:05:55 +0400

    Homework:Chapter 3

---
+++ src/Davit_Poghosyan/src/AutoArray.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class AutoArray {
+    public static void main(String[] args){
+        int[] mouth_days = {31 , 28 , 31 , 30 , 31 , 30 , 31 , 31 , 30 , 31 , 30 , 31};
+        System.out.println("April has" + mouth_days[3] + "days.");
+    }
+}
---
+++ src/Davit_Poghosyan/src/ScopeErr.java
@@ -0,0 +1,8 @@
+public class ScopeErr {
+    public static void main(String[] args) {
+        int bar = 1;
+        {
+            int bar=2; //already defined
+        }
+    }
+}
