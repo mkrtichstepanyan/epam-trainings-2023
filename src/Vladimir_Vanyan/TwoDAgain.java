a75766e2f95fefd1a9a39a82eb546690996253d3
vladvanyan95 <vlovvayan95@gmail.com>
2023-01-12 19:20:04 +0400

    Created 19 classes from chapter 3

--- src/Vladimir_Vanyan/TwoDAgain.java
+++ src/Vladimir_Vanyan/TwoDAgain.java
@@ -17,7 +17,7 @@ public class TwoDAgain {
             }
         for (i = 0; i < 4; i++)
             for (j = 0; j < i + 1; j++)
-                System.out.println(twoD[i][j] + " ");
+                System.out.print(twoD[i][j] + " ");
         System.out.println();
 
     }
