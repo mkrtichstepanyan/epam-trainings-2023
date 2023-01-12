947c51c88a78e8e5aab8ddeeb78f8a1d6a7edc25
hovhannes.abrahamyan.1995@mail.ru <levonaloian@gmail.com>
2023-01-13 00:41:13 +0400

    Refactoring

--- src/Hovhannes_Abrahamyan/ThreeDMatrix.java
+++ src/Hovhannes_Abrahamyan/ThreeDMatrix.java
@@ -4,14 +4,14 @@ public class ThreeDMatrix {
         int threeD[][][] = new int[3][4][5];
         int i, j, k;
 
-        for(i=0; i<3; i++)
-            for(j=0;j<4; j++)
-                for(k=0; k<5; k++)
+        for (i = 0; i < 3; i++)
+            for (j = 0; j < 4; j++)
+                for (k = 0; k < 5; k++)
                     threeD[i][j][k] = i * j * k;
 
-        for(i=0; i<3; i++) {
-            for(j=0; j<4; j++) {
-                for(k=0; k<5; k++)
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 4; j++) {
+                for (k = 0; k < 5; k++)
                     System.out.print(threeD[i][j][k] + " ");
                 System.out.println();
             }
