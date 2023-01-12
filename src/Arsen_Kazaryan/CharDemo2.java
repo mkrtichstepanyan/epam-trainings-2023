524da559992cca4d2a9d36d660ddf61824831d93
ArsenKazaryan <akazaran239@gmail.com>
2023-01-12 23:51:40 +0400

    Homework_1_Arsen_Kazaryan

---
+++ src/Arsen_Kazaryan/CharDemo2.java
@@ -0,0 +1,10 @@
+package Arsen_Kazaryan;
+
+public class CharDemo2 {
+    public static void main(String[] args) {
+        char char1 = 100;
+        System.out.println("old char1 - " + char1);
+        char1++;
+        System.out.println("new char1 - " + char1);
+    }
+}
---
+++ src/Arsen_Kazaryan/Dynlnit.java
@@ -0,0 +1,11 @@
+package Arsen_Kazaryan;
+
+public class Dynlnit {
+    public static void main(String[] args) {
+        double a = 3.0;
+        double b = 4.0;
+        double c = Math.sqrt(a * a + b * b);
+
+        System.out.println("Hypotenuse is " + c);
+    }
+}
