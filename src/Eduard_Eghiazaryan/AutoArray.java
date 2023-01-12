f1e01a32b52ef31cbb9557f4b89df4a1331d6573
Eduard Eghiazaryan <egiazaryaneduard3@gmail.com>
2023-01-12 17:19:53 +0400

    Homework_1,2_Eduard_Eghiazaryan

---
+++ src/Eduard_Eghiazaryan/AutoArray.java
@@ -0,0 +1,9 @@
+package Eduard_Eghiazaryan;
+
+public class AutoArray {
+    public static void main (String[] args){
+        int [] month_days = {31, 28, 31, 30, 31, 30,
+                            31, 31, 30, 31, 30, 31};
+        System.out.printf("In April" + month_days [3] + "days.");
+    }
+}
