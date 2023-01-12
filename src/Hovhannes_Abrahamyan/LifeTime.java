f87fdb0cfdc0eac8c3b3abcd4e1fca3c985dab82
LevonAloyan <levonaloian@gmail.com>
2023-01-13 00:08:57 +0400

    Done_Homework

---
+++ src/Hovhannes_Abrahamyan/LifeTime.java
@@ -0,0 +1,12 @@
+
+public class LifeTime {
+    public static void main(String args[]) {
+        int x;
+
+        for(x = 0;x < 3; x++){
+            int y = -1;
+            System.out.println("y is: " + y);
+            System.out.println("y is now: " + y);
+        }
+    }
+}
---
+++ src/Hovhannes_Abrahamyan/ScopeErr.java
@@ -0,0 +1,9 @@
+
+public class ScopeErr {
+    public static void main(String args[]) {
+        int bar = 1;
+        {
+//            int bar = 2;
+        }
+    }
+}
