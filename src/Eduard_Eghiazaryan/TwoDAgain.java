08f719573c62be0ba732af0f15e93fca80d0faec
Eduard Eghiazaryan <egiazaryaneduard3@gmail.com>
2023-01-13 10:52:48 +0400

    FixHomework_1,2_Eduard_Eghiazaryan

--- src/Eduard_Eghiazaryan/TwoDAgain.java
+++ src/Eduard_Eghiazaryan/TwoDAgain.java
@@ -9,14 +9,14 @@ public class TwoDAgain {
         twoD[3] = new int[4];
 
         int i, j, k = 0;
-        for (i = 0; i < 4; i++) {
-            for (j = 0; j < i + i; j++)
-            twoD [i] [j] = k;
-            k++;
-        }
+        for (i = 0; i < 4; i++)
+            for (j = 0; j < i + 1; j++) {
+                twoD[i][j] = k;
+                k++;
+            }
         for (i = 0; i < 4; i++) {
             for (j = 0; j < i + 1; j++)
-                System.out.printf(twoD[i][j] + "");
+                System.out.print(twoD[i][j] + "");
             System.out.println();
         }
     }
