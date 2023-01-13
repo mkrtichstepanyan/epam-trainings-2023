600367c9ce846d046fc61641a621a7e9d41fe95d
Shushanik Araqelyan <araqelyanshushan1991@gmail.com>
2023-01-13 11:10:46 +0400

    my homework

---
+++ src/Shushanik_Araqelyan/BoolTest.java
@@ -0,0 +1,16 @@
+package Shushanik_Araqelyan;
+
+public class BoolTest {
+    public static void main(String[] args){
+        boolean b;
+       b = false;
+
+System . out.println("b="+b);
+       b = true;
+       System.out.println("b=" + b);
+       if(b) System. out. println("This is executed.");
+       b = false ;
+       System. out. println("This is not executed.");
+       System. out. println("10>9 =" + (10>9) );
+    }
+}
---
+++ src/Shushanik_Araqelyan/CharDemo2.java
@@ -0,0 +1,9 @@
+package Shushanik_Araqelyan;
+
+public class CharDemo2 {
+    public static void main(String[] args) {
+        char ch1;
+        ch1 = 'x';
+        System.out.println("ch1" + ch1);
+    }
+}
