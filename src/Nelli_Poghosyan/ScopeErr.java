5abf6268c598fea73e82b27baff6d0e22f79dc01
DavitPoghosyan177 <davit.poghosyan1.d@gmail.com>
2023-01-13 18:04:49 +0400

    Updated homework:Chapter 3

---
+++ src/Nelli_Poghosyan/ScopeErr.java
@@ -0,0 +1,13 @@
+package Nelli_Poghosyan
+
+//this programm will not compile
+/*
+public class ScopeErr {
+    public static void main(String[] args) {
+        int bar = 1;
+        {
+            int bar = 2;
+        }
+    }
+}
+*/
---
+++ src/Nelli_Poghosyan/VarDemo.java
@@ -0,0 +1,15 @@
+package Nelli_Poghosyan
+
+//A simple demo of local variable type inference
+public class VarDemo {
+    public static void main(String[] args) {
+        var avg = 10.0;
+        System.out.println("Value of avg: " + avg);
+
+        int var = 1;
+        System.out.println("Value of var: " + var);
+
+        var k = - var;
+        System.out.println("Value of k: " +k);
+    }
+}
