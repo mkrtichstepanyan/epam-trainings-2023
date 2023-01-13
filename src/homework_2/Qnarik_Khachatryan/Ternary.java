2c6699acb52b5a09a281c8a3ceda53d270488f44
Knarik8 <k_armina@mail.ru>
2023-01-14 02:32:36 +0300

    homework_2 Qnarik Khachatryan

---
+++ src/homework_2/Qnarik_Khachatryan/Ternary.java
@@ -0,0 +1,20 @@
+package homework_2.Qnarik_Khachatryan;
+
+//Demonstrate ?
+class Ternary {
+    public static void main(String[] args) {
+        int i, k;
+
+        i = 10;
+        k = i < 0 ? -i : i; // get absolute value of i
+        System.out.print("Absolute value of ");
+        System.out.println(i + " is " + k);
+
+        i = -10;
+        k = i < 0 ? -i : i; // get absolute value of i
+        System.out.print("Absolute value of ");
+        System.out.println(i + " is " + k);
+
+
+    }
+}
