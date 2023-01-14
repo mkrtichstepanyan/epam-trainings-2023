be147b5385996f41df00c4ec4833f9e275a4196c
Samvel Hakobyan <121867165+Samhakobyan1@users.noreply.github.com>
2023-01-15 00:55:43 +0400

    Homework_2_samvel_hakobyan (#121)
    * Home_Work
    
    * Home_Work
    
    * Home_Work
    
    * Home_Work
    
    * Home_Work
    
    * Home_Work_4_Operations
    
    * Home_Work_4_Operations
    
    * Home_Work_4_Operations
    
    * Home_Work_4_Operations
    
    * homework
    
    * Fix in Homework_2.java
    
    Co-authored-by: Varduhi Karapetyan <varduhi_karapetyan@epam.com>

---
+++ src/homework_2/Samvel_Hakobyan/MultByTwo.java
@@ -0,0 +1,13 @@
+package homework_2.Samvel_Hakobyan;
+
+public class MultByTwo {
+    public static void main(String[] args) {
+
+        int i;
+        int num = 0xFFFFFFF;
+        for (i = 0; i < 4; i++) {
+            num = num << 1;
+            System.out.println(num);
+        }
+    }
+}
