9011e5da315c25fe55b1345d06203248568a5b83
Samhakobyan1 <samohakobyan35@gmail.com>
2023-01-14 13:13:35 +0400

    Home_Work

--- src/homework_2/Homework_2.java
+++ src/homework_2/Homework_2.java
@@ -7,12 +7,12 @@ public class Homework_2 {
         2. Write all book examples in Chapter 4
         3. Create separate classes if there is need
         4. Todo Calculate and write correct values instead of "???" symbols below (Write this part of homework inside this comment -> Just replace the "???" symbols)
-                   * Convert number 456 from decimal to binary = ????
-                   * Convert number 01001001011 from binary to decimal = ???
-                   * Convert number 450 from octal to decimal = ???
-                   * Convert number 345 from decimal to octal = ???
-                   * Convert number 542 from decimal to hexadecimal = ???
-                   * Convert number 34F1E from hexadecimal to decimal = ???
+                   * Convert number 456 from decimal to binary = 111001000
+                   * Convert number 01001001011 from binary to decimal = 587
+                   * Convert number 450 from octal to decimal = 296
+                   * Convert number 345 from decimal to octal = 531
+                   * Convert number 542 from decimal to hexadecimal = 21E
+                   * Convert number 34F1E from hexadecimal to decimal = 216862
                    Example:
                    * Convert number 2 from decimal to binary = 0000 0010
         5. Create new branch <homework_2_name_surname> -> example homework_2_mkrtich_stepanyan
