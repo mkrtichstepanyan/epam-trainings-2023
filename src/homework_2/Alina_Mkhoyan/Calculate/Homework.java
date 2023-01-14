619a75c047cf34ddf79cbbef8a7113bd98dd7f33
alinamkhoyan00 <alinamkhoyan19@gmail.com>
2023-01-14 23:47:40 +0400

    Chapter 4 homework and Calculate:

---
+++ src/homework_2/Alina_Mkhoyan/Calculate/Homework.java
@@ -0,0 +1,14 @@
+package homework_2.Alina_Mkhoyan.Calculate;
+
+// Todo Calculate and write correct values instead of "???" symbols below (Write this part of homework inside this comment -> Just replace the "???" symbols)
+//         * Convert number 456 from decimal to binary =  1110011000
+//         * Convert number 01001001011 from binary to decimal = 683
+//         * Convert number 450 from octal to decimal = 296 in decimal
+//         * Convert number 345 from decimal to octal = 531
+//         * Convert number 542 from decimal to hexadecimal = 21A
+//         * Convert number 34F1E from hexadecimal to decimal = 2171214
+//         Example:
+//         * Convert number 2 from decimal to binary = 0000 0010
+public class Homework {
+
+}
---
+++ src/homework_2/Alina_Mkhoyan/chap_4/ByteShift.java
@@ -0,0 +1,13 @@
+package homework_2.Alina_Mkhoyan.chap_4;
+
+public class ByteShift {
+    public static void main(String[] args) {
+        byte a = 64, b;
+        int i;
+
+        i = a << 2;
+        b = (byte) (a << 2);
+        System.out.println("Original value of a: " + a);
+        System.out.println("i and b: " + i + " " + b);
+    }
+}
