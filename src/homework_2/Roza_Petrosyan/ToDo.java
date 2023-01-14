409a377508ec0228d4e8d962e30fce8bfd46c647
roza-petrosyan <petrosyanroza95@gmail.com>
2023-01-14 17:53:09 +0400

    Added Chapter4 all code examples

---
+++ src/homework_2/Roza_Petrosyan/ToDo.java
@@ -0,0 +1,21 @@
+package homework_2.Roza_Petrosyan;
+
+public class ToDo {
+    public static void main(String[] args) {
+//        Todo Calculate and write correct values instead of "???" symbols below (Write this part of homework inside this comment -> Just replace the "???" symbols)
+//                * Convert number 456 from decimal to binary = ???
+//                * Convert number 01001001011 from binary to decimal = ???
+//                * Convert number 450 from octal to decimal = ???
+//                * Convert number 345 from decimal to octal = ???
+//                * Convert number 542 from decimal to hexadecimal = ???
+//                * Convert number 34F1E from hexadecimal to decimal = ???
+//        Example:
+//                   * Convert number 2 from decimal to binary = 0000 0010
+        System.out.println("The value of number 456 from decimal to binary is: 0001 1100 1000");
+        System.out.println("The value of number 01001001011 from binary to decimal: " + 587);
+        System.out.println("The value of number 450 from octal to decimal: " + 296);
+        System.out.println("The value of number 345 from decimal to octal: " + 531);
+        System.out.println("The value of number 542 from decimal to hexadecimal: 21E");
+        System.out.println("The value of number 34F1E from hexadecimal to decimal: " + 216862);
+    }
+}
