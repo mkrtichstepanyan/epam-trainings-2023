33c3ef8cc8d39c274560e5e7f2c839215ff12845
nona.asatryan <nonadev@gmail.com>
2023-01-14 22:42:01 +0400

    Homework_2_Chapter_4

---
+++ src/homework_2/Nona_Asatryan/Ternary.java
@@ -0,0 +1,17 @@
+package homework_2.Nona_Asatryan;
+
+public class Ternary {
+    public static void main(String[] args) {
+        int i, k;
+
+        i = 10;
+        k = i < 0 ? -i : i;
+        System.out.print("Absolute value of ");
+        System.out.println(i + " is " + k);
+
+        i = -10;
+        k = i < 0 ? -i : i;
+        System.out.print("Absolute value of ");
+        System.out.println(i + " is " + k);
+    }
+}
