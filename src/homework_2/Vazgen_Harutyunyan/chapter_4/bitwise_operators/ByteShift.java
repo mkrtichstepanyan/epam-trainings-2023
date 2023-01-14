d5081b972c47a1941831b1c5bc91ce290fcecf31
Vazgen Harutyunyan <vazgenharutyunyan38@gmail.com>
2023-01-14 11:30:23 -0800

    homework_2 is done

---
+++ src/homework_2/Vazgen_Harutyunyan/chapter_4/bitwise_operators/ByteShift.java
@@ -0,0 +1,14 @@
+package homework_2.Vazgen_Harutyunyan.chapter_4.bitwise_operators;
+
+public class ByteShift {
+    public static void main(String[] args) {
+        byte a = 64, b;
+        int i;
+
+        i = a << 2;
+        b = (byte) (a << 2);
+
+        System.out.println("Original value of a: " + a);
+        System.out.println("i and b: " + i + " " + b);
+    }
+}
