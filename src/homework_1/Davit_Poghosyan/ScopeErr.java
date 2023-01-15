5b3eb21823b7f75a7981110a33709a97800bc670
DavitPoghosyan <davit.poghosyan1.d@gmail.com>
2023-01-15 07:28:08 -0800

    Homework1 and Homework2

---
+++ src/homework_1/Davit_Poghosyan/ScopeErr.java
@@ -0,0 +1,10 @@
+package homework_1.Davit_Poghosyan;
+
+public class ScopeErr {
+    public static void main(String[] args) {
+        int bar = 1;
+        {
+            int bar=2; //already defined
+        }
+    }
+}
