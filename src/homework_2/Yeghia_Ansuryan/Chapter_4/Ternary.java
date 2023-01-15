9aa4064aa770383af540dc5f72ec705f97c316e6
Yeghia Ansuryan <yeghia.ansuryan.arm@gmail.com>
2023-01-15 16:05:33 +0400

    Added my 2 homework Chapter exampeles 4 and added calculations exercises

---
+++ src/homework_2/Yeghia_Ansuryan/Chapter_4/Ternary.java
@@ -0,0 +1,17 @@
+package homework_2.Yeghia_Ansuryan.Chapter_4;
+
+public class Ternary {
+    public static void main(String[] args) {
+        int i, k;
+
+        i = 10;
+        k = i < 0 ? -i : i;
+        System.out.print("Absolute value of ");
+        System.out.println(i + " is " + k);
+
+        i = -10;
+        k = i < 0 ? -i : i;
+        System.out.print("Absolute value of ");
+        System.out.println(i + " is " + k);
+    }
+}
