842ff3d31821d5f2044802b0a7d8e54e0869e580
Varsik <vpijoyan03@gmail.com>
2023-01-15 10:47:59 +0300

    Added 2nd homework

---
+++ src/homework_2/Varsik_Pijoyan/BasicMath.java
@@ -0,0 +1,30 @@
+package Varsik_Pijoyan;
+
+//Basic arithmetic operators
+public class BasicMath {
+    public static void main(String[] args) {
+//        arithmetic using integers
+        int a = 1 + 1;
+        int b = a * 3;
+        int c = b / 4;
+        int d = c - a;
+        int e = -d;
+        System.out.println("a =" + a);
+        System.out.println("b =" + b);
+        System.out.println("c =" + c);
+        System.out.println("d =" + d);
+        System.out.println("e =" + e);
+
+//        arithmetic using doubles
+        double da = 1 + 1;
+        double db = da * 3;
+        double dc = db / 4;
+        double dd = dc - a;
+        double de = -dd;
+        System.out.println("da =" + da);
+        System.out.println("db =" + db);
+        System.out.println("dc =" + dc);
+        System.out.println("dd =" + dd);
+        System.out.println("de =" + de);
+    }
+}
---
+++ src/homework_2/Varsik_Pijoyan/MultByTwo.java
@@ -0,0 +1,14 @@
+package Varsik_Pijoyan;
+
+//Left shifting as a quick way to multiply by 2
+public class MultByTwo {
+    public static void main(String[] args) {
+        int i;
+        int num = 0xFFFFFFE;
+
+        for (i = 0; i < 4; i++) {
+            num = num << 1;
+            System.out.println(num);
+        }
+    }
+}
