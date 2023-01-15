cd115028e910331dde10b1797973600ac8c79a15
Eduard Eghiazaryan <egiazaryaneduard3@gmail.com>
2023-01-15 16:30:15 +0400

    Homework_2_Eduard_Eghiazaryan

---
+++ src/Eduard_Eghiazaryan/OpEquals.java
@@ -0,0 +1,17 @@
+package Eduard_Eghiazaryan;
+
+public class OpEquals {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        a += 5;
+        b *= 4;
+        c += a * b;
+        c %= 6;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+    }
+}
