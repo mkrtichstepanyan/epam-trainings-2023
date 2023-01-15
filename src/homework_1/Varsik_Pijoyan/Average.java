2a714dcc5031174599b116edfa7b8636e68d6765
mkrtichstepanyan <32538476+mkrtichstepanyan@users.noreply.github.com>
2023-01-15 20:27:21 +0400

    fixed Varsik's homework 1

---
+++ src/homework_1/Varsik_Pijoyan/Average.java
@@ -0,0 +1,12 @@
+package homework_1.Varsik_Pijoyan;
+
+public class Average {
+    public static void main(String[] args){
+        double [] num={10.1, 11.2, 12.3, 13.4, 14.5};
+        double result=0; int i;
+        for(i=0; i<5; i++){
+            result=result+num[i];
+            System.out.println("Average is:" + result/5);
+        }
+    }
+}
