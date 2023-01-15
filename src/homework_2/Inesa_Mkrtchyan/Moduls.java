22ed30fbc8d9c35fe094539e28b71539dbe9988e
Inea99 <inesamkrtchyan9999@gmail.com>
2023-01-15 14:12:21 +0400

    Add Homework 2

---
+++ src/homework_2/Inesa_Mkrtchyan/Moduls.java
@@ -0,0 +1,10 @@
+package Inesa_Mkrtchyan;
+
+public class Moduls {
+    public static void main(String[] args) {
+        int x = 42;
+        double y = 42.56;
+        System.out.println("x mod 10 =" + x % 10);
+        System.out.println("y mod 10 = " + y % 10);
+    }
+}
