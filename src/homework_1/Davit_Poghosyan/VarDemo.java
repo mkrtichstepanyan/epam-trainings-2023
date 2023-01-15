3fd3eed63f16a3d55d5a325f397e5415950c4c4d
DavitPoghosyan <davit.poghosyan1.d@gmail.com>
2023-01-15 09:45:49 -0800

    homework 1 and homework2

---
+++ src/homework_1/Davit_Poghosyan/VarDemo.java
@@ -0,0 +1,15 @@
+package homework_1.Davit_Poghosyan;
+
+public class VarDemo {
+    public static void main(String[] args) {
+        var avg = 10.0;
+        System.out.println("Value of avg:" +avg);
+
+        int var = 1;
+        System.out.println("Value of var:" +var);
+
+        var k = -var;
+        System.out.println("Value of k" + k);
+
+    }
+}
