06bbfc015597f2230fe9389b50fee231b56b1769
Lyov-Harutyunyan <lyovharutyunian@gmail.com>
2023-01-16 00:50:13 +0400

    "New commit for all classes"

--- src/homework_2/Levon_Harutyunyan/MultByTwo.java
+++ src/homework_2/Levon_Harutyunyan/MultByTwo.java
@@ -1,2 +1,13 @@
-package homework_2.Levon_Harutyunyan;public class MultByTwo {
+package homework_2.Levon_Harutyunyan;
+
+public class MultByTwo {
+    public static void main(String[] args) {
+        int i;
+        int num = 0xFFFFFFE;
+//        System.out.println(num);
+        for (i = 0; i < 4; i++){
+            num = num << 1;
+            System.out.println(num);
+        }
+    }
 }
--- src/homework_2/Levon_Harutyunyan/Ternary.java
+++ src/homework_2/Levon_Harutyunyan/Ternary.java
@@ -1,2 +1,17 @@
-package homework_2.Levon_Harutyunyan;public class Ternary {
+package homework_2.Levon_Harutyunyan;
+
+public class Ternary {
+    public static void main(String[] args) {
+        int i = 10;
+        int k;
+        k = i < 0 ? -i : i;
+        System.out.print("Absolute value of ");
+        System.out.println(i + " is " + k);
+
+        i = -10;
+        k = i < 0 ? -i : i;
+        System.out.print("Absolute value of ");
+        System.out.println(i + " is " + k);
+
+    }
 }
