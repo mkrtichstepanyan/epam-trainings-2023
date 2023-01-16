9fcfcd51b79997f196b28847826116de39fd580e
Shushanik Araqelyan <araqelyanshushan1991@gmail.com>
2023-01-16 11:35:51 +0400

    electronic watch

--- src/homework_2/Shushanik_Araqelyan/ElectronicWatch.java
+++ src/homework_2/Shushanik_Araqelyan/ElectronicWatch.java
@@ -1,2 +1,11 @@
-package homework_2.Shushanik_Araqelyan;public class ElectronicWatch {
+package homework_2.Shushanik_Araqelyan;
+
+public class ElectronicWatch {
+
+    public static void main(String[] args) {
+        int hrs = 86400 / 3600; // 1 hour = 60 mins, 1 hour = 3600sec, 60 mins= 60 * 60secs
+        int mins = (86400 % 3600) / 60;
+        int secs = (86400 % 3600) % 60;
+        System.out.println(hrs +"Hours" + mins +"Minutes" + secs +"Seconds");
+    }
 }
