0cd0e8d133643344ea0fd9126874bc2008f53277
Argisht25 <argoo17@mail.ru>
2023-01-16 14:27:42 +0400

    fix bug homework_class

--- src/homework_2/Argishti_Mesropyan/BoolLogic.java
+++ src/homework_2/Argishti_Mesropyan/BoolLogic.java
@@ -7,7 +7,7 @@ public class BoolLogic {
         boolean c = a | b;
         boolean d = a & b;
         boolean e = a ^ b;
-        boolean f = (!a & b | (a & !b));
+        boolean f = (!a & b) | (a & !b);
         boolean g = !a;
 
         System.out.println("        a = " + a);
