0a4f20f5baa136452041687c36266674d8a3cee2
rafpahlevanyan <rafpahlevanyan@gmail.com>
2023-01-18 00:00:10 +0400

    Added chapter 5 examples in package Rafik Pahlevanyan and task1,2,3 are completed

---
+++ src/homework_3/Rafik_Pahlevanyan/tasks/Task_2.java
@@ -0,0 +1,191 @@
+package homework_3.Rafik_Pahlevanyan.tasks;
+
+import java.util.Scanner;
+
+public class Task_2 {
+
+    public static void main(String[] args) {
+//        printNumbers(50);            // Question 1 -> try with different integer values instead of 10 only
+//        sumNumbers(20);              // Question 2
+//        table();                     // Question 3
+//        factorialDemo1();            // Question 4
+//        powerDemo();                 // Question 5
+//        reverseNumber();             // Question 6
+//        readSetIntegers();           // Question 7
+//        testPrime();                 // Question 8
+    }
+
+
+    /* TODO: Question 1
+        Write a program to print numbers from 1 to given parameter.
+    */
+    public static void printNumbers(int count) {
+        int[] numbers = new int[count];
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(i);
+        }
+    }
+
+    /* TODO: Question 2
+        Write a program to calculate the sum of first 10 natural numbers.
+    */
+    public static void sumNumbers(int i) {
+        int sum = 0;
+        for (i = 0; i <= 10; i++) {
+            sum += i;
+        }
+        System.out.println("Sum: " + sum);
+    }
+
+
+    /* TODO: Question 3
+        Write a program that prompts the user to input a positive integer.
+        It should then print the multiplication table of that number.
+    */
+    public static void table() {
+        Scanner console = new Scanner(System.in);
+        int num;
+
+        System.out.print("Enter any positive integer: ");
+        num = console.nextInt();
+        System.out.println("Multiplication Table of " + num);
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+
+
+        // Todo write your code here ...
+    }
+
+
+    /* TODO: Question 4
+        Write a program to find the factorial value of any number entered through the keyboard.
+    */
+    public static void factorialDemo1() {
+        Scanner console = new Scanner(System.in);
+        int num; // To hold number
+        int fact = 1; // To hold factorial
+
+        System.out.print("Enter any positive integer: ");
+        num = console.nextInt();
+
+        for (int i = 1; i <= num; i++) {
+            fact *= i;
+        }
+
+        System.out.println("Factorial: " + num + " is: " + fact);
+    }
+
+     /* TODO: Question 5
+         Two numbers are entered through the keyboard.
+         Write a program to find the value of one number raised to the power of another.
+         (Do not use Java built-in method)
+     */
+
+    public static void powerDemo() {
+        Scanner console = new Scanner(System.in);
+
+        int base;
+        int power;
+        int result = 1;
+
+        System.out.print("Enter the base number ");
+        base = console.nextInt();
+
+        System.out.print("Enter the power ");
+        power = console.nextInt();
+
+        for (int i = 0; i <= power; i++) {
+            result *= base;
+        }
+
+        System.out.println(base + " raised to the power of " + power + " is " + result);
+    }
+
+     /* TODO: Question 6
+         Write a program that prompts the user to input an integer and then outputs the number with the digits reversed.
+         For example, if the input is 12345, the output should be 54321.
+     */
+
+    public static void reverseNumber() {
+        Scanner console = new Scanner(System.in);
+
+        int number;
+        int reverse = 0;
+
+        System.out.print("Enter the number ");
+        number = console.nextInt();
+
+        int temp = number;
+        while (number != 0) {
+            int digit = number % 10;
+            reverse = reverse * 10 + digit;
+            number /= 10;
+        }
+
+        System.out.println("Reverse of " + temp + " is " + reverse);
+    }
+
+    /* TODO: Question 7
+         Write a program that reads a set of integers, and then prints the sum of the even and odd integers.
+    */
+
+    public static void readSetIntegers() {
+        Scanner console = new Scanner(System.in);
+
+        int number;
+        char choice;
+        int evenSum = 0;
+        int oddSum = 0;
+
+        do {
+            System.out.print("Enter the number ");
+            number = console.nextInt();
+
+            if (number % 2 == 0) {
+                evenSum += number;
+
+            } else {
+                oddSum += number;
+
+            }
+            System.out.println("Sum of even numbers: " + evenSum);
+            System.out.println("Sum of odd numbers: " + oddSum);
+            System.out.print("Do you want to continue y/n? ");
+            choice = console.next().charAt(0);
+
+        } while (choice == 'y' || choice == 'Y');
+
+    }
+
+
+    /* TODO: Question 8
+        Write a program that prompts the user to input a positive integer.
+        It should then output a message indicating whether the number is a prime number.
+    */
+    public static void testPrime() {
+        Scanner console = new Scanner(System.in);
+
+        int number;
+
+        System.out.print("Enter the positive integer ");
+        number = console.nextInt();
+
+        boolean flag = true;
+
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                flag = false;
+                break;
+            }
+        }
+
+        if (flag) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+
+}
+
