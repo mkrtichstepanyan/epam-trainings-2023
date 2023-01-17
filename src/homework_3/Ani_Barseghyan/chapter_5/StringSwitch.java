836454b5427cc177d1b160d449ab1f4a1b60967d
Ani_Barseghyan <abarseghyan@zscaler.com>
2023-01-17 20:13:56 +0400

    Homework_3: Chapter 5 examples

---
+++ src/homework_3/Ani_Barseghyan/chapter_5/StringSwitch.java
@@ -0,0 +1,26 @@
+package homework_3.Ani_Barseghyan.chapter_5;
+
+public class StringSwitch {
+    public static void main(String[] args) {
+        String str = "two";
+
+        switch (str) {
+            case "one": {
+                System.out.println("one");
+                break;
+            }
+            case "two": {
+                System.out.println("two");
+                break;
+            }
+            case "three": {
+                System.out.println("three");
+                break;
+            }
+            default: {
+                System.out.println("No match");
+                break;
+            }
+        }
+    }
+}
