3aea395f93ddcfec317ec97f42aeea97100488b7
LAdamyan <ladamyan@bk.ru>
2023-01-17 22:21:32 +0400

    Add all examples of Chapter 5

---
+++ src/homework_3/Lilit_Adamyan/chapter_5/StringSwitch.java
@@ -0,0 +1,24 @@
+package homework_3.Lilit_Adamyan.chapter_5;
+
+public class StringSwitch {
+    public static void main(String[] args) {
+
+        String str = "two";
+
+        switch (str){
+            case "one":
+                System.out.println("one");
+                break;
+            case "two":
+                System.out.println("two");
+                break;
+            case "three":
+                System.out.println("three");
+                break;
+            default:
+                System.out.println("no match");
+                break;
+
+        }
+    }
+}
