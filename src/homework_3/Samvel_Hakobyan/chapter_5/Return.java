beb52df3b516b55f368d958bda3f44149864a499
Samvel Hakobyan <121867165+Samhakobyan1@users.noreply.github.com>
2023-01-17 19:20:23 +0400

    home_work_3

---
+++ src/homework_3/Samvel_Hakobyan/chapter_5/Return.java
@@ -0,0 +1,12 @@
+package homework_3.Samvel_Hakobyan.chapter_5;
+
+public class Return {
+    public static void main(String[] args) {
+        boolean t = true;
+        System.out.println("Before the return ");
+        if (t){
+            return;
+        }
+        System.out.println("This wont execute ");
+    }
+}
