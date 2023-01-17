d98e96ec26336a236617f4bab123988848c15aaf
Samvel Hakobyan <121867165+Samhakobyan1@users.noreply.github.com>
2023-01-17 15:18:52 +0400

    homework_3

--- src/homework_3/Samvel_Hakobyan/tasks/Task_2.java
+++ src/homework_3/Samvel_Hakobyan/tasks/Task_2.java
@@ -31,11 +31,11 @@ public class Task_2 {
     /* TODO: Question 2
         Write a program to calculate the sum of first 10 natural numbers.
     */
-    public static void sumNumbers(int i) {
+    public static void sumNumbers(int count) {
         // Todo write your code here ...
         int sum = 0;
-        for (i = 1; i <=10 ; i++) {
-            sum +=i;
+        for (int i = 1; i <=10 ; i++) {
+            sum += i;
         }
         System.out.println("Sum: " + sum);
     }
