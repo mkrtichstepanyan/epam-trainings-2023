6aed7216d434fb1e322d63084abedf7b0e7737c6
Yeghia Ansuryan <yeghia.ansuryan.arm@gmail.com>
2023-01-18 01:23:27 +0400

    Added my third my homework from the book, Chapter 5 exercises

---
+++ src/homework_3/Yeghia_Ansuryan/Chapter_5/NoBody.java
@@ -0,0 +1,13 @@
+package homework_3.Yeghia_Ansuryan.Chapter_5;
+
+public class NoBody {
+    public static void main(String[] args) {
+        int i, j;
+
+        i = 100;
+        j = 200;
+        while (++i < --j);
+            System.out.println("Midproint is " + i);
+
+    }
+}
