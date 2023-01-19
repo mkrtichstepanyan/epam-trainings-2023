10ce465b0c9e96731c6520faffd58bbd7855c093
Lyov-Harutyunyan <lyovharutyunian@gmail.com>
2023-01-20 01:26:03 +0400

    Compelete Chapter_5

---
+++ src/homework_3/Levon_Harutyunyan/Chapter_5/Nested.java
@@ -0,0 +1,15 @@
+package homework_3.Levon_Harutyunyan.Chapter_5;
+
+public class Nested {
+    public static void main(String[] args) {
+        int i;
+        int j;
+
+        for (i = 0; i < 10; i++) {
+            for (j = i; j < 10; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
