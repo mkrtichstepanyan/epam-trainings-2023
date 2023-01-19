75782a94d1092ea6f659a9568285fb05d3a04548
Knarik8 <k_armina@mail.ru>
2023-01-19 18:03:58 +0300

    Homework 3 Qnarik Khachatryan

---
+++ src/homework_3/Qnarik_Khachatryan/chapter_5/While.java
@@ -0,0 +1,12 @@
+package homework_3.Qnarik_Khachatryan.chapter_5;
+//Demonstrate the while loop.
+
+class While {
+    public static void main(String[] args) {
+        int n = 10;
+        while (n > 0) {
+            System.out.println("tick " + n);
+            n--;
+        }
+    }
+}
