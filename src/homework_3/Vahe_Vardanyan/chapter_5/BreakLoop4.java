109751e5494eaee05f7a8d31d8e5e83213b13f92
Vahe <vahe.vardanyan.0808@gmail.com>
2023-01-19 17:43:48 +0400

    Homework_3

---
+++ src/homework_3/Vahe_Vardanyan/chapter_5/BreakLoop4.java
@@ -0,0 +1,15 @@
+package homework_3.Vahe_Vardanyan.chapter_5;
+
+public class BreakLoop4 {
+    public static void main(String[] args) {
+        outer: for (int i = 0; i < 3; i++) {
+            System.out.println("Pass " + i + ": ");
+            for (int j = 0; j < 100; j++) {
+                if (j == 10) break outer;
+                System.out.println(j + " ");
+            }
+            System.out.println("This will not print");
+        }
+        System.out.println("Loops complete.");
+    }
+}
