fe43a617fb0e4336acd9dc8ea6be0a5221b0d28b
Samvel Hakobyan <121867165+Samhakobyan1@users.noreply.github.com>
2023-01-20 11:37:23 +0400

    home_work_3 (#195)
    * home_work_3
    
    * home_work_3
    
    * home_work_3
    
    * home_work_3
    
    * home_work_3
    
    * home_work_3_chapter5 and (tasks-1,2)
    
    * home_work_3_tasks-1,2(new comment corrected)

---
+++ src/homework_3/Samvel_Hakobyan/chapter_5/DoWhile.java
@@ -0,0 +1,12 @@
+package homework_3.Samvel_Hakobyan.chapter_5;
+
+public class DoWhile {
+    public static void main(String[] args) {
+
+        int n = 10;
+        do {
+            System.out.println("tick " + n);
+            n--;
+        } while (n > 0);
+    }
+}
