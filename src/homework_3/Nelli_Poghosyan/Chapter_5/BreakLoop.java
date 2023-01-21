4ca8f10216080463f57bf47753b5ec52e1290508
NelliPoghosyan <poghosyannelli78@gmail.com>
2023-01-21 14:12:04 -0800

    Homework 3:Chapter 5 and Tasks

---
+++ src/homework_3/Nelli_Poghosyan/Chapter_5/BreakLoop.java
@@ -0,0 +1,13 @@
+package homework_3.Nelli_Poghosyan.Chapter_5;
+
+public class BreakLoop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++){
+            if (i ==10) {
+                break;
+            }
+            System.out.println("i: " +i);
+        }
+        System.out.println("Loop complate");
+    }
+}
