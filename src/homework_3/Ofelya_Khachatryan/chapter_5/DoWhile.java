6baf14e5f279f15fb33df8d3e595138aeb0fcc4c
ofelyakhachatryan <ofelya.khachatriyan@gmail.com>
2023-01-22 00:47:37 +0400

    Homework_3 chapter5 and Tasks 1,2,3.

---
+++ src/homework_3/Ofelya_Khachatryan/chapter_5/DoWhile.java
@@ -0,0 +1,12 @@
+package homework_3.Ofelya_Khachatryan.chapter_5;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int n = 10;
+
+        do {
+            System.out.println("tick " + n);
+            n--;
+        } while (n > 0);
+    }
+}
---
+++ src/homework_3/Ofelya_Khachatryan/chapter_5/ForTick.java
@@ -0,0 +1,11 @@
+package homework_3.Ofelya_Khachatryan.chapter_5;
+
+public class ForTick {
+    public static void main(String[] args) {
+        int n;
+
+        for (n = 10; n > 0; n--) {
+            System.out.println("tick " + n);
+        }
+    }
+}
