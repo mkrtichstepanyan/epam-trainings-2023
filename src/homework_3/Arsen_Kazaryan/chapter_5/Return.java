2d23aa327efd84084b77ef8ba75a1e32aae74f15
ArsenKazaryan <akazaran239@gmail.com>
2023-01-22 20:39:33 +0400

    Homework_3_Arsen_Kazaryan

---
+++ src/homework_3/Arsen_Kazaryan/chapter_5/Return.java
@@ -0,0 +1,10 @@
+package homework_3.Arsen_Kazaryan.chapter_5;
+
+public class Return {
+    public static void main(String[] args) {
+        boolean t = true;
+        System.out.println("before the operator return. ");
+        if (t) return;
+        System.out.println("This will not run.");
+    }
+}
