93d8ddfe6d70fac30421ae55459a6b5282ba35e3
Gevorg Arghushyan <mr.argush@gmail.com>
2023-01-22 21:44:19 +0400

    add multiplication table for 10

--- src/homework_3/gevorg_arghushyan/tasks/Task_2.java
+++ src/homework_3/gevorg_arghushyan/tasks/Task_2.java
@@ -48,7 +48,7 @@ public class Task_2 {
         System.out.print("Enter any positive integer: ");
         num = console.nextInt();
         System.out.println("Multiplication Table of " + num);
-        for (int i = 1; i < 10; i++) {
+        for (int i = 1; i <= 10; i++) {
             System.out.println(num + " * " + i + " = " + (num * i));
         }
 
