a67226712797dc10d8297735a537eb83a493de7a
Eduard Eghiazaryan <egiazaryaneduard3@gmail.com>
2023-01-22 20:01:45 +0400

    Homework_3_Eduard_Eghiazaryan

--- src/Eduard_Eghiazaryan/tasks/Task_2.java
+++ src/Eduard_Eghiazaryan/tasks/Task_2.java
@@ -49,18 +49,16 @@ public class Task_2 {
     public static void table() {
         Scanner console = new Scanner(System.in);
         int num;
-        do {
-            System.out.print("Enter any positive integer: ");
-            num = console.nextInt();
-        } while (num <= 0);
-        System.out.println("Multiplication Table of " + num);
 
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(num + " x " + i + " = " + num * i);
+        System.out.print("Enter any positive integer: ");
+        num = console.nextInt();
+
+        System.out.println("Multiplication Table of " + num);
+        for (int i = 1; i < 10; i++) {
+            System.out.println(num + " * " + i + " = " + num * i);
         }
 
     }
-
 
     /* TODO: Question 4
         Write a program to find the factorial value of any number entered through the keyboard.
