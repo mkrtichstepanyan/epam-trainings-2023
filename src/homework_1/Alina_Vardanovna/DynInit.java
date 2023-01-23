57e4cf9a76457d62b563a00f7b94d7f8d9b76113
Aram Mehrabyan <amehrabyan111@gmail.com>
2023-01-23 20:44:08 +0300

    Made corrections

---
+++ src/homework_1/Alina_Vardanovna/DynInit.java
@@ -0,0 +1,13 @@
+package homework_1.Alina_Vardanovna;
+
+public class DynInit {
+
+    public static void main(String[] args) {
+        double a, b, c;
+        a = 3.0;
+        b = 4.0;
+        c = Math.sqrt(a * a + b * b);
+        System.out.println("a-ից և b-ից կախված c-ն = " + " " + c);
+    }
+
+}
