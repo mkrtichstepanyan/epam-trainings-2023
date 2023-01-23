11e4ff266d134a5abc8f8500bfc0ca28269f3ddd
Gohar Hakobyan <hak.goh95@gmail.com>
2023-01-24 02:10:19 +0400

    Wrote examples from Chapter 6 of the book and task about stack.

---
+++ src/homework_4/gohar_hakobyan/Stack.java
@@ -0,0 +1,43 @@
+package homework_4.gohar_hakobyan;
+
+public class Stack {
+    private int[] stack;
+    private int top;
+    private int size;
+
+    //Overloaded Constructor
+    public Stack(int size) {
+        this.size = 10;
+        stack = new int[this.size];
+        top = -1;
+    }
+
+    //Push method
+    public void push(int element) {
+        if (top == size - 1) {
+            System.out.println("The stack is already full!");
+        } else {
+            stack[++top] = element;
+        }
+        if (top == 0.75 * size) {
+            int[] newStack = new int[size * 2];
+            if (size >= 0) {
+                System.arraycopy(stack, 0, newStack, 0, size);
+            }
+            stack = newStack;
+            size *= 2;
+        }
+    }
+
+    //Pop method
+    public Integer pop() {
+        if (top < 0) {
+            System.out.println("The stack is empty!");
+            return null;
+        } else {
+            return stack[top--];
+        }
+    }
+}
+
+
