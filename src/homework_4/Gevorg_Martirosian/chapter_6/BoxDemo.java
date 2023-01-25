909ce05a3c04e99176e4386dfb8ca3be17cc6e15
gevorgmartirosian <gevorg.martirosian27@gmail.com>
2023-01-25 21:52:03 +0400

    created a new files for each class.

--- src/homework_4/Gevorg_Martirosian/chapter_6/BoxDemo.java
+++ src/homework_4/Gevorg_Martirosian/chapter_6/BoxDemo.java
@@ -1,34 +1,5 @@
 package homework_4.Gevorg_Martirosian.chapter_6;
 
-class Box {
-    double width;
-    double height;
-    double depth;
-
-    //this is a default constructor for Box
-
-    public Box() {
-    }
-
-    //this is a constructor for Box
-    public Box(double width, double height, double depth) {
-        this.width = width;
-        this.height = height;
-        this.depth = depth;
-    }
-
-    //method for calculating the volume of the box
-    double volume() {
-        return height * width * depth;
-    }
-
-    public void setDim(double w, double h, double d) {
-        width = w;
-        height = h;
-        depth = d;
-    }
-}
-
 class BoxDemo {
     public static void main(String[] args) {
         Box myBox = new Box();
--- src/homework_4/Gevorg_Martirosian/chapter_6/TestStack.java
+++ src/homework_4/Gevorg_Martirosian/chapter_6/TestStack.java
@@ -1,34 +1,5 @@
 package homework_4.Gevorg_Martirosian.chapter_6;
 
-//this class realises the stack of integer numbers, which can save 10 values
-class Stack {
-    int[] stack = new int[10];
-    int tos;
-
-    //initialise the top of stack
-    Stack() {
-        tos = -1;
-    }
-
-    //pushing element to the stack
-    void push(int item) {
-        if (tos == 9) {
-            System.out.println("stack is already full");
-        } else {
-            stack[++tos] = item;
-        }
-    }
-
-    int pop() {
-        if (tos < 0) {
-            System.out.println("stack is empty");
-            return 0;
-        } else {
-            return stack[tos--];
-        }
-    }
-}
-
 public class TestStack {
     public static void main(String[] args) {
         Stack myStack1 = new Stack();
