c97fa562079e68e3e21e9de397af91ba46e6e9be
DianaMelkonyan84 <dianamelkonyanit@gmail.com>
2023-01-25 19:02:01 +0400

    Add 4a 4b 4c tasks

--- src/homework_4/Diana_Melkonyan/stack/Stack.java
+++ src/homework_4/Diana_Melkonyan/stack/Stack.java
@@ -1,39 +1,47 @@
 package homework_4.Diana_Melkonyan.stack;
 
 public class Stack {
-    int [] array = new int [10];
+    int[] array;
     int index;
-    int size;
 
-    Stack(){
-
+    Stack() {
+        array = new int[2];
         index = -1;
     }
-    Stack(int size ){
-        this.size = size;
-        array= new int[size];
+
+    Stack(int size) {
+
+        array = new int[size];
+        index = -1;
 
     }
 
-    void push(int item){
-        if(index ==9){
-            System.out.println("The stack is already full!");
-        }else {
-            array[++index] = item;
+    void push(int item) {
+
+
+        if (index >= (array.length * 75) / 100) {
+            int[] newArr = new int[array.length * 2];
+            for (int i = 0; i < array.length; i++) {
+                newArr[i] = array[i];
+            }
+            array = newArr;
+
         }
+        array[++index] = item;
     }
 
-    int pop(){
-        if(index<0){
+
+    int pop() {
+        if (index < 0) {
             System.out.println("The stack is empty!");
             return 0;
-        }else {
+        } else {
 
             int carentElement = array[index];
-            array[index]=0;
-                index--;
+            array[index] = 0;
+            index--;
 
-            return carentElement ;
+            return carentElement;
 
 
         }
--- src/homework_4/Diana_Melkonyan/stack/StackTest.java
+++ src/homework_4/Diana_Melkonyan/stack/StackTest.java
@@ -3,15 +3,14 @@ package homework_4.Diana_Melkonyan.stack;
 public class StackTest {
     public static void main(String[] args) {
         Stack mystack1 = new Stack();
-        Stack mystack2 = new Stack();
-        Stack mystack3= new Stack(10);
+
+        Stack mystack3= new Stack(3);
+
 
         for (int i = 1; i < 12; i++) {
             mystack1.push(i);
         }
-        for (int i = 10; i < 21; i++) {
-            mystack2.push(i);
-        }
+
         for (int i = 1; i <10 ; i++) {
             mystack3.push(i);
        }
@@ -21,10 +20,7 @@ public class StackTest {
             System.out.println(mystack1.pop());
         }
 
-        System.out.println("Stack in mystack2:");
-        for (int i = 0; i < 10; i++) {
-            System.out.println(mystack2.pop());
-        }
+
         System.out.println("Stack in mystack3:");
        for (int i = 1; i < 10; i++) {
            System.out.println(mystack3.pop());
