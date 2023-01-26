27ae6e12d27de97bfb8ad35c361f4ac3425f3afc
Karen-Mikayelyan <mikkar89@mail.ru>
2023-01-26 23:33:04 +0400

    added homework_4 - tasks, book examples in Chapter 6

---
+++ src/homework_4/Karen_Mikayelyan/task/Stack.java
@@ -0,0 +1,44 @@
+package homework_4.Karen_Mikayelyan.task;
+
+public class Stack {
+    private int[] stack;
+
+    private int index;
+
+    public Stack() {
+        stack = new int[10];
+        index = -1;
+    }
+
+    public Stack(int size) {
+        stack = new int[size];
+        index = -1;
+    }
+
+    public void push(int item) {
+        if (index == stack.length - 1) {
+            System.out.println("The stack is already full!");
+        } else {
+            stack[++index] = item;
+        }
+    }
+
+    public void doublePush(int item) {
+        if (index > stack.length * 0.75) {
+            int doubleSize = stack.length * 2;
+            int[] doubleStack = new int[doubleSize];
+            System.arraycopy(stack, 0, doubleStack, 0, stack.length);
+            stack = doubleStack;
+        }
+        stack[++index] = item;
+    }
+
+    public int pop() {
+        if (index < 0) {
+            System.out.println("The stack is empty!");
+            return 0;
+        } else {
+            return stack[index--];
+        }
+    }
+}
