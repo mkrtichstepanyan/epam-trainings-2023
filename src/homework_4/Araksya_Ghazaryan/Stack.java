0420568a336b6fd92889ea28e18333a1e96a8715
AraksyaGhazaryan <araksya.ghazaryan.im@gmail.com>
2023-01-28 20:24:44 +0400

    added homework_4.1

---
+++ src/homework_4/Araksya_Ghazaryan/Stack.java
@@ -0,0 +1,56 @@
+package homework_4.Araksya_Ghazaryan;
+
+public class Stack {
+
+    private int[] array = new int[10];
+    private int index;
+    private int length;
+
+    Stack() {
+        index = -1;
+        length = 10;
+    }
+    Stack(int arraySize) {
+        this.length = arraySize;
+    }
+    public void push(int item) {
+        if (index == length -1) {
+            System.out.println("The stack is already full!");
+            extend();
+        } else {
+            array[++index] = item;
+        }
+    }
+    private void extend() {
+        if (index >= length * 75 / 100) {
+            System.out.println("length dynamically increased");
+            int[] temp = new int[length * 2];
+            for (int i = 0; i <= index; i++) {
+                temp[i] = array[i];
+            }
+            array = temp;
+            length = temp.length;
+        }
+    }
+    public Integer pop() {
+        if (index <= -1) {
+            System.out.print("The stack is empty! ");
+            return null;
+        } else {
+            return array[index--];
+        }
+    }
+    public void print() {
+        for (int i = 0; i < index; i++) {
+            System.out.println(array[i] + " ");
+        }
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+}
---
+++ src/homework_4/Araksya_Ghazaryan/StackTest.java
@@ -0,0 +1,57 @@
+package homework_4.Araksya_Ghazaryan;
+
+public class StackTest {
+    public static void main(String[] args) {
+        Stack myStack = new Stack();
+        myStack.push(1);
+        myStack.push(2);
+        myStack.push(3);
+        myStack.push(18);
+        myStack.push(11);
+        myStack.push(15);
+        myStack.push(14);
+        myStack.push(12);
+        myStack.push(12);
+        myStack.push(127);
+        myStack.push(125);
+        myStack.push(172);
+        myStack.push(1132);
+        myStack.push(1288);
+        myStack.pop();
+        myStack.pop();
+        myStack.pop();
+        myStack.print();
+        myStack.push(127);
+        myStack.push(125);
+        myStack.push(172);
+        myStack.push(1132);
+        myStack.push(1288);
+        myStack.pop();
+        myStack.print();
+
+        System.out.println();
+
+        Stack myStach2 = new Stack(6);
+        myStack.push(1);
+        myStack.push(2);
+        myStack.push(3);
+        myStack.push(18);
+        myStack.push(11);
+        myStack.push(15);
+        myStack.push(14);
+        myStack.push(12);
+        myStack.push(12);
+        myStack.push(127);
+        myStack.push(125);
+        myStack.push(172);
+        myStack.push(1132);
+        myStack.push(1288);
+        myStack.pop();
+        myStack.pop();
+        myStack.pop();
+        myStack.print();
+
+        System.out.println(myStack.getIndex());
+
+    }
+}
