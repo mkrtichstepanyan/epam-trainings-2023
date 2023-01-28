3956c253790f56e9cf878883dd53c32f252f27ed
garik <garik.garibyan.1990@gmail.com>
2023-01-28 11:30:54 +0400

    created first 5 class of book

---
+++ src/homework_5/Overload2.java
@@ -0,0 +1,24 @@
+package homework_5;
+class OverloadDemo2{
+    void test(){
+        System.out.println("No parameters");
+    }
+    void test(int a,int b){
+        System.out.println("a and b: " + a +" " + b);
+    }
+    void test(double a){
+        System.out.println("Inside test (double) a: " + a);
+    }
+}
+public class Overload2 {
+    public static void main(String[] args) {
+        OverloadDemo2 ov2 = new OverloadDemo2();
+        int i = 88;
+
+        ov2.test();
+        ov2.test(10,20);
+        ov2.test(i);
+        ov2.test(123.25);
+
+    }
+}
---
+++ src/homework_5/OverloadCons.java
@@ -0,0 +1,48 @@
+package homework_5;
+
+  class Box {
+    double wight;
+    double height;
+    double dept;
+
+    Box(double w, double h, double d) {
+        this.wight = w;
+        this.height = h;
+        this.dept = d;
+    }
+
+    Box() {
+        wight = -1;
+        height = -1;
+        dept = -1;
+    }
+
+    Box(double len) {
+        wight = height = dept = len;
+    }
+
+    double value() {
+        return wight * height * dept;
+    }
+
+}
+    public class OverloadCons {
+        public static void main(String[] args) {
+            Box myBox1 = new Box(10,20,15);
+            Box myBox2 = new Box();
+            Box myCub = new Box(7);
+
+            double vol;
+
+            vol = myBox1.value();
+            System.out.println("Value of myBox1 is " + vol);
+
+            vol = myBox2.value();
+            System.out.println("Value of myBox2 is " + vol);
+
+            vol = myCub.value();
+            System.out.println("Value of myCub is " + vol);
+
+        }
+
+}
