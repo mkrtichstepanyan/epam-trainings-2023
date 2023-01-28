b4ac5fc032cfb694f7048dc06b94f2db12ee97bf
DavitPoghosyan177 <davit.poghosyan1.d@gmail.com>
2023-01-28 01:25:19 -0800

    Fixed Homework_4

--- src/homework_4/Davit_Poghosyan/Stack/Stack_4c.java
+++ src/homework_4/Davit_Poghosyan/Stack/Stack_4c.java
@@ -1,39 +1,42 @@
 package homework_4.Davit_Poghosyan.Stack;
 
 public class Stack_4c {
-        int[] stackArray;
-        int topS=-1;
-        int counter;
-        Stack_4c(int sizeOfStack){
+    int[] stackArray;
+    int topS = -1;
+    int counter;
 
-            stackArray = new int[sizeOfStack];
+    Stack_4c(int sizeOfStack) {
+
+        stackArray = new int[sizeOfStack];
+    }
+
+    public int[] stackCopy(int size) {
+        int[] array = new int[size];
+        for (int i = 0; i < stackArray.length; i++) {
+            array[i] = stackArray[i];
         }
-        public int[] stackCopy(int size){
-            int[] array = new int[size];
-            for(int i = 0; i < stackArray.length; i++){
-                array[i] = stackArray[i];
-            }
-            return  array;
-        }
-        public void push(int k) {
-            if (counter < stackArray.length * 75 / 100) {
-                if (topS == stackArray.length) {
-                    System.out.println("The stack is already full");
-                } else {
-                    stackArray[++topS] = k;
-                    counter++;
-                }
+        return array;
+    }
+
+    public void push(int k) {
+        if (counter >= stackArray.length * 75 / 100) {
+            stackArray = stackCopy(stackArray.length * 2);}
+            if (topS == stackArray.length) {
+                System.out.println("The stack is already full");
             } else {
-                stackArray = stackCopy(stackArray.length*2);
+                stackArray[++topS] = k;
+                counter++;
             }
         }
-        public int pop() {
-            if (topS < 0) {
-                System.out.println("The stack is empty");
-                return 0;
-            } else {
-                return stackArray[topS--];
-            }
+
+
+    public int pop() {
+        if (topS < 0) {
+            System.out.println("The stack is empty");
+            return 0;
+        } else {
+            return stackArray[topS--];
         }
     }
+}
 
