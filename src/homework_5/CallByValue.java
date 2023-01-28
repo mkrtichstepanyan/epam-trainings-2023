671d4d25c32ec721803ace7126d020f3370cb7f2
garik <garik.garibyan.1990@gmail.com>
2023-01-29 01:10:41 +0400

    second commit

---
+++ src/homework_5/CallByValue.java
@@ -0,0 +1,23 @@
+package homework_5;
+
+class Test1 {
+    void meth(int i, int j) {
+        i *= 2;
+        j /= 2;
+    }
+}
+
+public class CallByValue {
+    public static void main(String[] args) {
+
+        Test1 ob = new Test1();
+        int a = 15;
+        int b = 20;
+
+        System.out.println("a and before call: " + a + " " + b);
+
+        ob.meth(a, b);
+
+        System.out.println("a and after call: " + a + " " + b);
+    }
+}
---
+++ src/homework_5/stack/TestStack.java
@@ -0,0 +1,28 @@
+package homework_5.stack;
+
+import homework_5.stack.Stack;
+
+public class TestStack {
+    public static void main(String[] args) {
+        Stack myStack1 = new Stack();
+        Stack myStack2 = new Stack();
+
+        for (int i = 0; i < 10; i++) {
+            myStack1.push(i);
+        }
+        for (int i = 10; i < 20; i++) {
+            myStack2.push(i);
+        }
+
+        System.out.println("Stack in myStack1");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(myStack1.pop());
+        }
+
+        System.out.println("Stack in myStack1");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(myStack2.pop());
+        }
+
+    }
+}
