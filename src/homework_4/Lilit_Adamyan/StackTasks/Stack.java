fd37950e19268e946c3db2d03edc7033cf1cc585
LAdamyan <ladamyan@bk.ru>
2023-01-28 19:35:55 +0400

    Add access modifiers to stack class

--- src/homework_4/Lilit_Adamyan/StackTasks/Stack.java
+++ src/homework_4/Lilit_Adamyan/StackTasks/Stack.java
@@ -3,9 +3,9 @@ package homework_4.Lilit_Adamyan.StackTasks;
 import java.util.Arrays;
 
 public class Stack {
-     int [] stack;
-     int tos;
-     int size = 10;
+    private int [] stack;
+    private int tos;
+    private int size = 10;
 
 //    overloaded constructor
 
@@ -19,14 +19,14 @@ public class Stack {
          stack = new int[10];
          tos = -1;
      }
-     void push(int item){
+    public void push(int item){
          if(tos == size-1){
              System.out.println("Stack is already full!");
          }else
              stack[++tos] = item;
      }
 
-     int pop() {
+     public int pop() {
          if (tos == -1) {
              System.out.println("The stack is empty!");
              return  0;
