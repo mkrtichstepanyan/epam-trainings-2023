800357d0833a534056cbb66ee9c6bc0ceaaa6895
Robert Nazaryan <robertnazaryan0@gmail.com>
2023-01-28 20:22:05 +0400

    homework 4.1

--- src/homework_4/Robert_Nazaryan/TestStack.java
+++ src/homework_4/Robert_Nazaryan/TestStack.java
@@ -6,21 +6,21 @@ public class TestStack {
         Stack stack1 = new Stack();
         Stack stack2 = new Stack(12);
 
-        for (int i = 0; i < stack1.size; i++) {
+        for (int i = 0; i < stack1.setLength(); i++) {
             stack1.push(i);
         }
 
-        for (int i = 0; i < stack2.size; i++) {
+        for (int i = 0; i < stack2.setLength(); i++) {
             stack2.push(i);
         }
 
         System.out.println("stack1:");
-        for (int i = 0; i < stack1.size; i++) {
+        for (int i = 0; i < stack1.setLength(); i++) {
             System.out.println(stack1.pop());
         }
 
         System.out.println("stack2:");
-        for (int i = 0; i < stack2.size; i++) {
+        for (int i = 0; i < stack2.setLength(); i++) {
             System.out.println(stack2.pop());
         }
     }
