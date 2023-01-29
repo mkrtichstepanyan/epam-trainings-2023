e78a0271c42727a1e68b22aa29118e191a7f42fc
ofelyakhachatryan <ofelya.khachatriyan@gmail.com>
2023-01-29 16:56:37 +0400

    Homework_4.1 and chapter7.

---
+++ src/homework_4/Ofelya_Khachatryan/chapter_7/StaticByName.java
@@ -0,0 +1,8 @@
+package homework_4.Ofelya_Khachatryan.chapter_7;
+
+public class StaticByName {
+    public static void main(String[] args) {
+        StaticDemo.callme();
+        System.out.println("b = " + StaticDemo.b);
+    }
+}
