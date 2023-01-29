26de8edb769e499e634aa1c0e78a1925cbf849b7
Gohar Hakobyan <hak.goh95@gmail.com>
2023-01-29 21:45:20 +0400

    Wrote examples from chapter 7 of the book.

---
+++ src/homework_4/gohar_hakobyan/chapter_7/ClassInner.java
@@ -0,0 +1,23 @@
+package homework_4.gohar_hakobyan.chapter_7;
+
+public class ClassInner {
+    public static void main(String[] args) {
+        int outer_x = 100;
+//  doesn't compile
+
+//        void test() {
+//            Inner inner = new Inner();
+//            inner.display();
+//        }
+//
+//        class Inner {
+//            int y =10;
+//        }
+//            void display() {
+//            System.out.println("Output outer x: " + outer_x);
+//        }
+//        void showy(){
+//            System.out.println(y);
+//        }
+    }
+}
---
+++ src/homework_4/gohar_hakobyan/chapter_7/access/Test.java
@@ -0,0 +1,16 @@
+package homework_4.gohar_hakobyan.chapter_7.access;
+
+public class Test {
+    int a;
+    public int b;
+    private int c;
+
+    void setC(int i) {
+        c = i;
+    }
+
+    int getC() {
+        return c;
+
+    }
+}
