7dc84928686d175bb03502d559262e04a00776eb
Sofya Ghazaryan <sofyaghazaryan25@gmail.com>
2023-01-30 01:22:47 +0400

    Homework_4.1. Added examples of chapter 7.

---
+++ src/homework_4/Sofya_Ghazaryan/chapter_7/CallByValue.java
@@ -0,0 +1,13 @@
+package homework_4.Sofya_Ghazaryan.chapter_7;
+
+public class CallByValue {
+    public static void main(String[] args) {
+        Test1 ob = new Test1();
+
+        int a = 15, b = 20;
+        System.out.println("a and b before call: " + a + " " + b);
+
+        ob.meth(a, b);
+        System.out.println("a and b after call: " + a + " " + b);
+    }
+}
