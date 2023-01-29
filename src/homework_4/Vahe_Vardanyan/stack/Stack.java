ceb09527321b4878709e762cc7247c2729802710
Vahe <vahe.vardanyan.0808@gmail.com>
2023-01-29 15:18:12 +0400

    Updeted Stack 4.1

--- src/homework_4/Vahe_Vardanyan/stack/Stack.java
+++ src/homework_4/Vahe_Vardanyan/stack/Stack.java
@@ -1,42 +1,47 @@
 package homework_4.Vahe_Vardanyan.stack;
 
 public class Stack {
-    Integer[] arr;
-    int size;
+    private Integer[] arr;
+    private int size;
 
-    Stack(int capacity) {
+    public Stack(int capacity) {
         this.arr = new Integer[capacity];
     }
 
-    boolean isEmpty() {
+    private  int size(){
+        return  this.size;
+    }
+
+  public boolean isEmpty() {
         return this.size == 0;
     }
 
-    boolean isFully() {
+   public boolean isFully() {
         return this.size == this.arr.length;
     }
 
 
-    void push(int value) {
+    public void push(int value) {
         if (this.isFully()) {
             System.err.println("Error Stacl is Fully " + value);
             return;
-        }else{
-            this.arr[this.size++] = value;
-            refactor();
-
-
         }
+        this.arr[this.size++] = value;
+
+
+
+
     }
 
-    Integer pop() {
+   public Integer pop() {
         if (this.isEmpty()) {
             System.err.println("Error Stack is Empty");
             return null;
         }
         return this.arr[--this.size];
-        }
-    void refactor() {
+    }
+
+   public Integer checkAndIncrease() {
         if (size >= (arr.length * 75) / 100) {
             System.out.println("Stack is reached 75%");
             Integer[] newarr = new Integer[this.arr.length + 2];
@@ -47,6 +52,7 @@ public class Stack {
             this.arr = newarr;
 
         }
+        return null;
 
     }
 }
--- src/homework_4/Vahe_Vardanyan/stack/TestStack.java
+++ src/homework_4/Vahe_Vardanyan/stack/TestStack.java
@@ -2,12 +2,16 @@ package homework_4.Vahe_Vardanyan.stack;
 
 public class TestStack {
     public static void main(String[] args) {
-        Stack stack = new Stack(30);
+        Stack stack = new Stack(10);
 
 
         for (int i = 20; i >= 0; i--) {
             stack.push(i);
-            System.out.println("Push element " + i);
+
+        }
+
+        for (int i = 0; i < 20; i++) {
+            stack.checkAndIncrease();
 
         }
         for (int i = 0; i < 10; i++) {
