a46f87dfed308c3a2113f74cf6b816971d2dde8d
DavitPoghosyan177 <davit.poghosyan1.d@gmail.com>
2023-01-30 09:57:07 -0800

    finished chapter_7

---
+++ src/homework_4_1/Davit_Poghosyan/Chapter_7/Commandline.java
@@ -0,0 +1,9 @@
+package homework_4_1.Davit_Poghosyan.Chapter_7;
+
+class Commandline {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++)
+            System.out.println("args[" + i + "]: " + args[i]);
+        }
+    }
+
---
+++ src/homework_4_1/Davit_Poghosyan/Chapter_7/StaticByName.java
@@ -0,0 +1,8 @@
+package homework_4_1.Davit_Poghosyan.Chapter_7;
+
+class StaticByName {
+    public static void main(String[] args) {
+        StaticDemo.callme();
+        System.out.println("b = " + StaticDemo.b);
+    }
+}
