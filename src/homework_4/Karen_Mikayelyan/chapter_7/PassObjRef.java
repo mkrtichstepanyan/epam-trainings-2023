db61fccd0f34cfd32151c0fd0dba50799636ea15
Karen-Mikayelyan <mikkar89@mail.ru>
2023-01-30 04:54:07 +0400

    added homework_4.1, Chapter 7

---
+++ src/homework_4/Karen_Mikayelyan/chapter_7/PassObjRef.java
@@ -0,0 +1,14 @@
+package homework_4.Karen_Mikayelyan.chapter_7;
+
+public class PassObjRef {
+    public static void main(String[] args) {
+        Test ob = new Test(15, 20);
+
+        System.out.println("ob.a and ob.b before call: "
+                + ob.a + " " + ob.b);
+        ob.meth(ob);
+
+        System.out.println("ob.a and ob.b before call: "
+                + ob.a + " " + ob.b);
+    }
+}
