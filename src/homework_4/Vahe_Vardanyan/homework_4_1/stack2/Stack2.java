2b4351cee00ee691ee1a1f186ef7ecef80fd83ce
Vahe <vahe.vardanyan.0808@gmail.com>
2023-01-30 19:02:33 +0400

    Updeted Stack 4.1

--- src/homework_4/Vahe_Vardanyan/homework_4_1/stack2/Stack2.java
+++ src/homework_4/Vahe_Vardanyan/homework_4_1/stack2/Stack2.java
@@ -1,28 +1,30 @@
 package homework_4.Vahe_Vardanyan.homework_4_1.stack2;
 
 public class Stack2 {
-    private  int length;
-private int[] arr;
-    private int size;
+    private int length=10;
+    private int[] arr = new int[length];
+    private int size = -1;
 
+    public Stack2() {
+    }
 
-   public Stack2(int[] arr){
-        this.arr = arr;
+    public Stack2(int length) {
+        this.length = length;
     }
 
 
-   public boolean isEmpty() {
-        return this.size == 0;
+    public boolean isEmpty() {
+        return size < 0;
     }
 
-   public boolean isFully() {
-        return this.size == this.arr.length;
+    public boolean isFully() {
+        return size == length;
     }
 
-    void push(int value) {
-        if (size == length - 1){
+    public void push(int value) {
+        if (isFully()) {
             System.out.println("The stack is already full!");
-        }else {
+        } else {
             arr[++size] = value;
             if (size >= length * 75 / 100) {
                 extend();
@@ -33,24 +35,27 @@ private int[] arr;
     }
 
 
-   public Integer pop() {
-        if (this.isEmpty()) {
+    public Integer pop() {
+        if (isEmpty()) {
             System.err.println("Error Stack is Empty");
             return null;
-
         }
-        return this.arr[--this.size];
+        return arr[size--];
     }
 
 
-   private void extend() {
-
-            System.out.println("Stack is reached 75%");
-            Integer[] newarr = new Integer[arr.length + 1 * 2];
-            for (int i = 0; i < arr.length; i++) {
-                newarr[i] = arr[i];
-            }
-   }
+    private void extend() {
+        System.out.println("Stack is reached 75%");
+        int [] newarr = new int[arr.length  * 2];
+        for (int i = 0; i < size; i++) {
+            newarr[i] = arr[i];
+        }
+        arr=newarr;
+        length = newarr.length;
 
     }
+}
+
+
+
 
--- src/homework_4/Vahe_Vardanyan/homework_4_1/stack2/TestStack2.java
+++ src/homework_4/Vahe_Vardanyan/homework_4_1/stack2/TestStack2.java
@@ -2,7 +2,7 @@ package homework_4.Vahe_Vardanyan.homework_4_1.stack2;
 
 public class TestStack2 {
     public static void main(String[] args) {
-        Stack2 stack2 = new Stack2(new int[]{20});
+        Stack2 stack2 = new Stack2(20);
         for (int i = 20; i >= 0; i--) {
             stack2.push(i);
         }
