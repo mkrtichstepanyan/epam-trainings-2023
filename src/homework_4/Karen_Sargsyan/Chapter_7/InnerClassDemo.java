46c37854a41b85d549824e639e94c7db6aa7f9a4
karsargsyan2001 <karsargsyan40@gmail.com>
2023-02-01 13:21:36 +0400

    I wrote all examples of Chapter_7 and changed Stack task

---
+++ src/homework_4/Karen_Sargsyan/Chapter_7/InnerClassDemo.java
@@ -0,0 +1,9 @@
+package homework_4.Karen_Sargsyan.Chapter_7;
+
+public class InnerClassDemo {
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+        outer.test();
+    }
+
+}
