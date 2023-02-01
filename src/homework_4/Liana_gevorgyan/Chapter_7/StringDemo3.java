8da95cfc4f3b3417a4e07d34554a34edd755a3a0
Liana_Gevorgyan <liannagevorgyan@gmail.com>
2023-02-01 22:59:24 +0400

    Chapter_6 and Improvement version of Stack class

---
+++ src/homework_4/Liana_gevorgyan/Chapter_7/StringDemo3.java
@@ -0,0 +1,9 @@
+package homework_4.Liana_gevorgyan.Chapter_7;
+//Demonstrate String arrays
+class StringDemo3 {
+    public static void main(String[] args) {
+        String str [] = {"one", "two", "three"};
+        for (int i = 0; i<str.length; i++)
+            System.out.println("str[" + "] :" + str[i]);
+    }
+}
