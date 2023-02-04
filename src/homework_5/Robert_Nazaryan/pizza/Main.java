191525a1ff6582b07df0de6237b6f571234ff3e2
Robert Nazaryan <robertnazaryan0@gmail.com>
2023-02-04 13:36:00 +0400

    pizza homework

--- src/homework_5/Robert_Nazaryan/pizza/Main.java
+++ src/homework_5/Robert_Nazaryan/pizza/Main.java
@@ -3,24 +3,14 @@ package homework_5.Robert_Nazaryan.pizza;
 public class Main {
 
     public static void main(String[] args) {
-
-        Order orderMargarita = new Order("Margarita", "closed", 1);
-        Order orderPepperoniOro = new Order("PepperoniOro", "regular", 3);
-
-        orderPepperoniOro.orderInfo();
+        Pizza pizzaMargarita = new Pizza();
+        pizzaMargarita.addIngredient("Tomato");
+        pizzaMargarita.addIngredient("Pepperoni");
+        pizzaMargarita.addIngredient("Garlic");
+        pizzaMargarita.addIngredient("Cheese");
+        Pizza[] pizzas = new Pizza[]{pizzaMargarita};
+        Order orderMargarita = new Order(new Customer("Margarita"),pizzas, "closed" );
         orderMargarita.orderInfo();
-
-        orderMargarita.addIngredient("tomato");
-        orderMargarita.addIngredient("pepperoni");
-        orderMargarita.addIngredient("garlic");
-        orderMargarita.addIngredient("bacon");
-
-        orderPepperoniOro.addIngredient("Tomato");
-        orderPepperoniOro.addIngredient("Cheese");
-        orderPepperoniOro.addIngredient("Salami");
-        orderPepperoniOro.addIngredient("Olives");
-
         orderMargarita.printCheck();
-        orderPepperoniOro.printCheck();
     }
 }
--- src/homework_5/Robert_Nazaryan/pizza/Pizza.java
+++ src/homework_5/Robert_Nazaryan/pizza/Pizza.java
@@ -1,33 +1,112 @@
 package homework_5.Robert_Nazaryan.pizza;
 
 public class Pizza {
-    private String[] pizza;
-    private double[] price;
-    private int indexPizza, indexPrice;
+
+    private String pizzaType;
+    private String[] ingredients;
+    private double price;
+    private int index;
+    private final double regularPrice = 1;
+    private final double closedPrice = 1.5;
+    private final double tomatoPrice = 1;
+    private final double cheesePrice = 1;
+    private final double salamiPrice = 1.5;
+    private final double baconPrice = 1.2;
+    private final double garlicPrice = 0.3;
+    private final double cornPrice = 0.7;
+    private final double pepperoniPrice = 0.6;
+    private final double olivesPrice = 0.5;
 
     public Pizza() {
-        indexPizza = -1;
-        indexPrice = -1;
-        pizza = new String[8];
-        price = new double[8];
+        price = 0;
+        ingredients = new String[7];
     }
 
-    public void push(String item) {
-        pizza[++indexPizza] = item;
-    }
-    public void push(double p) {
-        price[++indexPrice] = p;
+    public String getPizzaType() {
+        return pizzaType;
     }
 
-    public String popIngredient() {
-        return pizza[indexPizza--];
-    }
-    public double popPrice() {
-        return price[indexPrice--];
+    public void setPizzaType(String pizzaType) {
+        this.pizzaType = pizzaType;
+        if (pizzaType == "closed" || pizzaType == "Closed") {
+            price += closedPrice;
+        } else {
+            price += regularPrice;
+        }
     }
 
+    public void addIngredient(String item) {
+        if (index < 8) {
+            for (int i = 0; i < index; i++) {
+                if (item == ingredients[i]) {
+                    System.out.println("Check the order again!");
+                    return;
+                }
+            }
+            ingredients[index++] = item;
+            switch (item) {
+                case "Tomato":
+                    this.price += tomatoPrice;
+                    break;
+                case "tomato":
+                    this.price += tomatoPrice;
+                    break;
+                case "Cheese":
+                    this.price += cheesePrice;
+                    break;
+                case "cheese":
+                    this.price += cheesePrice;
+                    break;
+                case "Salami":
+                    this.price += salamiPrice;
+                    break;
+                case "salami":
+                    this.price += salamiPrice;
+                    break;
+                case "Bacon":
+                    this.price += baconPrice;
+                    break;
+                case "bacon":
+                    this.price += baconPrice;
+                    break;
+                case "Garlic":
+                    this.price += garlicPrice;
+                    break;
+                case "garlic":
+                    this.price += garlicPrice;
+                    break;
+                case "Corn":
+                    this.price += cornPrice;
+                    break;
+                case "corn":
+                    this.price += cornPrice;
+                    break;
+                case "Pepperoni":
+                    this.price += pepperoniPrice;
+                    break;
+                case "pepperoni":
+                    this.price += pepperoniPrice;
+                    break;
+                case "Olives":
+                    this.price += olivesPrice;
+                    break;
+                case "olives":
+                    this.price += olivesPrice;
+                    break;
+            }
 
-    public int getIndex() {
-        return indexPizza;
+        } else {
+            System.out.println("Pizza is already full!");
+        }
+    }
+
+    public void getIngredients() {
+        for (int i = 0; i < index; i++) {
+            System.out.println(ingredients[i]);
+        }
+    }
+
+    public double getPrice() {
+        return price;
     }
 }
