4fc37a217328270d5802d72297cae4195d0c97fa
Anush-An <anush.ananyanv@gmail.com>
2023-02-05 20:39:56 +0400

    added updated exercises of task 1

--- src/homework_5/Anush_Ananyan/Task_1.java
+++ src/homework_5/Anush_Ananyan/Task_1.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class Task_1 {
 
     public static void main(String[] args) {
-//        printNumbers(10);            // Question 1 -> try with different integer values instead of 10 only
+//        printNumbers(7);            // Question 1 -> try with different integer values instead of 10 only
 //        System.out.println(sumNumbers(10));                // Question 2
 //        System.out.println(factorialDemo1(7));             // Question 3
-//        System.out.println(powerDemo(2, 3));               // Question 4
-//        reverseNumber(5236);                               // Question 5
+//        System.out.println(powerDemo(-2, 3));              // Question 4
+//        reverseNumber(4256);                               // Question 5
 //        System.out.println(testPrime(9));                  // Question 6
     }
 
@@ -18,10 +18,9 @@ public class Task_1 {
         Write a program to print numbers from 1 to given parameter.
     */
     public static void printNumbers(int count) {
-        // Todo write your code here ...
 
-        if (count == 0) {
-            System.out.println(" ");
+        if (count <= 0) {
+            System.out.println("Enter the positive number");
         } else {
             printNumbers(count - 1);
             System.out.println(count);
@@ -34,12 +33,14 @@ public class Task_1 {
     */
     public static int sumNumbers(int number) {
         int sum = 0;
-        // Todo write your code here ...
-        if (number == 1) {
+        if (number <= 0) {
+            System.out.println("Enter the positive number");
+        } else if (number == 1) {
             return sum = 1;
         } else {
-            return sum = number + sumNumbers(number - 1);
+            sum = number + sumNumbers(number - 1);
         }
+        return sum;
     }
 
 
@@ -47,18 +48,11 @@ public class Task_1 {
         Write a program to find the factorial value of any number entered through the keyboard.
     */
     public static int factorialDemo1(int num) {
-        //Scanner console = new Scanner(System.in);
-        // int num; // To hold number
-        int fact; // To hold factorial
 
-        //System.out.print("Enter any positive integer: ");
-        //  num = console.nextInt();
-
-        // Todo write your code here ...
-        if (num == 0 || num == 1)
+        if (num == 0 || num == 1) {
             return 1;
-
-        return fact = num * factorialDemo1(num - 1);
+        }
+        return num * factorialDemo1(num - 1);
     }
 
 
@@ -69,27 +63,12 @@ public class Task_1 {
      */
 
     public static int powerDemo(int base, int power) {
-        // Scanner console = new Scanner(System.in);
 
-        //int base;
-        // int power;
-        int result = 1;
-
-        //System.out.print("Enter the base number ");
-        // base = console.nextInt();
-
-        //  System.out.print("Enter the power ");
-        //  power = console.nextInt();
-
-        // Todo write your code here ...
         if (power == 1) {
-            result = base;
-            return result;
+            return base;
         } else {
-            result = base * powerDemo(base, power - 1);
-            return result;
+            return base * powerDemo(base, power - 1);
         }
-        //System.out.println("Result: " + result);
     }
 
      /* TODO: Question 5
@@ -97,24 +76,19 @@ public class Task_1 {
          For example, if the input is 12345, the output should be 54321.
      */
 
-    public static void reverseNumber(int number) {
-        //Scanner console = new Scanner(System.in);
-
-        // int reverse = 0;
-
-        //System.out.print("Enter the number ");
-        //number = console.nextInt();
-
-        // int temp = number;
-        //int remainder = 0;
-        if (number < 10) {
-            System.out.println(number);
-            return;
+    public static int reverseNumber(int number) {
+        return reverseNumber1(number, 0);
+    }
+    private static int reverseNumber1(int number, int reverse) {
+        int reminder = 0;
+        if (number != 0) {
+            reminder = number % 10;
+            reverse = reverse * 10 + reminder;
+            return (reverseNumber1(number / 10, reverse));
         } else {
-            System.out.print(number % 10);
-            reverseNumber(number / 10);
+            System.out.println(reverse);
+            return 0;
         }
-        // System.out.println("Reverse of " + number + " is " + reverse);
     }
 
 
@@ -136,25 +110,6 @@ public class Task_1 {
         } else {
             return (prime(number, num+1));
         }
-
-        // Todo write your code here ...
-       /* if (number < 2) {
-            flag = false;
-            System.out.println("the " + number + " is not a prime");
-            return;
-        } else {
-            flag = true;
-            if (number % divisor == 0) {
-                flag = false;
-                System.out.println("the " + number + " is not a prime");
-                return;
-            }
-            System.out.println("the " + number + " is a prime");
-        }
-        if (divisor * divisor < number) {
-            divisor++;
-            testPrime(number, divisor);
-        }*/
     }
 }
 
