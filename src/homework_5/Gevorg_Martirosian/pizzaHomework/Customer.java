de35f691247286b3e2fb6a1d8f48b1a7e8bb9018
gevorgmartirosian <gevorg.martirosian27@gmail.com>
2023-02-10 00:35:27 +0400

    fixed problems in pizza homework

---
+++ src/homework_5/Gevorg_Martirosian/pizzaHomework/Customer.java
@@ -0,0 +1,16 @@
+package homework_5.Gevorg_Martirosian.pizzaHomework;
+
+public class Customer {
+    public String name;
+    public String surname;
+    private final int customerNumber = (int)(Math.random() * 9000) + 1000;
+
+    public Customer(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public int getCustomerNumber() {
+        return customerNumber;
+    }
+}
