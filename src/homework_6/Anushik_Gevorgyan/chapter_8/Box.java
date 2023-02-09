6f96d94e564522216f1d9b8f9e526cff6c4aaaa6
Anushik04 <anushikgevorgyan19@gmail.com>
2023-02-09 06:36:15 +0400

    I have done  homework 6

---
+++ src/homework_6/Anushik_Gevorgyan/chapter_8/Box.java
@@ -0,0 +1,33 @@
+package homework_6.Anushik_Gevorgyan.chapter_8;
+
+class Box {
+    double width;
+    double height;
+    double depth;
+
+    Box(Box ob) {
+        width = ob.width;
+        height = ob.height;
+        depth = ob.depth;
+    }
+
+    Box(double w, double h, double d) {
+        width = w;
+        height = h;
+        depth = d;
+    }
+
+    Box() {
+        width = -1;
+        height = -1;
+        depth = -1;
+    }
+
+    Box(double len) {
+        width = height = depth = len;
+    }
+
+    double volume() {
+        return width * height * depth;
+    }
+}
