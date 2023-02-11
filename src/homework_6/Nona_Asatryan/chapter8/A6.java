751b37140f6e240ccefb1b6bf4a9f13a337ef4c9
nona.asatryan <nonadev@gmail.com>
2023-02-11 18:17:55 +0400

    Homework_6_Chapter_8

---
+++ src/homework_6/Nona_Asatryan/chapter8/A6.java
@@ -0,0 +1,9 @@
+package homework_6.Nona_Asatryan.chapter8;
+
+abstract class A6 {
+    abstract void callme();
+
+    void callmetoo() {
+        System.out.println("This is a concrete method.");
+    }
+}
---
+++ src/homework_6/Nona_Asatryan/chapter8/SecondDerivedClass.java
@@ -0,0 +1,6 @@
+package homework_6.Nona_Asatryan.chapter8;
+
+public class SecondDerivedClass extends FirstDerivedClass {
+    int y;
+//    ...
+}
