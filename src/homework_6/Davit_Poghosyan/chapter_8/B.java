b1cf374c15e729ff12c2bf523cf16814fc1ed4a1
DavitPoghosyan177 <davit.poghosyan1.d@gmail.com>
2023-02-12 16:34:23 +0400

    homework_6_Davit_Poghosyan

---
+++ src/homework_6/Davit_Poghosyan/chapter_8/B.java
@@ -0,0 +1,14 @@
+package homework_6.Davit_Poghosyan.chapter_8;
+
+public class B extends A {
+    int k;
+
+    void showk() {
+        System.out.println("k: " + k);
+    }
+
+    void sum() {
+        System.out.println("i + j + k: " + (i + j +k));
+    }
+
+}
