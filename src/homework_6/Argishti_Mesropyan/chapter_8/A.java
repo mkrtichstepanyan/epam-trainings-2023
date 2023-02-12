fb3c64893ba61a85bf23852e7468dc84783100e7
Argisht25 <argoo17@mail.ru>
2023-02-12 20:46:39 +0400

    ads of classes, end package pizza

---
+++ src/homework_6/Argishti_Mesropyan/chapter_8/A.java
@@ -0,0 +1,9 @@
+package homework_6.Argishti_Mesropyan.chapter_8;
+
+public class A {
+    int i;
+    int j;
+    void showij(){
+        System.out.println("i end j " + i + " " + j);
+    }
+}
---
+++ src/homework_6/Argishti_Mesropyan/chapter_8/B.java
@@ -0,0 +1,14 @@
+package homework_6.Argishti_Mesropyan.chapter_8;
+
+public class B extends A {
+    int k;
+
+    void showk() {
+        System.out.println("k : " + k);
+    }
+
+    void sum() {
+        System.out.println("i + j + k: " + (i + j + k));
+    }
+}
+
