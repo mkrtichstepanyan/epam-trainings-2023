ef3d033547914e570f30e52932c61500866bc232
vladvanyan95 <vlovvayan95@gmail.com>
2023-02-13 06:01:34 +0400

    Pizza task complited

--- src/homework_5/Vladimir_Vanyan/Pizza_Task/Order.java
+++ src/homework_5/Vladimir_Vanyan/Pizza_Task/Order.java
@@ -3,60 +3,62 @@ package homework_5.Vladimir_Vanyan.Pizza_Task;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.Scanner;
 
 public class Order {
-    public int orderNumber;
-    public int costomerNumber;
+    private int orderNumber;
+    private Customer customer;
+    private List<Pizza> pizzaList;
     Random r = new Random();
     private int low = 10000;
     private int high = 99999;
 
-    Pizza pizza;
-
-    public Order(Pizza pizza) {
-        this.pizza = pizza;
-        costomerNumber = r.nextInt(high - low) + low;
-        orderNumber=r.nextInt(high-low)+low;
-        calculatePrice();
+    public Order(Customer customer) {
+        orderNumber = r.nextInt(high - low) + low;
+        this.customer = customer;
+        this.pizzaList = new ArrayList<>();
     }
 
-    private void calculatePrice() {
+    public void addPizza(Pizza pizza) {
 
-        if (pizza.type == "Calzone" || pizza.type == "calzone") {
-            pizza.price += 0.5;
+        this.pizzaList.add(pizza);
+    }
+
+    public void displayPizzaAttributes(Pizza pizza) {
+        if (pizza != null) {
+            System.out.println("[Order: " + this.orderNumber + " : " + this.customer.getNumber()
+                    + " : " + pizza.getName() + " : " + pizza.getQuantity() + "]");
         }
-        for (int i = 0; i < pizza.ingredients.size(); i++) {
-            switch (pizza.ingredients.get(i)) {
-                case "Cheese":
-                    pizza.price += 1;
-                    break;
-                case "Salami":
-                    pizza.price += 1.5;
-                    break;
-                case "Bacon":
-                    pizza.price += 1.2;
-                    break;
-                case "Garlic":
-                    pizza.price += 0.3;
-                    break;
-                case "Corn":
-                    pizza.price += 0.7;
-                    break;
-                case "Pepperoni":
-                    pizza.price += 0.6;
-                    break;
-                case "Olives":
-                    pizza.price += 0.5;
-                    break;
+    }
+
+    public void printCheck() {
+        System.out.println("********************************");
+        System.out.println("Order: " + this.orderNumber);
+        System.out.println("Client: " + this.customer.getNumber());
+        double totalAmount = 0.0;
+        totalAmount = getTotalAmount(totalAmount);
+        System.out.println("Total amount: " + totalAmount + " $");
+    }
+
+    private double getTotalAmount(double totalAmount) {
+        for (Pizza pizza : this.pizzaList) {
+            if (pizza == null) {
+                continue;
             }
+            System.out.println("Name: " + pizza.getName());
+            System.out.println("--------------------------------");
+            double basePrice = 1;
+            if (pizza.getType().equals("Calzone")) {
+                basePrice += 0.5;
+            }
+            System.out.println("Pizza Base (" + pizza.getType() + ") " + basePrice + " $");
+            System.out.println("--------------------------------");
+            System.out.println("Amount: " + basePrice + " $");
+            System.out.println("Client: " + this.customer.getNumber());
+            System.out.println("Quantity: " + pizza.getQuantity());
+            System.out.println("--------------------------------");
+            totalAmount += basePrice * pizza.getQuantity();
         }
-    }
-    public void showInfo(){
-     System.out.println("Order "+this.orderNumber);
-        System.out.println("Costomer "+this.costomerNumber);
-        System.out.println("Name "+pizza.costomerName );
-        System.out.println("Quantity "+pizza.quantity);
-        System.out.println("Price "+pizza.price* pizza.quantity);
+        return totalAmount;
     }
 }
+
