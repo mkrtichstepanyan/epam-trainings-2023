b4ee48b94b89ddc94c76bb8eee65fe4a85d93fd0
arzumanyan91 <arzumanyan100991@gmail.com>
2023-02-13 11:10:50 +0400

    Added homework_6.

---
+++ src/homework_6/Galust_Arzumanyan/Shipment.java
@@ -0,0 +1,25 @@
+package homework_6.Galust_Arzumanyan;
+
+class Shipment extends BoxWeight2 {
+    double cost;
+
+    Shipment(Shipment ob) {
+        super(ob);
+        cost = ob.cost;
+    }
+
+    Shipment(double w, double h, double d, double m, double c) {
+        super(w, h, d, m);
+        cost = c;
+    }
+
+    Shipment() {
+        super();
+        cost = -1;
+    }
+
+    Shipment(double len, double m, double c) {
+        super(len, m);
+        cost = c;
+    }
+}
