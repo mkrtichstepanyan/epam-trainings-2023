cd75979e2d1ff75f2cbdc37255aa965efa1c38ce
Ani Kovalenko <ani.kovalenko97@gmail.com>
2023-02-13 13:22:12 +0400

    Added homework 6. Chapter 8

---
+++ src/homework_6/ani_kovalenko/chapter_8/Dispatch/A.java
@@ -0,0 +1,7 @@
+package homework_6.ani_kovalenko.chapter_8.Dispatch;
+
+public class A {
+    void callme(){
+        System.out.println("Inside A`s callme method");
+    }
+}
---
+++ src/homework_6/ani_kovalenko/chapter_8/UseSuper/B.java
@@ -0,0 +1,15 @@
+package homework_6.ani_kovalenko.chapter_8.UseSuper;
+
+public class B extends A {
+    int i;
+
+    B(int a, int b) {
+        super.i = a;
+        i = b;
+    }
+
+    void show() {
+        System.out.println("i in superclass: " + super.i);
+        System.out.println("i in subclass: " + i);
+    }
+}
