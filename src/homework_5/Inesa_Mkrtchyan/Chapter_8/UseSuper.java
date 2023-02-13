06e8d8ceb3947be040d44a3373a06542163e7d7c
Inesa99 <inesamkrtchyan9999@gmail.com>
2023-02-13 14:04:59 +0400

    Homework 5 Inesa Mkrtchyan

--- src/homework_5/Inesa_Mkrtchyan/Chapter_8/UseSuper.java
+++
@@ -1,9 +0,0 @@
-package homework_5.Inesa_Mkrtchyan.Chapter_8;
-
-public class UseSuper {
-    public static void main(String[] args) {
-        B subOb = new B(1, 2);
-        subOb.show();
-    }
-
-}
