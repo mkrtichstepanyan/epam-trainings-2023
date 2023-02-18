d98aaafbe9a1b130179dfd95147ac568852408f2
Alina <alinamkhoyan19@gmail.com>
2023-02-19 01:17:46 +0400

    Homework 7:

---
+++ src/homework_7/Alina_Mkhoyan/pizza/Product.java
@@ -0,0 +1,29 @@
+//package homework_7.Alina_Mkhoyan.pizza;
+//
+//public abstract class Product {
+//    private String name;
+//    private double price;
+//    private int quantity;
+//
+//    public Product(String name, double price, int quantity) {
+//        this.name = name;
+//        this.price = price;
+//        this.quantity = quantity;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public double getPrice() {
+//        return price;
+//    }
+//
+//    public int getQuantity() {
+//        return quantity;
+//    }
+//
+//    public abstract double calculatePrice();
+//
+////    public abstract void printProductDetails();
+//}
