21f8b3b5d95327a3ac95f2fd03e7067cad5fac7f
anna.melikyan94 <anna.melikyan94@gmail.com>
2023-02-18 15:48:45 +0400

    Add logic in Pizza task

---
+++ src/homework_7/Anna_Manukyan/Pizza.java
@@ -0,0 +1,84 @@
+package homework_7.Anna_Manukyan;
+
+import java.util.List;
+
+// is a
+// has a
+public class Pizza {
+    private static final int MAX_ALLOWED_INGREDIENTS = 7;
+    private static final double BASE_COST = 1;
+
+    private String name;
+    private PizzaType pizzaType;
+    private Ingredient[] ingredients;
+    private int quantity;
+
+    Pizza(String name, PizzaType pizzaType, int quantity) {
+        this.name = name;
+        this.pizzaType = pizzaType;
+        this.quantity = quantity;
+    }
+
+    Pizza(String name, PizzaType pizzaType, Ingredient[] ingredients, int quantity) {
+        this.name = name;
+        this.pizzaType = pizzaType;
+        this.ingredients = ingredients;
+        this.quantity = quantity;
+    }
+
+    public double calculatePrice() {
+        double ingredientPrice = 0.0;
+        for (Ingredient ingredient : ingredients) {
+            ingredientPrice += ingredient.getPrice();
+        }
+        return this.pizzaType.getPrice() + ingredientPrice + BASE_COST;
+    }
+
+    public double calculateBasePrice() {
+        return BASE_COST + pizzaType.getPrice();
+    }
+
+    public void addIngredient(Ingredient ingredient) {
+        if (ingredients.length == MAX_ALLOWED_INGREDIENTS) {
+            System.out.println("Pizza is full");
+        } else {
+            isExistInPizza(ingredient);
+        }
+    }
+
+    public static void printPizzaIngredients(Pizza pizza) {
+        Ingredient[] pizzaIngredients = pizza.getIngredients();
+        for (Ingredient ingredient : pizzaIngredients) {
+            System.out.println(ingredient.getName() + " " + ingredient.getPrice() + " €");
+        }
+    }
+
+    private boolean isExistInPizza(Ingredient ingredient) {
+        for (int i = 0; i < ingredients.length; i++) {
+            if (ingredient.equals(ingredients[i])) {
+                System.out.println("This ingredient is already exist!");
+                return false;
+            } else {
+                ingredients[i++] = ingredient;
+            }
+        }
+        return true;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public Ingredient[] getIngredients() {
+        return ingredients;
+    }
+
+    public PizzaType getPizzaType() {
+        return pizzaType;
+    }
+}
+
---
+++ src/homework_7/Anna_Manukyan/Printer.java
@@ -0,0 +1,45 @@
+package homework_7.Anna_Manukyan;
+
+
+import static homework_7.Anna_Manukyan.Pizza.printPizzaIngredients;
+
+public class Printer {
+
+    public static void printCheck(Order order) {
+        leadingLine();
+        System.out.println("Order: " + order.getOrderNumber());
+        System.out.println("Client: " + order.getCustomer().getNumber());
+        double totalSum = 0;
+        for (Pizza pizza : order.getPizzas()) {
+            System.out.println("Name: " + pizza.getName());
+            delimiter();
+            System.out.println("Pizza base (" + pizza.getPizzaType().getName() + ") " + (pizza.calculateBasePrice()) + " €");
+            printPizzaIngredients(pizza);
+            delimiter();
+            double pizzaAmount = pizza.calculatePrice();
+            System.out.println("Amount: " + pizzaAmount + " €");
+            System.out.println("Quantity " + pizza.getQuantity());
+            delimiter();
+            totalSum += pizzaAmount * pizza.getQuantity();
+        }
+        delimiter();
+        System.out.println("Total amount " + totalSum + " €");
+        delimiter();
+        System.out.println("Order time " + order.orderTime.getHour() + ":" + order.orderTime.getMinute());
+        trailingLine();
+    }
+
+    private static void leadingLine() {
+        System.out.println("********************");
+    }
+
+    private static void trailingLine() {
+        System.out.println("********************");
+    }
+
+    private static void delimiter() {
+        System.out.printf("---------------------------------------");
+    }
+
+
+}
