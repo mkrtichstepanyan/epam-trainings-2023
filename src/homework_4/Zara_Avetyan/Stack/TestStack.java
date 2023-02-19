9af01d35e70f4cee7b827e6ce70fe03ba45ae498
Zara Avetyan <zara.avetyan.2004@gmail.com>
2023-02-20 01:01:51 +0400

    Added homework_6_Zara_Avetyan

--- src/homework_4/Zara_Avetyan/Stack/TestStack.java
+++ src/homework_4/Zara_Avetyan/Stack/TestStack.java
@@ -5,34 +5,14 @@ import java.util.List;
 
 public class TestStack {
     public static void main(String[] args) {
-//        int size = 10;
-//        Stack stack = new Stack(size);
-//        for (int i = 0; i < 20; i++) {
-//            stack.push(i);
-//        }
-//
-//        for (int i = 0; i < 20; i++) {
-//            System.out.println("Pop element is: " + stack.pop());
-//        }
-        pop();
-    }
+        int size = 10;
+        Stack stack = new Stack(size);
+        for (int i = 0; i < 20; i++) {
+            stack.push(i);
+        }
 
-    public static void pop() {
-        System.out.println("Vax@ gnalu em atamis!");
-        System.out.println("Vax@ gnalu em atamis!");
-        System.out.println("Vax@ gnalu em atamis!");
-        System.out.println("Vax@ gnalu em atamis!");
-        int push = push();
-        System.out.println(push());
-
-    }
-
-    private static int push() {
-        List list = new ArrayList();
-        System.out.println("xdfcghjk");
-        System.out.println("xdfcghjk");
-        System.out.println("xdfcghjk");
-        System.out.println("xdfcghjk");
-        return 2;
+        for (int i = 0; i < 20; i++) {
+            System.out.println("Pop element is: " + stack.pop());
+        }
     }
 }
