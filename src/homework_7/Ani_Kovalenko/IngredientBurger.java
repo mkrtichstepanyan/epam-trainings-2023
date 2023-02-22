9115ac7d14d9675550293609ed70302fb73100c4
Ani Kovalenko <ani.kovalenko97@gmail.com>
2023-02-23 01:52:02 +0400

    Added Pizza homework with abstract classes

---
+++ src/homework_7/Ani_Kovalenko/IngredientBurger.java
@@ -0,0 +1,9 @@
+package homework_7.Ani_Kovalenko;
+
+public class IngredientBurger extends Ingredient {
+    public static final IngredientBurger CHICKEN_MEAT = new IngredientBurger("Chicken Meat", 0.5);
+    public static final IngredientBurger CHEESE = new IngredientBurger("Cheese", 0.3);
+    IngredientBurger(String name, double price){
+        super(name, price);
+    }
+}
