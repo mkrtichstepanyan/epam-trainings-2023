9a7d0caf6e2b34a1a76004293d896a4676aef385
Anush-An <anush.ananyanv@gmail.com>
2023-02-23 01:01:51 +0400

    Added homework 8, examples from Chapter 9

---
+++ src/homework_8/Anush_Ananyan/Callback.java
@@ -0,0 +1,5 @@
+package homework_8.Anush_Ananyan;
+
+public interface Callback {
+    void callback(int param);
+}
---
+++ src/homework_8/Anush_Ananyan/p2/Protection2.java
@@ -0,0 +1,11 @@
+package homework_8.Anush_Ananyan.p2;
+
+public class Protection2 extends  homework_8.Anush_Ananyan.p1.Protection {
+    Protection2 (){
+        System.out.println("derived other package constructor");
+        //System.out.println("n= " + n);
+        //System.out.println("n_pri = " + n_pri);
+        System.out.println("n_pro = " + n_pro);
+        System.out.println("n_pub = " + n_pub);
+    }
+}
