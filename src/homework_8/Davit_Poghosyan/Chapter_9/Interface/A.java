92ce16844dcc593cc0859067879525fe064274fc
DavitPoghosyan177 <davit.poghosyan1.d@gmail.com>
2023-02-22 17:59:05 +0400

    homework_8.chapter_9

---
+++ src/homework_8/Davit_Poghosyan/Chapter_9/Interface/A.java
@@ -0,0 +1,7 @@
+package homework_8.Davit_Poghosyan.Chapter_9.Interface;
+
+class A {
+    public interface NestedIF{
+        boolean isNotNegative(int x);
+    }
+}
---
+++ src/homework_8/Davit_Poghosyan/Chapter_9/Interface/Incomplete.java
@@ -0,0 +1,11 @@
+package homework_8.Davit_Poghosyan.Chapter_9.Interface;
+
+/*
+public class Incomplete implements Callback{
+    int a , b;
+    void show(){
+        System.out.println(a + " " + b);
+    }
+    //...
+}
+*/
