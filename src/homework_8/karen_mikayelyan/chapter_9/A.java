40539316007ae500273dbb4c6c900ae1d316ee39
Karen-Mikayelyan <mikkar89@mail.ru>
2023-02-23 04:09:21 +0400

    Added homework 8, examples from Chapter 9

---
+++ src/homework_8/karen_mikayelyan/chapter_9/A.java
@@ -0,0 +1,7 @@
+package homework_8.karen_mikayelyan.chapter_9;
+
+public class A {
+    public interface NestedIF {
+        boolean isNotNegative(int x);
+    }
+}
---
+++ src/homework_8/karen_mikayelyan/chapter_9/BInterface.java
@@ -0,0 +1,5 @@
+package homework_8.karen_mikayelyan.chapter_9;
+
+interface BInterface extends AInterface {
+    void meth3();
+}
