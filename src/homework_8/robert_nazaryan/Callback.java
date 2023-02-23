85d05c781be7842eea779a4c576aba250e4fdf7f
Robert Nazaryan <robertnazaryan0@gmail.com>
2023-02-23 12:07:53 +0400

    chapter 9 examples

---
+++ src/homework_8/robert_nazaryan/Callback.java
@@ -0,0 +1,5 @@
+package homework_8.robert_nazaryan;
+
+public interface Callback {
+    void callback (int param);
+}
---
+++ src/homework_8/robert_nazaryan/MyIFImp2.java
@@ -0,0 +1,12 @@
+package homework_8.robert_nazaryan;
+
+public class MyIFImp2 implements MyIF {
+    //Here, implementations for both getNumber() and getString() are provided
+    public int getNumber(){
+        return 100;
+    }
+
+    public String getString() {
+        return "this is a different string.";
+    }
+}
