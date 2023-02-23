c1edf68a66383e0d33d1005ffe3aaaf3c2fc15d9
Hovhannes Abrahamyan <hovhannes.abrahamyan.1995@mail.ru>
2023-02-23 13:30:56 +0400

    errors corrected

--- src/homework_5/Hovhannes_Abrahamyan/Task_1.java
+++ src/homework_5/Hovhannes_Abrahamyan/Task_1.java
@@ -1,40 +1,42 @@
 package homework_5.Hovhannes_Abrahamyan;
 
-public class Task_1 {
+public static void main(String[] args) {
+        printNumbers(10);            // Question 1 -> try with different integer values instead of 10 only
+        //        sumNumbers();                // Question 2
+        //        factorialDemo1();            // Question 3
+        //        powerDemo();                 // Question 4
+        //reverseNumber();             // Question 5
+        //        readSetIntegers();           // Question 6
+        //        testPrime();                 // Question 7
+        }
 
-    public static void main(String[] args) {
-//        printNumbers(8);            // Question 1 -> try with different integer values instead of 10 only
-//        System.out.println(sumNumbers(10));                // Question 2
-//        System.out.println(factorialDemo1(4));             // Question 3
-//        System.out.println(powerDemo(5,4));                // Question 4
-//        System.out.println(reverseNumber(245782));         // Question 5
-//        System.out.println(isPrime(11));                   // Question 6
-    }
+private static void reverseNumber() {
+
+        }
 
 
     /* TODO: Question 1
         Write a program to print numbers from 1 to given parameter.
     */
-    public static void printNumbers(int count) {
-        if (count == 1) {
-            System.out.print(count + " ");
-            return;
+public static void printNumbers(int count) {
+        if (count > 0) {
+        printNumbers(count-1);
+        }
+        System.out.print(count + " ");
         }
 
-        printNumbers(count - 1);
-        System.out.print(count + " ");
-    }
 
     /* TODO: Question 2
         Write a program to calculate the sum of first 10 natural numbers.
     */
 
-    public static int sumNumbers(int number) {
-        if (number == 1) {
-            return 1;
+public static int sumNumbers(int i) {
+        if (i == 1) {
+        return 1;
         }
-        return number + sumNumbers(number - 1);
-    }
+        return i + sumNumbers(i - 1);
+        }
+
 
 
     /* TODO: Question 3
@@ -42,12 +44,12 @@ public class Task_1 {
     */
 //    public static int fact = 1;
 
-    public static int factorialDemo1(int num) {
+public static int factorialDemo1(int num) {
         if (num == 1) {
-            return 1;
+        return 1;
         }
         return num * factorialDemo1(num - 1);
-    }
+        }
 
      /* TODO: Question 4
          Two numbers are entered through the keyboard.
@@ -56,31 +58,31 @@ public class Task_1 {
      */
 
 
-    public static int powerDemo(int num, int pow) {
+public static int powerDemo(int num, int pow) {
         if (pow != 0) {
-            return (num * powerDemo(num, pow - 1));
+        return (num * powerDemo(num, pow - 1));
         } else {
-            return 1;
+        return 1;
         }
-    }
+        }
 
      /* TODO: Question 5
          Write a program that prompts the user to input an integer and then outputs the number with the digits reversed.
          For example, if the input is 12345, the output should be 54321.
      */
 
-    public static int reverseNumber(int number) {
+public static int reverseNumber(int number) {
         return innerReverseNumber(number, 0);
-    }
-
-    private static int innerReverseNumber(int number, int reverse) {
-        if (number == 0) {
-            return reverse;
-        } else {
-            reverse = reverse * 10 + number % 10;
-            return innerReverseNumber(number / 10, reverse);
         }
-    }
+
+private static int innerReverseNumber(int number, int reverse) {
+        if (number == 0) {
+        return reverse;
+        } else {
+        reverse = reverse * 10 + number % 10;
+        return innerReverseNumber(number / 10, reverse);
+        }
+        }
 
 
     /* TODO: Question 6
@@ -88,23 +90,23 @@ public class Task_1 {
         It should then output a message indicating whether the number is a prime number.
     */
 
-    public static boolean isPrime(int number) {
+public static boolean isPrime(int number) {
         return isPrimeInternal(number, 3);
-    }
+        }
 
-    private static boolean isPrimeInternal(int n, int currDivisor) {
+private static boolean isPrimeInternal(int n, int currDivisor) {
         if (n < 2 || n % currDivisor == 0) {
-            return false;
+        return false;
         }
 
         if (n % 2 == 0) {
-            return (n == 2);
+        return (n == 2);
         }
 
         if (currDivisor * currDivisor > n) {
-            return true;
+        return true;
         }
 
         return isPrimeInternal(n, currDivisor + 2);
-    }
-}
+        }
+        }
