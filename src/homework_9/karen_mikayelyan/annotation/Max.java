a0a1a2dce535a7750990b32ae1ca079841c3d6c1
Karen-Mikayelyan <mikkar89@mail.ru>
2023-02-27 02:29:15 +0400

    Added homework_9

---
+++ src/homework_9/karen_mikayelyan/annotation/Max.java
@@ -0,0 +1,14 @@
+package homework_9.karen_mikayelyan.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Max {
+    int value();
+
+    String message() default "Discount rate can not be more then 100";
+}
---
+++ src/homework_9/karen_mikayelyan/dto/CustomerDto.java
@@ -0,0 +1,45 @@
+package homework_9.karen_mikayelyan.dto;
+
+import homework_9.karen_mikayelyan.annotation.*;
+
+import java.time.LocalDate;
+
+public class CustomerDto {
+    @NotNull(message = "Customer name can not be null")
+    @Length(min = 2, max = 30)
+    private String name;
+
+    @NotNull(message = "Customer email can not be null")
+    @Email
+    private String email;
+
+    @Adulthood
+    private LocalDate birthDay;
+
+    @Min(value = 0)
+    @Max(value = 100)
+    private int discountRate;
+
+    public CustomerDto(String name, String email, LocalDate birthDay, int discountRate) {
+        this.name = name;
+        this.email = email;
+        this.birthDay = birthDay;
+        this.discountRate = discountRate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public LocalDate getBirthDay() {
+        return birthDay;
+    }
+
+    public int getDiscountRate() {
+        return discountRate;
+    }
+}
