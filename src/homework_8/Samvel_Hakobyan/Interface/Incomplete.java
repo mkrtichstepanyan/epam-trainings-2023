f0771dde6d024385c3f8fa3a9981cedbb3a60bae
Samvel Hakobyan <121867165+Samhakobyan1@users.noreply.github.com>
2023-02-28 10:34:33 +0400

    homework_8_chapter.9 (#545)
    * homework_8_chapter.9
    
    * Reverted changes on pizza.docx
    
    ---------
    
    Co-authored-by: Varduhi Karapetyan <varduhi_karapetyan@epam.com>

---
+++ src/homework_8/Samvel_Hakobyan/Interface/Incomplete.java
@@ -0,0 +1,9 @@
+package homework_8.Samvel_Hakobyan.Interface;
+
+abstract class Incomplete implements  Callback{
+    int a, b;
+
+    void show(){
+        System.out.println(a + " " + b);
+    }
+}
