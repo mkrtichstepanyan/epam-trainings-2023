cd7fab1477949a5fdcc5eaa3ba789c987ddd49c3
Diana Melkonyan <dianamelkonyanit@gmail.com>
2023-03-01 14:50:28 +0400

    annotation homework

---
+++ src/homework_9/Diana_Melkonyann/CustomerDto.java
@@ -0,0 +1,29 @@
+package homework_9.Diana_Melkonyann;
+
+import homework_9.Diana_Melkonyan.annotations.*;
+
+import java.time.LocalDate;
+
+public class CustomerDto {
+    @Email(email = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:" +
+            "\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+    private String email;
+    @Length(min = 2, max = 30)
+    private String name;
+    @Adulthood()
+    private LocalDate birthDay;
+
+    public String getName() {
+        return name;
+    }
+
+    @Min(0)
+    @Max(100)
+    private int discountRate;
+    public CustomerDto(String email, String name, LocalDate birthDay, int discountRate) {
+        this.email = email;
+        this.name = name;
+        this.birthDay = birthDay;
+        this.discountRate = discountRate;
+    }
+}
