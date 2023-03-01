5919fdbfde962523c74db491c61cf2990d0e5f3d
Diana Melkonyan <dianamelkonyanit@gmail.com>
2023-03-01 12:39:46 +0400

    annotation homework

---
+++ src/homework_9/Diana_Melkonyann/annotations/procesors/EmailAnnotationProcessor.java
@@ -0,0 +1,24 @@
+package homework_9.Diana_Melkonyann.annotations.procesors;
+
+import homework_9.Diana_Melkonyan.annotations.Email;
+
+import java.lang.reflect.Field;
+import java.util.regex.Pattern;
+public class EmailAnnotationProcessor {
+        public static Error processAnnotation(Object customer, Field field) throws IllegalAccessException {
+            Object o = field.get(customer);
+            // pattern matching
+            if (o instanceof String email) {
+
+                Email annotation = field.getAnnotation(Email.class);
+                Pattern regex = Pattern.compile(annotation.email());
+                if (!regex.matcher(email).matches()) {
+                    return new Error("The invalid email", field.getName());
+                }
+            } else {
+                return new Error("Wrong usage error", field.getName());
+            }
+            return null;
+        }
+    }
+
