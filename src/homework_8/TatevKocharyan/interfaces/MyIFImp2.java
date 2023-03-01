c6b3f466a27604f796eb0832b30dc7a9c29d91c9
TatevTatev <fcb.tatev@gmail.com>
2023-03-01 13:17:49 +0400

    homework 8, book examples

---
+++ src/homework_8/TatevKocharyan/interfaces/MyIFImp2.java
@@ -0,0 +1,13 @@
+package homework_8.TatevKocharyan.interfaces;
+
+public class MyIFImp2 implements MyIF {
+    @Override
+    public int getNumber() {
+        return 100;
+    }
+
+    @Override
+    public String getString() {
+        return "This is a different String";
+    }
+}
