c758b9ffcad2df71fa067545143c1066a938dd5d
DianaMelkonyan84 <121571353+DianaMelkonyan84@users.noreply.github.com>
2023-03-01 14:01:09 +0400

    Delete Validator.java
    delete

--- src/homework_9/Diana_Melkonyan/Validator.java
+++
@@ -1,7 +0,0 @@
-package homework_9.Diana_Melkonyan;
-
-import homework_9.Diana_Melkonyan.error.Errors;
-
-public interface Validator {
-    Errors validate(Object obj) throws IllegalAccessException;
-}
