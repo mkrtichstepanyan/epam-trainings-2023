15a533f279f187a682024f8411a7a6e48b9543a8
Anush-An <anush.ananyanv@gmail.com>
2023-03-04 20:27:27 +0400

    added examples of the book

---
+++ src/homework_10/Anush_Ananyan/ChainExcDemo.java
@@ -0,0 +1,20 @@
+package homework_10.Anush_Ananyan;
+
+import homework_9.garik_gharibyan.assignment.annotations.name_annotatuion.NameAnnotationProcessor;
+
+public class ChainExcDemo {
+    static void demoproc() {
+        NullPointerException e = new NullPointerException("top layer");
+        e.initCause(new ArithmeticException("cause"));
+        throw e;
+    }
+
+    public static void main(String[] args) {
+        try {
+            demoproc();
+        } catch (NullPointerException e) {
+            System.out.println("Caught" + e);
+            System.out.println("Original cause: "+ e.getCause());
+        }
+    }
+}
