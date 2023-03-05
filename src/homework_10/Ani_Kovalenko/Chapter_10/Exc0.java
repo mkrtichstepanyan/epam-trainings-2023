b78bfa1ce401aef595aa0d90b6891bdde0e7d082
Ani Kovalenko <ani.kovalenko97@gmail.com>
2023-03-05 19:14:39 +0400

    Added examples from chapter 10

---
+++ src/homework_10/Ani_Kovalenko/Chapter_10/Exc0.java
@@ -0,0 +1,8 @@
+package homework_10.Ani_Kovalenko.Chapter_10;
+
+public class Exc0 {
+    public static void main(String[] args) {
+        int d = 0;
+        int a = 42/d;
+    }
+}
---
+++ src/homework_10/Ani_Kovalenko/Chapter_10/MyException.java
@@ -0,0 +1,29 @@
+package homework_10.Ani_Kovalenko.Chapter_10;
+
+public class MyException extends Exception{
+    private int detail;
+    MyException(int a){
+        detail =  a;
+    }
+    public String toString(){
+        return "MyException["+ detail + "]";
+    }
+}
+class ExceptionDemo{
+    static void compute(int a) throws MyException{
+        System.out.println("Called compute(" + a + ")");
+        if(a>10){
+            throw new MyException(a);
+        }
+        System.out.println("Normal Exit");
+    }
+
+    public static void main(String[] args) {
+        try{
+            compute(1);
+            compute(20);
+        }catch(MyException e){
+            System.out.println("Caught " + e);
+        }
+    }
+}
