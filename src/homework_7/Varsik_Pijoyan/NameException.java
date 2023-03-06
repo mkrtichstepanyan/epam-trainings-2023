926489ee431125f026a88d551c2e32f6e97e090d
Varsik <vpijoyan03@gmail.com>
2023-03-06 13:07:34 +0300

    Added Exceptions in Pizzeria Palmetto homework

--- src/homework_7/Varsik_Pijoyan/NameException.java
+++ src/homework_7/Varsik_Pijoyan/NameException.java
@@ -3,6 +3,6 @@ package homework_7.Varsik_Pijoyan;
 public class NameException extends RuntimeException{
     @Override
     public String toString() {
-        return "Please enter the right name: [A-Z][a-z]+";
+        return "Please enter the right name: [A-Z][a-z]+ ";
     }
 }
