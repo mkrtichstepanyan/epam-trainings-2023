db9f592e448ee3d8eb1df8e9c159ebaeacc7c540
Lyov-Harutyunyan <lyovharutyunian@gmail.com>
2023-03-06 07:47:12 +0400

    Examples of Exception

---
+++ src/homework_10/Levon_Harutyunyan/Exc1.java
@@ -0,0 +1,13 @@
+package homework_10.Levon_Harutyunyan;
+
+public class Exc1 {
+    public static void subroutine() {
+        int d = 0;
+        int a = 10 / d;
+    }
+
+    public static void main(String[] args) {
+        Exc1.subroutine();
+
+    }
+}
