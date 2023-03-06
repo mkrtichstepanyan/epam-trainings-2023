2e16bf9d1c30bae90e88a558e53831e3e4fde82e
Varsik <vpijoyan03@gmail.com>
2023-03-06 16:16:38 +0300

    Added 2 exceptions in Pizza homework.

--- src/homework_7/Varsik_Pijoyan/PhoneNumberException.java
+++ src/homework_7/Varsik_Pijoyan/PhoneNumberException.java
@@ -1,7 +1,6 @@
 package homework_7.Varsik_Pijoyan;
 
 public class PhoneNumberException extends Exception{
-    @Override
     public String toString() {
         return "Please enter the correct format of phone number! ";
     }
