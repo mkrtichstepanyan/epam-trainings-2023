7085736eec083f5770697894ba26fdb0e338db93
Knarik8 <k_armina@mail.ru>
2023-03-06 14:48:24 +0300

    examples from chapter 10

---
+++ src/homework_10/Qnarik_Khachatryan/chapter_10/Exc0.java
@@ -0,0 +1,8 @@
+package homework_10.Qnarik_Khachatryan.chapter_10;
+
+class Exc0 {
+    public static void main(String[] args) {
+        int d = 0;
+        int a = 42 / d;
+    }
+}
---
+++ src/homework_10/Qnarik_Khachatryan/chapter_10/NestTry.java
@@ -0,0 +1,30 @@
+package homework_10.Qnarik_Khachatryan.chapter_10;
+
+//An example of nested try statements.
+
+class NestTry {
+    public static void main(String[] args) {
+        try {
+            int a = args.length;
+
+            /*If no command-line args are present, the following statement will generate a divide-by-zero exception. */
+            int b = 42 / a;
+            System.out.println("a = " + a);
+            try { //nested try block
+                /* If one command-line arg is used , then a divide -by-zero exception will be generated
+                by the following code
+                 */
+
+                /* If two command-line args are used, then generate an out-of-bounds exception. */
+                if (a == 2) {
+                    int[] c = {1};
+                    c[42] = 99; // generate an out-of-bounds exception
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Array index is out-of-bounds: " + e);
+            }
+        } catch (ArithmeticException e) {
+            System.out.println("Divide by 0: " + e);
+        }
+    }
+}
