a60a275d006b33d498c44560ecd19883860acad8
Robert Nazaryan <robertnazaryan0@gmail.com>
2023-03-07 17:35:29 +0400

    Change toStreing to super constructor

--- src/homework_10/robert_nazaryan/exceptionsHomework/exceptions/InvalidPizzaNameException.java
+++ src/homework_10/robert_nazaryan/exceptionsHomework/exceptions/InvalidPizzaNameException.java
@@ -2,8 +2,7 @@ package homework_10.robert_nazaryan.exceptionsHomework.exceptions;
 
 public class InvalidPizzaNameException extends Exception{
 
-    @Override
-    public String toString() {
-        return "Pizza name isn't correct!";
+    public InvalidPizzaNameException(){
+        super("Pizza name isn't correct!");
     }
 }
