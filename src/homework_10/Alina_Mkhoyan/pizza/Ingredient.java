c4a02eb74452751e7b242a5d1441c799757fdd5c
Alina <alinamkhoyan19@gmail.com>
2023-03-07 21:53:20 +0400

    homework 10: Exceptions and chapter_10 examples

---
+++ src/homework_10/Alina_Mkhoyan/pizza/Ingredient.java
@@ -0,0 +1,28 @@
+package homework_10.Alina_Mkhoyan.pizza;
+
+public enum Ingredient {
+    Tomato_paste("Tomato paste", 1),
+    Cheese("Cheese", 1),
+    Salami("Salami", 1),
+    Bacon("Bacon", 1.2),
+    Garlic("Garlic", 0.3),
+    Corn("Corn", 0.7),
+    Pepperoni("Pepperoni", 0.6),
+    Olives("Olives", 0.5);
+
+    Ingredient(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    private final String name;
+    private final double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
---
+++ src/homework_10/Alina_Mkhoyan/pizza/exception/InvalidPriceException.java
@@ -0,0 +1,7 @@
+package homework_10.Alina_Mkhoyan.pizza.exception;
+
+public class InvalidPriceException extends Exception {
+    public InvalidPriceException(String message) {
+        super(message);
+    }
+}
