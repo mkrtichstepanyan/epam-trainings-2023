defd86494c50c0e82595353f847dd68080d041cc
Argisht25 <argoo17@mail.ru>
2023-03-08 17:10:15 +0400

    pizza solution

---
+++ src/homework_7/Argishti_Mesropyan/Customer.java
@@ -0,0 +1,9 @@
+package homework_7.Argishti_Mesropyan;
+
+public class Costumer {
+    private static final int BASE_COSTUMER_NUMBER = 1000;
+    private int customerNumber;
+    private String customerName;
+    private String phoneNumber;
+
+}
---
+++ src/homework_7/Argishti_Mesropyan/Pizza.java
@@ -0,0 +1,2 @@
+package homework_7.Argishti_Mesropyan;public class Pizza {
+}
