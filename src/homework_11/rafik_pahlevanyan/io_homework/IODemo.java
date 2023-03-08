c3df07e53e3cb2bca1e7b997233a0deb8e45425e
rafpahlevanyan <rafpahlevanyan@gmail.com>
2023-03-08 23:14:23 +0400

    solved IO tasks

---
+++ src/homework_11/rafik_pahlevanyan/io_homework/IODemo.java
@@ -0,0 +1,17 @@
+package homework_11.rafik_pahlevanyan.io_homework;
+
+import java.util.List;
+
+public class IODemo {
+    public static final String FILE_PAT = "/D:/javaexample/example.txt";
+
+
+    public static void main(String[] args) {
+        List<String> phoneNumbers = PhoneNumbersCreator.generatePhoneNumbers("093");
+        PhoneNumbersCreator.WriteToFile(phoneNumbers, FILE_PAT);
+
+        FileReadAndWrite.write(FILE_PAT, "hello");
+        System.out.println(FileReadAndWrite.read(FILE_PAT));
+        System.out.println(FileReadAndWrite.processData("hello"));
+    }
+}
