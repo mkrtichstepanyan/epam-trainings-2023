07ed3d7c395713334d1c2887e325e5f3b0b0921d
Manasyanrs <radikmanasyan@gmail.com>
2023-03-12 15:42:18 +0400

    have copied book all book examples and change file path in cass Main

--- src/homework_11/Radik_Manasyan/readAndWriteFiles/Main.java
+++ src/homework_11/Radik_Manasyan/readAndWriteFiles/Main.java
@@ -4,9 +4,9 @@ import homework_11.Radik_Manasyan.phoneNumbers.GeneratePhoneNumbers;
 
 public class Main {
     public static void main(String[] args) {
-        String filePath = "/home/radik/IdeaProjects/epam-trainings-2023/src/homework_11/Radik_Manasyan/readAndWriteFiles/test.txt";
-        String outPath = "/home/radik/IdeaProjects/epam-trainings-2023/src/homework_11/Radik_Manasyan/readAndWriteFiles/copyFile.txt";
-        String phoneNumbers = "/home/radik/IdeaProjects/epam-trainings-2023/src/homework_11/Radik_Manasyan/phoneNumbers/phoneNumbers.txt";
+        String filePath = "src/homework_11/Radik_Manasyan/readAndWriteFiles/test.txt";
+        String outPath = "src/homework_11/Radik_Manasyan/readAndWriteFiles/copyFile.txt";
+        String phoneNumbers = "src/homework_11/Radik_Manasyan/phoneNumbers/phoneNumbers.txt";
 
 
         String text = IO.readFile(filePath);
