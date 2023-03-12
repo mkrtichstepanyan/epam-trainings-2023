262dcf412a9c98a8d5d6aa7df690a2175a87fa8f
roza-petrosyan <petrosyanroza95@gmail.com>
2023-03-12 19:43:38 +0400

    Homework_12 NIO

---
+++ src/homework_12/Roza_Petrosyan/chapter_23/directory_stream/DirList.java
@@ -0,0 +1,33 @@
+package homework_12.Roza_Petrosyan.chapter_23.directory_stream;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class DirList {
+    public static void main(String[] args) {
+        String dirname = "src/homework_12/Roza_Petrosyan/chapter_23/directory_stream";
+
+        try (DirectoryStream<Path> dirstrm =
+                     Files.newDirectoryStream(Path.of(dirname))) {
+            System.out.println("Directory of " + dirname);
+
+            for (Path entry : dirstrm) {
+                BasicFileAttributes attribs =
+                        Files.readAttributes(entry, BasicFileAttributes.class);
+                if (attribs.isDirectory()) {
+                    System.out.println("<DIR> ");
+                } else {
+                    System.out.println("       ");
+                }
+                System.out.println(entry.getName(1));
+            }
+        } catch (InvalidPathException e) {
+            System.out.println("Path Error " + e);
+        } catch (NotDirectoryException e) {
+            System.out.println(dirname + " is not a directory");
+        } catch (IOException e) {
+            System.out.println("I/O Error: " + e);
+        }
+    }
+}
