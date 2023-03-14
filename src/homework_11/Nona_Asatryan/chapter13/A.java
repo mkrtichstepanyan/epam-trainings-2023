de5367449d50a21c1e28a6c5b3410e9e0bfc8406
nona.asatryan <nonadev@gmail.com>
2023-03-14 17:15:43 +0400

    Chapter_13_And_22_Examples_And_IOTasks

---
+++ src/homework_11/Nona_Asatryan/chapter13/A.java
@@ -0,0 +1,7 @@
+package homework_11.Nona_Asatryan.chapter13;
+
+public class A {
+    int i, j;
+}
+
+
---
+++ src/homework_11/Nona_Asatryan/chapter13/B.java
@@ -0,0 +1,5 @@
+package homework_11.Nona_Asatryan.chapter13;
+
+public class B {
+    int i, j;
+}
