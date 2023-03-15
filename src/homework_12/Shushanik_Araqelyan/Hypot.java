5dcb442cf8c572d8b6a3acad16c903caf8e500ae
Shushanik Araqelyan <araqelyanshushan1991@gmail.com>
2023-03-15 20:35:13 +0400

    Shushanik chapter 13

---
+++ src/homework_12/Shushanik_Araqelyan/Hypot.java
@@ -0,0 +1,13 @@
+package homework_12.Shushanik_Araqelyan;
+
+public class Hypot {
+    public static void main(String[] args) {
+        double side1, side2;
+        double hypot;
+        side1 = 3.0;
+        side2 = 4.0;
+        hypot = Math.sqrt(Math.pow(side1, 2) +
+                Math.pow(side2, 2));
+        System.out.println("Given sides of lengths" + side1 + "and" + side2 + "the hypotenuse is" + hypot);
+    }
+}
---
+++ src/homework_12/Shushanik_Araqelyan/PrintWriterDemo.java
@@ -0,0 +1,14 @@
+package homework_12.Shushanik_Araqelyan;
+
+import java.io.PrintWriter;
+
+public class PrintWriterDemo {
+    public static void main(String[] args) {
+        PrintWriter pw = new PrintWriter(System.out, true);
+        pw.println("This is a string");
+        int i = -7;
+        pw.println(i);
+        double d = 4.5e -7;
+        pw.println(d);
+    }
+}
