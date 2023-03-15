528bb6e35b8e9200203588b7b0191e65cff312c5
Karen-Mikayelyan <mikkar89@mail.ru>
2023-03-16 01:01:50 +0400

    Added homework_12

---
+++ src/homework_12/karen_mikayelyan/NioFileProcessor.java
@@ -0,0 +1,44 @@
+package homework_12.karen_mikayelyan;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class NioFileProcessor {
+
+    public static String readFile(String fileName) {
+        if (fileName.endsWith(".txt") || fileName.endsWith(".csv") || fileName.endsWith(".json")) {
+            try {
+                Path path = Paths.get(fileName);
+                byte[] bytes = Files.readAllBytes(path);
+                return new String(bytes, StandardCharsets.UTF_8);
+            } catch (IOException e) {
+                System.err.println("Error: An error occurred while reading file");
+                System.exit(0);
+            }
+        } else return "Unsupported file format";
+        return fileName;
+    }
+
+    public static void writeFile(String filename, String data) {
+        if (filename.endsWith(".txt") || filename.endsWith(".csv") || filename.endsWith(".json")) {
+            try {
+                Path path = Paths.get(filename);
+                Files.writeString(path, data);
+                System.out.println("The data are successfully written!");
+            } catch (IOException e) {
+                System.err.println("Error: An error occurred while writing into file");
+                System.exit(0);
+            }
+        } else {
+            System.err.println("The data format is wrong!!!");
+        }
+    }
+
+
+    public static String processData(String data) {
+        return data.toUpperCase();
+    }
+}
---
+++ src/homework_12/karen_mikayelyan/NioMain.java
@@ -0,0 +1,32 @@
+package homework_12.karen_mikayelyan;
+
+
+import java.util.List;
+
+import static homework_12.karen_mikayelyan.NioPhoneNumberGenerator.generatePhoneNumbers;
+import static homework_12.karen_mikayelyan.NioPhoneNumberGenerator.writeToFile;
+
+
+public class NioMain {
+
+
+    public static void main(String[] args) {
+
+
+        String readFile = "src\\homework_12\\karen_mikayelyan\\newNio.txt";
+        String writeFile = "src\\homework_12\\karen_mikayelyan\\nio.txt";
+        String phoneNumbersFile = "src\\homework_12\\karen_mikayelyan\\NioPhoneNumbers.txt";
+
+
+        String data = NioFileProcessor.readFile(readFile);
+        System.out.println(data);
+
+        String processData = NioFileProcessor.processData(data);
+        System.out.println(processData);
+
+        NioFileProcessor.writeFile(writeFile, processData);
+
+        List<String> phoneNumbers = generatePhoneNumbers("093");
+        writeToFile(phoneNumbers, phoneNumbersFile);
+    }
+}
