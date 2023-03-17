5174b259bdc610e5749ad40ca8cf0c4b24218cf5
Ani Kovalenko <ani.kovalenko97@gmail.com>
2023-03-18 03:22:47 +0400

    Added search tark of 13th homework and visually improved the 11th homework to have less complicated methods

--- src/homework_11/Ani_Kovalenko/task_1/AcceptableFileFormats.java
+++ src/homework_11/Ani_Kovalenko/task_1/AcceptableFileFormats.java
@@ -1,13 +1,19 @@
 package homework_11.Ani_Kovalenko.task_1;
 
 public enum AcceptableFileFormats {
-    TXT(".txt"), CSV(".csv"),JSON(".json");
+
+    TXT(".txt"), CSV(".csv"), JSON(".json");
+
     String fileFormat;
-    AcceptableFileFormats(String fileFormat){
+
+
+    AcceptableFileFormats(String fileFormat) {
         this.fileFormat = fileFormat;
     }
+
 
     public String getFileFormat() {
         return fileFormat;
     }
+
 }
