de54aaa221e69e054de66595e599678a19324af9
Yeghia Ansuryan <yeghia.ansuryan.arm@gmail.com>
2023-03-17 19:05:38 +0400

    Added homework 13 assignments Notepad and FileSearcher

--- src/homework_13/Yeghia_Ansuryan/search/FileSearch.java
+++ src/homework_13/Yeghia_Ansuryan/search/FileSearch.java
@@ -4,13 +4,12 @@ import java.io.File;
 
 public class FileSearch {
     private static int index = 0;
-
-    public static File[] searchFiles(String fileNamePattern, String directory) {
-        if (fileNamePattern == null || fileNamePattern.equals("")) {
+    public static File[] searchFiles(String fileNamePattern, String directory){
+        if(fileNamePattern == null || fileNamePattern.equals("")){
             System.out.println("fileNamePattern can not be null or empty");
             return new File[0];
         }
-        if (directory == null || directory.equals("")) {
+        if (directory == null || directory.equals("")){
             System.out.println("directory name can not be null or empty");
             return new File[0];
         }
@@ -19,34 +18,32 @@ public class FileSearch {
         searchRecursively(currentDirectory, fileNamePattern, result);
         return result;
     }
-
-    private static void searchRecursively(File currentDirectory, String fileNamePattern, File[] result) {
-        if (currentDirectory.isDirectory()) {
+    private static void searchRecursively(File currentDirectory, String fileNamePattern, File[] result){
+        if(currentDirectory.isDirectory()){
             File[] files = currentDirectory.listFiles();
-            if (files != null) {
+            if(files != null){
                 for (File file : files) {
-                    if (file.isDirectory()) {
+                    if(file.isDirectory()){
                         searchRecursively(file, fileNamePattern, result);
-                    } else if (matchesPattern(fileNamePattern, file.getName())) {
+                    }else if (matchesPattern(fileNamePattern, file.getName())){
                         result[index++] = file;
                     }
                 }
             }
         }
     }
-
-    private static boolean matchesPattern(String fileNamePattern, String fileName) {
-        if (!fileNamePattern.contains("*")) {
+    private static boolean matchesPattern(String fileNamePattern, String fileName){
+        if(!fileNamePattern.contains("*")){
             return fileNamePattern.equals(fileName);
-        } else {
+        }else {
             String[] parts = fileNamePattern.split("\\*");
-            if (parts.length == 0) {
+            if(parts.length == 0){
                 return true;
             }
             int startIndex = 0;
-            for (String part : parts) {
+            for(String part : parts){
                 int index = fileName.indexOf(part, startIndex);
-                if (index == -1) {
+                if(index == -1){
                     return false;
                 }
                 startIndex = index + part.length();
