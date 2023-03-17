a3e52bbe361e4495681cba2b1a2d828844f842c8
nona.asatryan <nonadev@gmail.com>
2023-03-17 15:20:01 +0400

    Notepad_And_FileSearcher

---
+++ src/homework_13/Nona_Asatryan/search/FileSearcherDemo.java
@@ -0,0 +1,17 @@
+package homework_13.Nona_Asatryan.search;
+
+import java.io.File;
+
+public class FileSearcherDemo {
+
+    private static final String DIRECTORY = "/Users/nona.asatryan/epam-trainings-2023/src/homework_13/Nona_Asatryan/search/FileSearcher.java";
+
+    public static void main(String[] args) {
+        File[] files = FileSearcher.searchFiles("*test*",DIRECTORY);
+        for (File file : files) {
+            if (file != null){
+                System.out.println(file.getName());
+            }
+        }
+    }
+}
