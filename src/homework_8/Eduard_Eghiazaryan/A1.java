ca9ab2a4ee08ec73b2980e04dc3d5e203a9265eb
Eduard Eghiazaryan <egiazaryaneduard3@gmail.com>
2023-03-18 12:40:55 +0400

    homework_8_eduard_eghiazaryan

---
+++ src/homework_8/Eduard_Eghiazaryan/A1.java
@@ -0,0 +1,8 @@
+package homework_8.Eduard_Eghiazaryan;
+
+public interface A1 {
+    void meth1();
+    void meth2();
+}
+
+
---
+++ src/homework_8/Eduard_Eghiazaryan/mypack/TestBalance.java
@@ -0,0 +1,14 @@
+package homework_8.Eduard_Eghiazaryan.mypack;
+
+public class TestBalance {
+    public static void main(String[] args) {
+
+        /*
+            Because Balance is public, you may use Balance
+            class and call its constructor
+         */
+        Balance test = new Balance("J. J. Jasper's", 99.88);
+
+        test.show();//you may also call show()
+    }
+}
