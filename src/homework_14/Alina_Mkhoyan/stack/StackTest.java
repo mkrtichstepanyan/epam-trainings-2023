1165cbfbb7f7204ed600ae24fff1e53ff05fcfb3
Alina <alinamkhoyan19@gmail.com>
2023-03-21 20:51:40 +0400

    homework 14: generic with stack

--- src/homework_14/Alina_Mkhoyan/stack/StackTest.java
+++ src/homework_14/Alina_Mkhoyan/stack/StackTest.java
@@ -6,9 +6,16 @@ public class StackTest {
         Stack<String> stack = new Stack<>(6);
         System.out.println(stack.push("Alina"));
         System.out.println(stack.push("maybe"));
-        System.out.println(stack.push("1"));
+        System.out.println(stack.push(String.valueOf(4)));
+        System.out.println(stack.push("@"));
+        System.out.println(stack.push(String.valueOf(true)));
+
         System.out.println(stack.pop());
         System.out.println(stack.pop());
         System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+
     }
 }
