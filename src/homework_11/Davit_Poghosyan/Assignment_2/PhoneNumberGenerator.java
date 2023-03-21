0a3c3541f21a821e10858501d4fadff06fe37a7a
DavitPoghosyan177 <davit.poghosyan1.d@gmail.com>
2023-03-22 00:38:51 +0400

    Homework_11

---
+++ src/homework_11/Davit_Poghosyan/Assignment_2/PhoneNumberGenerator.java
@@ -0,0 +1,37 @@
+package homework_11.Davit_Poghosyan.Assignment_2;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+public class PhoneNumberGenerator {
+
+    public static String[] generatePhoneNumbers(String code) {
+        String[] numbers = new String[1_000_000];
+        NumberFormat format = new DecimalFormat("000000");
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = code + format.format(i);
+        }
+        return numbers;
+
+    }
+
+    public static void writePhoneNumbers(String[] numbers, String fileName) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
+            for (String number : numbers) {
+                bufferedWriter.write(number);
+                bufferedWriter.write("\n");
+            }
+        } catch (IOException e) {
+            System.out.println("I/O error" + e);
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] numbers = generatePhoneNumbers("094");
+        String fileName = "C:\\Users\\Davit\\Desktop\\Davit_Epam\\epam-trainings-2023\\src\\homework_11\\Davit_Poghosyan\\Assignment_2\\PhoneNumber.txt";
+        writePhoneNumbers(numbers, fileName);
+    }
+}
