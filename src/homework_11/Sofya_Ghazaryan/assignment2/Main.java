877a8eb0bdf0e1056d0840c063c52c69eb7dbdaf
Sofya Ghazaryan <sofyaghazaryan25@gmail.com>
2023-03-23 02:38:47 +0400

    Homework 11. Assignment 1 & 2

---
+++ src/homework_11/Sofya_Ghazaryan/assignment2/Main.java
@@ -0,0 +1,9 @@
+package homework_11.Sofya_Ghazaryan.assignment2;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] numbers = PhoneNumberGenerator.generatePhoneNumbers("077");
+        String fileName = "C:\\Users\\Hp\\Desktop\\RD\\epam-trainings-2023\\src\\homework_11\\test.txt";
+        PhoneNumberGenerator.writeInFile(numbers, fileName);
+    }
+}
