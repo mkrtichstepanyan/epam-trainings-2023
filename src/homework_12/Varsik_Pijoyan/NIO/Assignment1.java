56b09dbdb34164c5db3e9da5667576f87b670e45
varsikpijoyan <vpijoyan03@gmail.com>
2023-03-23 15:34:02 +0400

    Added NIO tasks

---
+++ src/homework_12/Varsik_Pijoyan/NIO/Assignment1.java
@@ -0,0 +1,62 @@
+package homework_12.Varsik_Pijoyan.NIO;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class Assignment1 {
+    private static String readFile(String fileName) {
+        if (isSupportedFormat(fileName)) {
+            ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+            try (FileChannel channel = FileChannel.open(Paths.get(fileName), StandardOpenOption.READ)) {
+                channel.read(readBuffer);
+                readBuffer.flip();
+                String content = new String(readBuffer.array()).trim();
+                return content;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+
+    private static void writeFile(String fileName, String data) {
+        if (isSupportedFormat(fileName)) {
+            ByteBuffer buffer = ByteBuffer.wrap(data.getBytes());
+            try (FileChannel channel = FileChannel.open(Paths.get(fileName),
+                    StandardOpenOption.CREATE, StandardOpenOption.WRITE)) {
+                channel.write(buffer);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static String processData(String data) {
+        if (data != null) {
+            return data.toUpperCase();
+        }
+        return "No data for modification";
+    }
+
+    private static boolean isSupportedFormat(String filename) {
+        if (filename.endsWith(".txt") || filename.endsWith(".csv") || filename.endsWith(".json")) {
+            return true;
+        } else {
+            System.out.println("Specified format does not supported by program");
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        String data = readFile("src/homework_12/Varsik_Pijoyan/NIO/" +
+                "file/test.txt");
+        String modifiedData = processData(data);
+        writeFile("src/homework_12/Varsik_Pijoyan/NIO/" +
+                "file/test.txt", modifiedData);
+    }
+
+}
---
+++ src/homework_12/Varsik_Pijoyan/NIO/Assignment2.java
@@ -0,0 +1,39 @@
+package homework_12.Varsik_Pijoyan.NIO;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+public class Assignment2 {
+    public String[] generatePhoneNumbers(String code) {
+        String[] numbers = new String[1000000];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = code + String.format("%06d", i) + "\n";
+        }
+        return numbers;
+    }
+
+    public void writeToFile(String[] phoneNumbers, String fileName) {
+        try (FileChannel fileChannel = FileChannel.open(Path.of(fileName),
+                StandardOpenOption.CREATE, StandardOpenOption.WRITE)) {
+            for (String phoneNumber : phoneNumbers) {
+                byte[] byteArray = phoneNumber.getBytes();
+                ByteBuffer bf = ByteBuffer.wrap(byteArray);
+                fileChannel.write(bf);
+            }
+        } catch (IOException e) {
+            System.out.println("An I/O Error Occurred: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        String fileName = "src/homework_12/Varsik_Pijoyan/NIO/file/phoneNumber.txt";
+
+        Assignment2 phoneNumbers1 = new Assignment2();
+        String[] phoneNumbers2 = phoneNumbers1.generatePhoneNumbers("093");
+        phoneNumbers1.writeToFile(phoneNumbers2, fileName);
+    }
+
+}
