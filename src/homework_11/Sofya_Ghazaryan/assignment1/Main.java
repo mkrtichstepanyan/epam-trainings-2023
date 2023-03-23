a92f5e57575a540b81229d56be11defebd3cf3d6
Emma Khachatryan <emma.khachatryan@ukg.com>
2023-03-23 15:19:05 +0400

    Added several changes.

--- src/homework_11/Sofya_Ghazaryan/assignment1/Main.java
+++ src/homework_11/Sofya_Ghazaryan/assignment1/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
 
         String fileName = "C:\\Users\\Hp\\Desktop\\RD\\epam-trainings-2023\\src\\homework_11\\test.txt";
-        ReadAndWriteFiles.readFile(fileName);
         ReadAndWriteFiles.writeFile(fileName, "Hello");
+        ReadAndWriteFiles.readFile(fileName);
 //        ioFile.processData("ascdasc");
     }
 }
--- src/homework_11/Sofya_Ghazaryan/assignment2/Main.java
+++ src/homework_11/Sofya_Ghazaryan/assignment2/Main.java
@@ -3,7 +3,7 @@ package homework_11.Sofya_Ghazaryan.assignment2;
 public class Main {
     public static void main(String[] args) {
         String[] numbers = PhoneNumberGenerator.generatePhoneNumbers("077");
-        String fileName = "C:\\Users\\Hp\\Desktop\\RD\\epam-trainings-2023\\src\\homework_11\\test.txt";
+        String fileName = "C:\\Users\\Hp\\Desktop\\RD\\epam-trainings-2023\\src\\homework_11\\phoneNumberGenerator.txt";
         PhoneNumberGenerator.writeInFile(numbers, fileName);
     }
 }
