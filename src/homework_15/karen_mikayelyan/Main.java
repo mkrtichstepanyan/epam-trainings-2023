d1b0137e8fb1302923dadc61142922d499484849
Karen-Mikayelyan <mikkar89@mail.ru>
2023-03-26 00:45:25 +0400

    Added homework_15 Sort

---
+++ src/homework_15/karen_mikayelyan/Main.java
@@ -0,0 +1,17 @@
+package homework_15.karen_mikayelyan;
+
+import static homework_15.karen_mikayelyan.BubbleSort.printArray;
+import static homework_15.karen_mikayelyan.BubbleSort.sortBubble;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        int[] array = {7, 5, -1, 2, 8, 9, 3, 4, 1, 0, 6};
+
+//        printArray(sortBubble(array));
+//        printArray(SelectionSort.sortSelection(array));
+        printArray(InsertionSort.sortInsertion(array));
+
+    }
+}
