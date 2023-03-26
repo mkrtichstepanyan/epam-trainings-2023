1095bb0bce2e7b79ace32cd316360d7cd2d86e63
Anush-An <anush.ananyanv@gmail.com>
2023-03-26 13:49:03 +0400

    corrected homework_14

--- src/homework_14/Anush_Ananyan/Stack/Stack.java
+++ src/homework_14/Anush_Ananyan/Stack/Stack.java
@@ -1,7 +1,7 @@
 package homework_14.Anush_Ananyan.Stack;
 
 public class Stack<T> {
-    private Object[] stack;
+    private T[] stack;
     private int tos;
 
 
@@ -12,7 +12,7 @@ public class Stack<T> {
 
     public void push(T item) {
         if (tos == stack.length * 75 / 100) {
-            extendVolume();
+            extendVolume(item);
         }
         stack[++tos] = item;
     }
@@ -26,12 +26,17 @@ public class Stack<T> {
         }
     }
 
-    private void extendVolume() {
-        Object[] newStack = new Object[stack.length * 2];
-        for (int i = 0; i < stack.length; i++) {
-            newStack[i] = stack[i];
-        }
-        stack = newStack;
-    }
+//    private void extendVolume() {
+//        Object[] newStack = new Object[stack.length * 2];
+//        for (int i = 0; i < stack.length; i++) {
+//            newStack[i] = stack[i];
+//        }
+//        stack =(T[]) newStack;
+//    }
 
+    private void extendVolume(T item) {
+        Object[] newStack = new Object[stack.length * 2];
+        System.arraycopy(stack, 0, newStack, 0, stack.length);
+        stack = (T[]) newStack;
+    }
 }
--- src/homework_14/Anush_Ananyan/Stack/TestStack.java
+++ src/homework_14/Anush_Ananyan/Stack/TestStack.java
@@ -3,6 +3,7 @@ package homework_14.Anush_Ananyan.Stack;
 public class TestStack {
     public static void main(String[] args) {
         Stack<Integer> mystack1 = new Stack<Integer>(6);
+        Stack<Character> mystack2 = new Stack<Character>(2);
 
         mystack1.push(1);
         mystack1.push(2);
@@ -12,14 +13,23 @@ public class TestStack {
         mystack1.push(6);
         mystack1.push(7);
 
+        System.out.print(mystack1.pop() + " ");
+        System.out.print(mystack1.pop() + " ");
+        System.out.print(mystack1.pop() + " ");
+        System.out.print(mystack1.pop() + " ");
+        System.out.print(mystack1.pop() + " ");
+        System.out.print(mystack1.pop() + " ");
+        System.out.print(mystack1.pop() + " ");
 
-        System.out.print(mystack1.pop() +" ");
-        System.out.print(mystack1.pop() +" ");
-        System.out.print(mystack1.pop() +" ");
-        System.out.print(mystack1.pop() +" ");
-        System.out.print(mystack1.pop() +" ");
-        System.out.print(mystack1.pop() +" ");
-        System.out.print(mystack1.pop() +" ");
+
+        mystack2.push('a');
+        mystack2.push('b');
+        mystack2.push('c');
+        System.out.println();
+
+        System.out.print(mystack2.pop() + " ");
+        System.out.print(mystack2.pop() + " ");
+        System.out.print(mystack2.pop() + " ");
 
     }
 }
