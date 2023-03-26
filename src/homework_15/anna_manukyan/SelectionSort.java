6c5b088028b273f0af83fc52a1b7739af73fd15d
anna.melikyan94 <anna.melikyan94@gmail.com>
2023-03-26 11:22:57 +0400

    homework 15 (selection and insertion sorts)

---
+++ src/homework_15/anna_manukyan/SelectionSort.java
@@ -0,0 +1,34 @@
+package homework_15.anna_manukyan;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 9, 6, 2, 7, 4};
+        printArray(sortArray(arr));
+    }
+
+    static int[] sortArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int min_id = i;
+            for (int j = i +1; j < arr.length; j++) {
+                if (arr[j] < arr[min_id]) {
+                    min_id = j;
+                }
+                swap(arr, i, min_id);
+            }
+        }
+        return arr;
+    }
+
+    private static void swap(int[] arr, int index1, int index2) {
+        int tmp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = tmp;
+
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + ", ");
+        }
+    }
+}
