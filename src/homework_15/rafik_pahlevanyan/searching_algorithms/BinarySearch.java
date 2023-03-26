9f5452f1df0f46a419a28ab62865abf67aa28e23
rafpahlevanyan <rafpahlevanyan@gmail.com>
2023-03-26 12:00:53 +0400

    added searching and sorting algorithms

---
+++ src/homework_15/rafik_pahlevanyan/searching_algorithms/BinarySearch.java
@@ -0,0 +1,32 @@
+package homework_15.rafik_pahlevanyan.searching_algorithms;
+
+public class BinarySearch {
+    
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+    
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int target = 6;
+        int index = binarySearch(arr, target);
+        if (index == -1) {
+            System.out.println("Target not found");
+        } else {
+            System.out.println("Target found at index " + index);
+        }
+    }
+}
---
+++ src/homework_15/rafik_pahlevanyan/sorting_algorithms/HeapSort.java
@@ -0,0 +1,50 @@
+package homework_15.rafik_pahlevanyan.sorting_algorithms;
+
+import java.util.Arrays;
+
+public class HeapSort {
+    
+    public static void heapSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        for (int i = n - 1; i > 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            
+            heapify(arr, i, 0);
+        }
+    }
+    
+    public static void heapify(int[] arr, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        
+        if (left < n && arr[left] > arr[largest]) {
+            largest = left;
+        }
+        
+        if (right < n && arr[right] > arr[largest]) {
+            largest = right;
+        }
+        
+        if (largest != i) {
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+            
+            heapify(arr, n, largest);
+        }
+    }
+    
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 3, 6, 1, 8, 7, 4};
+        heapSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
