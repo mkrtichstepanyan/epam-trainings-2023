0dc09400145d88559c654d50738ab69deb35e301
ofelyakhachatryan <ofelya.khachatriyan@gmail.com>
2023-03-26 22:12:12 +0400

    homework_15 bubble, selection, insertion sorts.

---
+++ src/homework_15/ofelya_khachatryan/BubbleSort.java
@@ -0,0 +1,38 @@
+package homework_15.ofelya_khachatryan;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] array = {0, 1, -1};
+        printArray(sort(array));
+
+        printArray(array);
+    }
+
+    public static int[] sort(int[] array) {
+        int count = 0;
+
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                count++;
+                swap(array, j);
+            }
+        }
+        System.out.println("count -> " + count);
+        return array;
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + ", ");
+        }
+    }
+
+    private static void swap(int[] array, int index) {
+        if (array[index] > array[index + 1]) {
+            int temp = array[index];
+            array[index] = array[index + 1];
+            array[index + 1] = temp;
+        }
+    }
+
+}
