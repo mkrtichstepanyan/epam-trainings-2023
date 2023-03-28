67e4627f08657c080aee6f5e5a95480a2d32ed4b
Vardanyan0808 <121484788+Vardanyan0808@users.noreply.github.com>
2023-03-28 12:52:42 +0400

    Delete B.java

--- src/homework_8/Vahe_Vardanyan/nesedInterface/B.java
+++
@@ -1,9 +0,0 @@
-package homework_8.Vahe_Vardanyan.nesedInterface;
-
-public class B implements A.NestedF {
-
-    @Override
-    public boolean inNotNegative(int x) {
-        return x < 0 ? false: true;
-    }
-}
