14afffd8e264afc91632d7bb0137979434a9501b
Vardanyan0808 <121484788+Vardanyan0808@users.noreply.github.com>
2023-03-28 12:47:25 +0400

    Delete TestIface.java

--- src/homework_8/Vahe_Vardanyan/Interface/TestIface.java
+++
@@ -1,8 +0,0 @@
-package homework_8.Vahe_Vardanyan.Interface;
-
-public class TestIface {
-    public static void main(String[] args) {
-        Callback c = new Client();
-        c.callback(42);
-    }
-}
