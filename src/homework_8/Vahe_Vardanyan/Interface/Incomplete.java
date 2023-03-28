48ce2c3b4a05cdeda476a19809f8bc78467e2340
Vardanyan0808 <121484788+Vardanyan0808@users.noreply.github.com>
2023-03-28 12:47:13 +0400

    Delete Incomplete.java

--- src/homework_8/Vahe_Vardanyan/Interface/Incomplete.java
+++
@@ -1,9 +0,0 @@
-package homework_8.Vahe_Vardanyan.Interface;
-
-abstract class Incomplete implements Callback {
-    int a, b;
-
-    void show(){
-        System.out.println(a + " " + b);
-    }
-}
