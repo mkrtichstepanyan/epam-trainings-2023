c9f6aa18fe483d60d4be1e692a23be343aa178b4
Knarik8 <k_armina@mail.ru>
2023-03-30 00:03:41 +0300

    added MergeSort

--- src/homework_15/Qnarik_Khachatryan/SortsDemo.java
+++ src/homework_15/Qnarik_Khachatryan/SortsDemo.java
@@ -3,15 +3,17 @@ package homework_15.Qnarik_Khachatryan;
 import static homework_15.Qnarik_Khachatryan.BubbleSort.bubbleSort;
 import static homework_15.Qnarik_Khachatryan.SelectionSort.selectionSort;
 import static homework_15.Qnarik_Khachatryan.InsertionSort.insertionSort;
+import static homework_15.Qnarik_Khachatryan.MergeSort.mergeSort;
 
 public class SortsDemo {
     public static void main(String[] args) {
 
 
-        int[] arrayToSort = {21, 10, 1, -15, 2, 5, 55};
+        int[] arrayToSort = {-3, 21, 10, 1, -15, 2, 5, 55};
 //        printArray(bubbleSort(arrayToSort));
 //        printArray(selectionSort(arrayToSort));
-        printArray(insertionSort(arrayToSort));
+//        printArray(insertionSort(arrayToSort));
+        printArray(mergeSort(arrayToSort));
 
 
     }
