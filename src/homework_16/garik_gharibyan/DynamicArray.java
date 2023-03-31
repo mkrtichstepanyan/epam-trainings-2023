5fe3f2c0dfaf7e832bbd32d6e35575df3d8fc2ab
garik <graik.garibyan.1990@gmail.com>
2023-03-31 23:11:26 +0400

    DynamicArray

---
+++ src/homework_16/garik_gharibyan/DynamicArray.java
@@ -0,0 +1,254 @@
+package homework_16.garik_gharibyan;
+
+import homework_15.garik_gharibyan.Sort;
+
+import javax.naming.OperationNotSupportedException;
+import java.util.ArrayList;
+
+/* Please create methods below
++   addAll() -> adds all elements of a collection to arraylist
++   clear() -> removes all the elements from arraylist
++   clone() -> makes a copy of the array list
++   contains() -> checks if the element is present in the arraylist
++   containsAll() -> checks if a collection is a subset of arraylist
++   indexOf() -> returns the position of the specified element
++   remove() -> removes the single element from the arraylist
++   removeAll() -> removes multiple elements from the arraylist
++   isEmpty() -> checks if the arraylist is empty
++   subList() -> returns a portion of the arraylist
++   set() -> replace the single element from an arraylist
++   sort() -> sorts the arraylist according to specified order -> use one of our previous algorithms (bubble, insertion ...)
++   toString() -> converts the arraylist into a String
++   trimToSize() -> trims the capacity of arraylist equal to the size
++   removeRange() -> removes a portion of the arraylist
++   replaceAll() -> replace all elements from the arraylist
+ */
+public class DynamicArray {
+
+    private int[] array;
+
+    private int size = 0;
+
+    private static final int DEFAULT_CAPACITY = 16;
+
+
+    public DynamicArray(int capacity) {
+        if (capacity > 0) {
+            this.array = new int[capacity];
+        } else {
+            throw new IllegalArgumentException("Array length must be bigger than 0");
+        }
+    }
+
+    public DynamicArray() {
+        this.array = new int[DEFAULT_CAPACITY];
+    }
+
+    public int getSize() {
+        return size;
+    }
+    public void add(int value) {
+        if (size == array.length) {
+            extend();
+        }
+        array[size++] = value;
+    }
+
+    public void addAll(int[] newArray) {
+        for (int i : newArray) {
+            add(i);
+        }
+    }
+
+    public void addAll(DynamicArray dynamicArray) {
+        int[] array = dynamicArray.array;
+        int size = dynamicArray.getSize();
+        for (int i = 0; i < size; i++) {
+            add(array[i]);
+        }
+    }
+
+    public boolean remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("The index must be in to range of 0-" + size);
+        }
+        int[] newArray = new int[size - 1];
+        System.arraycopy(array, 0, newArray, 0, index);
+        System.arraycopy(array, index + 1, newArray, index, size - (index + 1));
+        array = newArray;
+        size = size - 1;
+        return true;
+    }
+
+    public void removeRange(int fromIndex, int toIndex) {
+        if (fromIndex > toIndex) {
+            throw new IllegalArgumentException("fromIndex must be less  than toIndex");
+        }
+        if (fromIndex < 0) {
+            throw new IllegalArgumentException("fromIndex must be bigger than 0");
+        }
+        if (toIndex > size) {
+            throw new IllegalArgumentException("toIndex must be less  than " + size);
+        }
+        int[] newArray = new int[size - (toIndex - fromIndex)];
+
+
+        System.arraycopy(array, 0, newArray, 0, fromIndex);
+        System.arraycopy(array, toIndex, newArray, fromIndex, size - toIndex);
+
+
+        array = newArray;
+        size = size - (toIndex - fromIndex);
+    }
+
+    public void removeInt(int i){
+        for (int j = 0; j < size; j++) {
+            if (array[j] == i){
+                remove(j);
+            }
+        }
+    }
+
+    public void removeAll(DynamicArray dynamicArray){
+        for (int i:array) {
+            for (int j: dynamicArray.array) {
+                if (j == i){
+                    this.removeInt(j);
+                }
+            }
+        }
+    }
+
+    public DynamicArray subList(int fromIndex, int toIndex) {
+
+        if (size == 0){
+            throw new IllegalArgumentException("DynamicArray is Empty");
+        }
+        if (fromIndex > toIndex) {
+            throw new IllegalArgumentException("fromIndex must be less  than toIndex");
+        }
+        if (fromIndex < 0) {
+            throw new IllegalArgumentException("fromIndex must be bigger than 0");
+        }
+        if (toIndex > size) {
+            throw new IllegalArgumentException("toIndex must be less  than " + size);
+        }
+        DynamicArray newDynamicArray = new DynamicArray();
+        for (int i = fromIndex; i < toIndex; i++) {
+            newDynamicArray.add(array[i]);
+        }
+        return newDynamicArray;
+    }
+
+    public int get(int index) {
+        if (index < 0 || index > size - 1) {
+            throw new ArrayIndexOutOfBoundsException("Wrong index");
+        }
+        return array[index];
+    }
+
+    public void clear() {
+        size = 0;
+    }
+
+    public DynamicArray clone() {
+        DynamicArray newDynamicArray = new DynamicArray();
+        for (int i = 0; i < size; i++) {
+            newDynamicArray.add(array[i]);
+        }
+        return newDynamicArray;
+    }
+
+    private void extend() {
+        int[] newArray = new int[array.length * 2];
+        for (int i = 0; i < array.length; i++) {
+            newArray[i] = array[i];
+        }
+        array = newArray;
+    }
+
+    public int indexOf(int obj) {
+        for (int i = 0; i < size; i++) {
+            if (array[i] == obj) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean contains(int obj) {
+        if (indexOf(obj) == -1) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean containsAll(int[] objs) {
+        int index = 0;
+        for (int i = 0; i < size; i++) {
+            for (int obj : objs) {
+                if (array[i] == obj) {
+                    if (++index == objs.length) {
+                        return true;
+                    }
+                    break;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int set(int index, int value) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("The index must be in to range 0-" + (size-1));
+        }
+
+        int temp;
+        temp = array[index];
+        array[index] = value;
+        return temp;
+    }
+
+    public void sort() {
+        Sort.insertion(array);
+
+    }
+
+    public void replaceAll(DynamicArray dynamicArray){
+       array = dynamicArray.array;
+       size = dynamicArray.getSize();
+
+    }
+
+    public void trimToSize() {
+        int[] newArray = new int[size];
+        if (size < array.length) {
+            System.arraycopy(array, 0, newArray, 0, size);
+            array = newArray;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        if (size == 0) {
+            stringBuilder.append("[]");
+            return stringBuilder.toString();
+        }
+        if (size == 1) {
+            stringBuilder.append("[").append(array[0]).append("]");
+            return stringBuilder.toString();
+        }
+
+        stringBuilder.append("[");
+        for (int i = 0; i < size - 1; i++) {
+            stringBuilder.append(array[i]).append(", ");
+        }
+        stringBuilder.append(array[size - 1]).append("]");
+        return stringBuilder.toString();
+    }
+}
---
+++ src/homework_16/garik_gharibyan/Main.java
@@ -0,0 +1,43 @@
+package homework_16.garik_gharibyan;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        DynamicArray dynamicArray = new DynamicArray();
+
+        dynamicArray.add(1);
+        dynamicArray.add(2);
+        dynamicArray.add(3);
+        dynamicArray.add(8);
+        dynamicArray.add(9);
+        dynamicArray.add(4);
+        dynamicArray.add(5);
+        dynamicArray.add(6);
+        dynamicArray.add(7);
+        dynamicArray.add(0);
+
+        System.out.println("First Array: " + dynamicArray);
+        System.out.println("Array Size: " + dynamicArray.getSize());
+
+        DynamicArray clone = dynamicArray.clone();
+        System.out.println("Clone: " + clone);
+        System.out.println("Clone size: " + clone.getSize());
+
+        System.out.println("Contains int 1? :" + dynamicArray.contains(1));
+        System.out.println("indexOf 15? : " + dynamicArray.indexOf(15));
+        System.out.println("isEmpty: " + dynamicArray.isEmpty());
+
+        System.out.println("SubList(2,9): " + dynamicArray.subList(2, 9));
+
+        System.out.println(dynamicArray + " " + dynamicArray.getSize());
+
+        DynamicArray dynamicArray1 = new DynamicArray();
+        dynamicArray1.add(11);
+        dynamicArray1.add(12);
+        dynamicArray1.add(13);
+        dynamicArray.addAll(dynamicArray1);
+        System.out.println(dynamicArray + " " + dynamicArray.getSize());
+    }
+}
