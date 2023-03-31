23b0c07179ef9a2066fe4e03ec3bbbfdf182386b
MelikTutkhalyan <meliktutkhalyan@gmail.com>
2023-03-31 11:22:40 +0400

    homework_13

---
+++ src/homework_15/melo_tutkhalyan/BubbleSort.java
@@ -0,0 +1,17 @@
+package homework_15.melo_tutkhalyan;
+
+public class BubbleSort {
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+    }
+}
