e446aa1a41cbe4005226fb142870b3b84252a81b
MelikTutkhalyan <meliktutkhalyan@gmail.com>
2023-03-31 11:22:40 +0400

    homework_13

---
+++ src/homework_15/melo_tutkhalyan/InsertionSort.java
@@ -0,0 +1,15 @@
+package homework_15.melo_tutkhalyan;
+
+public class InsertionSort {
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+}
