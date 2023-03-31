b01e20983a8777a5724f6a06b41aa56de2ab9f60
mkrtichstepanyan <32538476+mkrtichstepanyan@users.noreply.github.com>
2023-03-31 22:00:57 +0400

    added homework 16 -> Dynamic Array

---
+++ src/homework_16/mkrtich_stepanyan/DynamicArray.java
@@ -0,0 +1,69 @@
+package homework_16.mkrtich_stepanyan;
+
+
+/* Please create methods below
+   addAll() -> adds all elements of a collection to arraylist
+   clear() -> removes all the elements from arraylist
+   clone() -> makes a copy of the array list
+   contains() -> checks if the element is present in the arraylist
+   containsAll() -> checks if a collection is a subset of arraylist
+   indexOf() -> returns the position of the specified element
+   remove() -> removes the single element from the arraylist
+   removeAll() -> removes multiple elements from the arraylist
+   isEmpty() -> checks if the arraylist is empty
+   subList() -> returns a portion of the arraylist
+   set() -> replace the single element from an arraylist
+   sort() -> sorts the arraylist according to specified order -> use one of our previous algorithms (bubble, insertion ...)
+   toString() -> converts the arraylist into a String
+   trimToSize() -> trims the capacity of arraylist equal to the size
+   removeRange() -> removes a portion of the arraylist
+   replaceAll() -> replace all elements from the arraylist
+ */
+public class DynamicArray {
+
+    private int[] array;
+
+    private int size = 0;
+
+    private static final int DEFAULT_CAPACITY = 16;
+
+
+    public DynamicArray(int capacity) {
+        if (capacity > 0) {
+            this.array = new int[capacity];
+        } else {
+            throw new IllegalArgumentException("Array length must be bigger than 0");
+        }
+    }
+
+    public DynamicArray() {
+        this.array = new int[DEFAULT_CAPACITY];
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void add(int value) {
+        if (size == array.length) {
+            extend();
+        }
+        array[size++] = value;
+    }
+
+    public int get(int index) {
+        if (index < 0 || index > size - 1) {
+            throw new ArrayIndexOutOfBoundsException("Wrong index");
+        }
+        return array[index];
+    }
+
+    private void extend() {
+        int[] newArray = new int[array.length * 2];
+        for (int i = 0; i < array.length; i++) {
+            newArray[i] = array[i];
+        }
+        array = newArray;
+    }
+
+}
---
+++ src/homework_16/mkrtich_stepanyan/Test.java
@@ -0,0 +1,13 @@
+package homework_16.mkrtich_stepanyan;
+
+public class Test {
+
+    public static void main(String[] args) {
+        DynamicArray myDynamicArray = new DynamicArray(5);
+
+        for (int i = 0; i < 14; i++) {
+            myDynamicArray.add(i);
+        }
+        System.out.println();
+    }
+}
