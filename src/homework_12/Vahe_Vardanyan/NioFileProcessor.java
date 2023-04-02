91a81a4b4d4828337de3381caf11e911271d3118
Vahe <vahe.vardanyan.0808@gmail.com>
2023-04-02 16:28:34 +0400

    homework_12.14

---
+++ src/homework_12/Vahe_Vardanyan/NioFileProcessor.java
@@ -0,0 +1,48 @@
+package homework_12.Vahe_Vardanyan;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class NioFileProcessor {
+    public static String readFile(String fileName) {
+
+        if (fileName.endsWith(".txt") || fileName.endsWith(".csv") || fileName.endsWith(".json")){
+            try {
+                Path path = Paths.get(fileName);
+                byte[] bytes = Files.readAllBytes(path);
+                return new  String(bytes, StandardCharsets.UTF_8);
+            }catch (IOException e){
+                System.err.println("Error: An error occurred while reading file");
+                System.exit(0);
+            }
+        }else return "Unsupported file format";
+        return fileName;
+
+    }
+
+
+    public static void writeFile(String filename, String data) {
+        if (filename.endsWith(".txt")||filename.endsWith(".csv") || filename.endsWith(".json")){
+        try {
+            Path path = Paths.get(filename);
+            Files.writeString(path,data);
+            System.out.println("The data are successfully written!");
+        }catch (IOException e){
+            System.out.println("Error: An error occurred while writing into file");
+            System.exit(0);
+        }
+    }else {
+            System.err.println("The data format is wrong!");
+        }
+    }
+
+    public static String processData(String data) {
+        return data.toUpperCase();
+    }
+
+
+}
