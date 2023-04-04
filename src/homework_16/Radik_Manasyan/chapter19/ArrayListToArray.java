e7cca2c2d5a6f77bbd54cdc5aaa6d1a450abf182
Manasyanrs <radikmanasyan@gmail.com>
2023-04-05 01:01:57 +0400

    have copied all book examples to chapter 19 and created class dynamic array

---
+++ src/homework_16/Radik_Manasyan/chapter19/ArrayListToArray.java
@@ -0,0 +1,23 @@
+package homework_16.Radik_Manasyan.chapter19;
+
+import java.util.ArrayList;
+
+public class ArrayListToArray {
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<>();
+        al.add(1);
+        al.add(2);
+        al.add(3);
+        al.add(4);
+
+        System.out.println("Content of al: " + al);
+        Integer[] ia = new Integer[al.size()];
+        ia = al.toArray(ia);
+        int sum = 0;
+        for (int i : ia) {
+            sum += i;
+        }
+
+        System.out.println("Sum is: " + sum);
+    }
+}
