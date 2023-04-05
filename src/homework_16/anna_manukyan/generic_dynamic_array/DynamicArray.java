e4ac40161ecc90e14e3647ae54e6cd7091ec85a9
anna.melikyan94 <anna.melikyan94@gmail.com>
2023-04-06 00:04:17 +0400

    homework 16 (Added generic methods implementations in GenericDynamicArray class)

--- src/homework_16/anna_manukyan/generic_dynamic_array/DynamicArray.java
+++ src/homework_16/anna_manukyan/generic_dynamic_array/DynamicArray.java
@@ -1,7 +1,5 @@
-package homework_16.anna_manukyan;
+package homework_16.anna_manukyan.generic_dynamic_array;
 
-
-import homework_15.anna_manukyan.MergeSort;
 
 import java.util.Arrays;
 
@@ -23,9 +21,9 @@ import java.util.Arrays;
    removeRange() -> removes a portion of the arraylist +
    replaceAll() -> replace all elements from the arraylist +
  */
-public class DynamicArray {
+public class DynamicArray<T> {
 
-    private int[] array;
+    private T[] array;
 
     private int size = 0;
 
@@ -34,34 +32,70 @@ public class DynamicArray {
 
     public DynamicArray(int capacity) {
         if (capacity > 0) {
-            this.array = new int[capacity];
+            this.array = (T[]) new Object[capacity];
         } else {
             throw new IllegalArgumentException("Array length must be bigger than 0");
         }
     }
 
     public DynamicArray() {
-        this.array = new int[DEFAULT_CAPACITY];
+        this.array = (T[]) new Object[DEFAULT_CAPACITY];
+    }
+
+    public DynamicArray(T item) {
+        for (T t : array) {
+            t = item;
+        }
     }
 
     public int getSize() {
         return size;
     }
 
-    public void add(int value) {
+    public void add(T value) {
         if (size == array.length) {
             extend();
         }
         array[size++] = value;
     }
 
-    public void addAll(int[] list) {
+    public void addAll(T[] list) {
         for (int i = 0; i < list.length; i++) {
             add(list[i]);
         }
     }
 
-    public void set(int index, int value) {
+    public void addByIndex(int index, T value) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("Wrong index.");
+        }
+        if (size == array.length) {
+            extend();
+        } else {
+            for (int i = size; i >= index; i--) {
+                array[i + 1] = array[i];
+            }
+
+        }
+        array[index] = value;
+        size++;
+    }
+
+    public void addAllByIndex(int index, T[] values) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("Wrong index.");
+        }
+        for (int i = size; i >= index; i--) {
+            extend();
+            array[i + values.length] = array[i];
+        }
+        for (int i = 0; i < values.length; i++) {
+            array[index++] = values[i];
+            size++;
+        }
+    }
+
+    public void set(int index, T value) {
         if (index < 0 || index > size - 1) {
             throw new ArrayIndexOutOfBoundsException("Wrong index");
         }
@@ -70,24 +104,40 @@ public class DynamicArray {
 
     public void clear() {
         if (!isEmpty()) {
-            for (int i = 0; i < size; i++) {
-                array[i] = 0;
+            for (T t : array) {
+                t = null;
             }
+            size = 0;
         }
     }
 
-    public int[] copy() {
-        int[] listCopy = new int[size];
+    public T[] copy() {
+        T[] listCopy = (T[]) new Object[size];
         for (int i = 0; i < size; i++) {
             listCopy[i] = array[i];
         }
         return listCopy;
     }
 
-    public boolean contains(int element) {
+    public T[] clone() {
+        T[] clone = (T[]) new Object[size];
+        for (int i = 0; i < size; i++) {
+            if (array[i] instanceof Number || array[i] instanceof String) {
+                clone[i] = array[i];
+            } else {
+                T item = (T) new DynamicArray(array[i]);
+                Class<?> aClass = item.getClass();
+                clone[i] = item;
+            }
+
+        }
+        return clone;
+    }
+
+    public boolean contains(T element) {
         if (!isEmpty()) {
-            for (int item : array) {
-                if (item == element) {
+            for (T item : array) {
+                if (item != null && item.equals(element)) {
                     return true;
                 }
             }
@@ -95,13 +145,12 @@ public class DynamicArray {
         return false;
     }
 
-    public boolean containsAll(int[] list) {
+    public boolean containsAll(T[] list) {
         boolean isContains = false;
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < list.length; j++) {
                 if (contains(list[j])) {
                     isContains = true;
-
                 } else {
                     isContains = false;
                     return isContains;
@@ -111,65 +160,80 @@ public class DynamicArray {
         return isContains;
     }
 
-    public boolean remove(int element) {
+    public boolean remove(T element) {
         if (!isEmpty()) {
-            for (int i = 0; i < size; i++) {
-                if (array[i] == element) {
-                    array[i] = 0;
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+            int elementCounter = elementCounter(element);
+            while (elementCounter > 0) {
+                int index = indexOf(element);
+                for (int i = index; i < size; i++) {
+                    array[i] = array[i + 1];
 
-    public boolean removeAll(int[] list) {
-        if (!isEmpty()) {
-            for (int i = 0; i < size; i++) {
-                for (int j = 0; j < list.length; j++) {
-                    if (array[i] == list[j]) {
-                        array[i] = 0;
-                    }
                 }
+                elementCounter--;
+                size--;
             }
             return true;
         }
         return false;
     }
 
-    public int[] subList(int fromIndex, int toIndex) {
+    private int elementCounter(T element) {
+        int elementCount = 0;
+        for (int i = 0; i < size; i++) {
+            if (array[i].equals(element)) {
+                elementCount++;
+            }
+        }
+        return elementCount;
+    }
+
+    public boolean removeAll(T[] list) {
+        if (!isEmpty()) {
+            for (int j = 0; j < list.length; j++) {
+                remove(list[j]);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public T[] subList(int fromIndex, int toIndex) {
         if ((fromIndex < 0 || fromIndex > size - 1) || (toIndex < 0 || toIndex > size - 1) || (fromIndex > toIndex)) {
-            throw new ArrayIndexOutOfBoundsException("Wrong index");
+            throw new ArrayIndexOutOfBoundsException("Wrong indexes");
         } else {
-            int[] subList = Arrays.copyOfRange(array, fromIndex, toIndex + 1);
+            T[] subList = Arrays.copyOfRange(array, fromIndex, toIndex + 1);
             return subList;
         }
     }
 
     public void removeRange(int fromIndex, int toIndex) {
         if ((fromIndex < 0 || fromIndex > size - 1) || (toIndex < 0 || toIndex > size - 1) || (fromIndex > toIndex)) {
-            throw new ArrayIndexOutOfBoundsException("Wrong index");
+            throw new ArrayIndexOutOfBoundsException("Wrong indexes");
         } else {
-            for (int i = fromIndex; i < toIndex; i++) {
-                array[i] = 0;
+            while (toIndex - fromIndex + 1 > 0) {
+                for (int i = fromIndex; i < size; i++) {
+                    array[i] = array[i + 1];
+                }
+                size--;
+                toIndex--;
             }
         }
     }
 
-    public void replaceAll(int[] list) {
+    public void replaceAll(T[] list, int index) {
         if (list.length <= size) {
             for (int i = 0; i < list.length; i++) {
-                array[i] = list[i];
+                array[index++] = list[i];
             }
         } else {
             throw new IndexOutOfBoundsException("List is bigger than array");
         }
     }
 
-    public int indexOf(int element) {
+    public int indexOf(T element) {
         if (!isEmpty() && contains(element)) {
             for (int i = 0; i < size; i++) {
-                if (array[i] == element) {
+                if (array[i].equals(element)) {
                     return i;
                 }
             }
@@ -185,7 +249,7 @@ public class DynamicArray {
         return false;
     }
 
-    public int get(int index) {
+    public T get(int index) {
         if (isEmpty()) {
             System.out.println("Array is empty.");
             System.exit(1);
@@ -197,11 +261,13 @@ public class DynamicArray {
     }
 
     public void sort() {
-        MergeSort.sortArray(array);
+        if (array instanceof Number[]) {
+//            MergeSort.sortArray(array);
+        }
     }
 
     private void extend() {
-        int[] newArray = new int[array.length * 2];
+        T[] newArray = (T[]) new Object[array.length * 2];
         for (int i = 0; i < array.length; i++) {
             newArray[i] = array[i];
         }
@@ -209,13 +275,24 @@ public class DynamicArray {
     }
 
     public String toString() {
-        String prefix = "[ ";
-        String suffix = " ]";
-        String toString = "";
-        for (int item : this.array) {
-            toString += item + ", ";
+        String prefix = "[";
+        String suffix = "]";
+        StringBuilder toString = new StringBuilder();
+        if (!isEmpty()) {
+            for (T item : this.array) {
+                if (item != null) {
+                    toString.append(item).append(", ");
+                }
+            }
         }
         return prefix + toString + suffix;
+    }
+
+    public void trimToSize() {
+        if (array.length > size) {
+            T[] newArr = Arrays.copyOf(array, size);
+            array = newArr;
+        }
     }
 
 }
