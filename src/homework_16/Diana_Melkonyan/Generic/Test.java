d25ef75a70148eba05c5c9fe6ca6e2e5a245b1f4
Diana Melkonyan <dianamelkonyanit@gmail.com>
2023-04-06 02:15:34 +0400

    Homework 16 Generic

---
+++ src/homework_16/Diana_Melkonyan/Generic/Test.java
@@ -0,0 +1,76 @@
+package homework_16.Diana_Melkonyan.Generic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Test {
+
+    public static void main(String[] args) {
+        DynamicArray<Integer> myArrayList = new DynamicArray<>();
+
+        myArrayList.add(3);
+        myArrayList.add(6);
+        myArrayList.add(71);
+        myArrayList.add(3);
+        myArrayList.add(5);
+        myArrayList.add(9);
+        myArrayList.add(65);
+        myArrayList.add(7);
+        myArrayList.add(56);
+
+        System.out.println(myArrayList);
+
+
+
+//        System.out.println(myArrayList.get(2));
+//        System.out.println(myArrayList);
+
+//        System.out.println(myArrayList.contains(3));
+//        System.out.println(myArrayList);
+
+
+//        myArrayList.removeRange(1,3);
+//        System.out.println(myArrayList);
+
+//        myArrayList.trimToSize();
+//        System.out.println(myArrayList);
+
+//        myArrayList.set(0,1);
+//        System.out.println(myArrayList);
+
+//        myArrayList.addByIndex(0,2);
+//        System.out.println(myArrayList);
+
+//        myArrayList.remove(3);
+//        System.out.println(myArrayList);
+
+//        System.out.println(Arrays.toString(myArrayList.subList(1,3)))
+
+//        myArrayList.clear();
+//        System.out.println(myArrayList);
+
+
+//       myArrayList.replaceAll(3,11);
+//        System.out.println(myArrayList);
+
+//        DynamicArray <String> stringDynamicArray = new DynamicArray<>();
+//        stringDynamicArray.addAll(Arrays.asList("Hello", "Java"));
+//        System.out.println(stringDynamicArray);
+
+//        myArrayList.sort();
+//        System.out.println(myArrayList);
+
+
+//        DynamicArray <String> stringDynamicArray = new DynamicArray<>();
+//        stringDynamicArray.addAll(Arrays.asList("Lesson", "2023"));
+//        stringDynamicArray.addAllByIndex(0,new String[]{"Hello", "Java"});
+//        System.out.println(stringDynamicArray);
+
+//        System.out.println(myArrayList.izEmpty());
+
+//        myArrayList.removeAll(3);
+//        System.out.println(myArrayList);
+    }
+}
