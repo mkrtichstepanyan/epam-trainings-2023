5a6961006c56eddc2275f7ea0bdef272e32815bc
Diana Melkonyan <dianamelkonyanit@gmail.com>
2023-04-12 23:27:06 +0400

    homework 17

---
+++ src/homework_17/Diana_Melkonyan/Main.java
@@ -0,0 +1,71 @@
+package homework_17.Diana_Melkonyan;
+
+public class Main {
+    public static void main(String[] args) {
+        GenericLinkedList<Integer> list = new GenericLinkedList<Integer>();
+        list.add(22);
+        list.add(23);
+        list.add(24);
+        list.add(25);
+        list.add(26);
+
+        list.print();
+
+//        System.out.println("After iterator method");
+//        for (Iterator i = list.iterator(); i.hasNext();) {
+//            System.out.print(i.next() + " ");
+//        }
+//        System.out.println(list.get(2));
+//
+//
+//        System.out.println("After clear method");
+//        list.clear();
+//        list.print();
+//
+//        System.out.println("After remove mathod");
+//        list.remove(1);
+//        list.print();
+//
+//        System.out.println("After add mathod two parameters");
+//        list.add(1, 55);
+//        list.print();
+//
+//
+//        System.out.println("After size methods");
+//        System.out.println(list.size());
+//
+//
+//        System.out.println("After indexOf methods");
+//        System.out.println(list.indexOf(22));
+
+//        System.out.println("After lastindexOf methods");
+//        System.out.println(list.lastIndexOf(26));
+
+//        System.out.println(list.contains(24));
+//
+//
+//        System.out.println("After boolean remove(T o) method");
+//        list.remove(2);
+//        list.print();
+//
+//        System.out.println("After sublist methods");
+//        List subList = list.subList(1, 3);
+//        System.out.println(subList);
+//
+//
+//        System.out.println("After set methods");
+//        list.set(1, 999);
+//        list.print();
+//
+
+
+    }
+}
+
+
+
+
+            
+
+
+
