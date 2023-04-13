191863e2a036975645c2af12aa069b6696366815
vladvanyan95 <vlovvayan95@gmail.com>
2023-04-14 03:46:32 +0400

    homework_16

---
+++ src/homework_16/Vladimir_Vanyan/GenericDynamicArray/GenericArrayTest.java
@@ -0,0 +1,71 @@
+package homework_16.Vladimir_Vanyan.GenericDynamicArray;
+
+import java.util.Arrays;
+
+public class GenericArrayTest {
+    public static void main(String[] args) {
+        GenericDynamicArray myDynamicArray = new GenericDynamicArray(5);
+        String[] myArray = {"A", "B", "C"};
+        String[] myArray3 = {"aed", "sss", "asff", "scr", "aaaa"};
+
+        int[] myArray2 = {5, 9, 11, 17, 20};
+
+//        addAll method
+        myDynamicArray.addAll(myArray);
+
+//        addByIndex
+        myDynamicArray.addByIndex(2, "kk");
+
+//        addAllByIndex
+        myDynamicArray.addAllByIndex(1, myArray3);
+
+//        clone() -> makes a copy of the array list
+        System.out.println(Arrays.toString(myDynamicArray.clone(myArray)));
+
+//        clear() -> removes all the elements from arraylist
+        myDynamicArray.clear();
+
+//        contains() -> checks if the element is present in the arraylist
+        myDynamicArray.contains(3);
+        myDynamicArray.contains(20);
+
+//        containsAll() -> checks if a collection is a subset of arraylist
+        myDynamicArray.containsAll(myArray);
+//        myDynamicArray.containsAll(myArray2);
+
+//        indexOf() -> returns the position of the specified element
+        myDynamicArray.indexOf("A");
+        myDynamicArray.indexOf(91);
+
+//        remove() -> removes the single element from the arraylist
+        myDynamicArray.remove("A");
+
+//        removeAll() -> removes multiple elements from the arraylist
+        myDynamicArray.removeAll(myArray);
+
+//        isEmpty() -> checks if the arraylist is empty
+        myDynamicArray.isEmpty();
+
+//        subList() -> returns a portion of the arraylist
+        myDynamicArray.subList(5, 9);
+
+//        set() -> replace the single element from an arraylist
+        myDynamicArray.set(3, 30);
+
+//        sort() -> sorts the arraylist according to specified order -> use one of our previous algorithms (bubble, insertion ...)
+        myDynamicArray.sort(myArray2);
+
+//        toString() -> converts the arraylist into a String
+        System.out.println(myDynamicArray.toString(myArray));
+
+
+//        trimToSize() -> trims the capacity of arraylist equal to the size
+        myDynamicArray.trimToSize();
+
+//        removeRange() -> removes a portion of the arraylist
+        System.out.println(Arrays.toString(myDynamicArray.removeRange(1, 2)));
+
+//        replaceAll() -> replace all elements from the arraylist
+        myDynamicArray.replaceAll(2, myArray);
+    }
+}
