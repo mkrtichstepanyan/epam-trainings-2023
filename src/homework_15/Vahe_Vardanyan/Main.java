061191ded8844f4c333b6e52d5f2d77ff60f7a29
AshkhenHayrapetyan <63423560+AshkhenHay@users.noreply.github.com>
2023-04-14 13:53:10 +0400

    Update Main.java

--- src/homework_15/Vahe_Vardanyan/Main.java
+++ src/homework_15/Vahe_Vardanyan/Main.java
@@ -1,6 +1,5 @@
 package homework_15.Vahe_Vardanyan;
 
-import static  homework_15.Vahe_Vardanyan.BubbleSort.printArray;
 public class Main {
     public static void main(String[] args) {
         int[] array = {9,7,2,1,3,0,5,4,8,6};
@@ -8,5 +7,11 @@ public class Main {
         printArray(BubbleSort.BubbleSort(array));
         printArray(SelectionSort.SelectionSort(array));
         printArray(InsertionSort.InsertionSort(array));
+    }
+    
+       public static void printArray(int[] array) {
+        for (int i : array) {
+            System.out.println(i + " ");
+        }
     }
 }
