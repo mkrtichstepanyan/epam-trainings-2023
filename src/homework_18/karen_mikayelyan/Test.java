7abfa1852f0f816953cf7b662a3f7c1af8367b89
Karen-Mikayelyan <mikkar89@mail.ru>
2023-04-20 00:21:29 +0400

    Added User and Address classes, equals and hashcode methods

--- src/homework_18/karen_mikayelyan/Test.java
+++ src/homework_18/karen_mikayelyan/Test.java
@@ -1,6 +1,9 @@
 package homework_18.karen_mikayelyan;
 
 
+import homework_18.karen_mikayelyan.entity.Address;
+import homework_18.karen_mikayelyan.entity.User;
+
 public class Test {
     public static void main(String[] args) {
         GenericMap<String, String> map = new GenericMap<>();
@@ -17,5 +20,14 @@ public class Test {
         for (int i = 0; i < 25; i++) {
             System.out.print(genericMap.put(i, " $"));
         }
+        System.out.println();
+
+
+        GenericMap<User, Integer> userMap = new GenericMap<>();
+        Address address = new Address("Gyumri", "Sayat Nova", 26);
+        User user = new User("Karen", "Mikayelyan", "mikkar89@mail.ru", "abcdef777", 33, address);
+        userMap.put(user, 25);
+        System.out.println(userMap.get(user));
+
     }
 }
