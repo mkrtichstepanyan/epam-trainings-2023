5bbccbd4b729c16fec0fc3660f4e26d8ec0defa5
Karen-Mikayelyan <mikkar89@mail.ru>
2023-04-20 00:31:42 +0400

    added modified Test class

--- src/homework_18/karen_mikayelyan/Test.java
+++ src/homework_18/karen_mikayelyan/Test.java
@@ -6,21 +6,21 @@ import homework_18.karen_mikayelyan.entity.User;
 
 public class Test {
     public static void main(String[] args) {
-        GenericMap<String, String> map = new GenericMap<>();
-        map.put("1", "11");
-        map.put("2", "22");
-        map.put("3", "33");
-        for (String k : map.keys()) {
-            System.out.println(k + " " + map.get(k));
-        }
-        System.out.println(map.containsKey("2"));
-
-
-        GenericMap<Integer, String> genericMap = new GenericMap<>();
-        for (int i = 0; i < 25; i++) {
-            System.out.print(genericMap.put(i, " $"));
-        }
-        System.out.println();
+//        GenericMap<String, String> map = new GenericMap<>();
+//        map.put("1", "11");
+//        map.put("2", "22");
+//        map.put("3", "33");
+//        for (String k : map.keys()) {
+//            System.out.println(k + " " + map.get(k));
+//        }
+//        System.out.println(map.containsKey("2"));
+//
+//
+//        GenericMap<Integer, String> genericMap = new GenericMap<>();
+//        for (int i = 0; i < 25; i++) {
+//            System.out.print(genericMap.put(i, " $"));
+//        }
+//        System.out.println();
 
 
         GenericMap<User, Integer> userMap = new GenericMap<>();
