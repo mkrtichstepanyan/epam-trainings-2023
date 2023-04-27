b5d383bf4a225e0859524ba8d3b2c36c5d258b02
Argisht25 <argoo17@mail.ru>
2023-04-28 00:28:14 +0400

    fixed errer

--- src/homework_12/Argishti_Mesropyan/Assignment_1/FileDemo.java
+++ src/homework_12/Argishti_Mesropyan/Assignment_1/FileDemo.java
@@ -2,12 +2,11 @@ package homework_12.Argishti_Mesropyan.Assignment_1;
 
 public class FileDemo {
     public static void main(String[] args) {
-        String readFile = "src/homework_12/Argishti_Mesropyan/Assignment_1/nioText.txt";
+        String readFile = "src/homework_12/Argishti_Mesropyan/Assignment_1/nioFile.txt";
         String writePath = "src/homework_12/Argishti_Mesropyan/Assignment_1/writtenText.txt";
 
-        ReadAndWriteFile.readFile(readFile);
-        ReadAndWriteFile.writeFile(writePath, ReadAndWriteFile.readFile(readFile));
-        ReadAndWriteFile.processData(ReadAndWriteFile.readFile(readFile));
+        System.out.println(ReadAndWriteFile.readFile(readFile));
+        ReadAndWriteFile.writeFile(writePath, readFile);
 
     }
 }
--- src/homework_12/Argishti_Mesropyan/Assignment_1/ReadAndWriteFile.java
+++ src/homework_12/Argishti_Mesropyan/Assignment_1/ReadAndWriteFile.java
@@ -1,51 +1,47 @@
 package homework_12.Argishti_Mesropyan.Assignment_1;
 
-import java.io.File;
-import java.io.FilenameFilter;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
-public class ReadAndWriteFile implements FilenameFilter {
-    public static String readFile(String fileName){
-        String data = " ";
-        ByteBuffer rb = ByteBuffer.allocate(1024);
-        try(FileChannel channel = FileChannel.open(Paths.get(fileName),StandardOpenOption.READ)){
-            while (channel.read(rb) != -1){
-                rb.flip();
-                data = new String(rb.array()).trim();
-                rb.clear();
+public class ReadAndWriteFile  {
+    public static String readFile(String fileName) {
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        if (fileName.endsWith(".txt") || fileName.endsWith(".csv") || fileName.endsWith(".json")) {
+            try (FileChannel channel = FileChannel.open(Paths.get(fileName), StandardOpenOption.READ)) {
+                channel.read(buffer);
+                buffer.flip();
+                String content = new String(buffer.array()).trim();
+                return "The content of file " + content;
+            } catch (IOException e) {
+                System.out.println("I/O error " + e);
+                return "";
             }
-        }catch (IOException e){
-            System.out.println(fileName + "does not exist");
-            return null;
+        } else {
+            System.out.println("Error file format");
+            return "";
         }
-        System.out.println(data);
-        return data;
     }
 
-    public static void writeFile(String fileName, String data){
-        ByteBuffer wb = ByteBuffer.wrap(data.getBytes());
-        try(FileChannel channel = FileChannel.open(Paths.get(fileName), StandardOpenOption.CREATE,
-                StandardOpenOption.WRITE)){
-            channel.write(wb);
-            wb.flip();
-            wb.clear();
-        }catch (IOException e){
-            System.out.println(fileName + "does not exist");
+    public static void writeFile(String writingFileName, String readingFileName) {
+        String message = readFile(readingFileName);
+        String precessedMessage = precessData(message);
+        ByteBuffer buffer = ByteBuffer.wrap(precessedMessage.getBytes());
+        if (writingFileName.endsWith(".txt") || writingFileName.endsWith(".csv") || writingFileName.endsWith(".json")) {
+            try (FileChannel channel = FileChannel.open(Paths.get(writingFileName), StandardOpenOption.CREATE, StandardOpenOption.WRITE)) {
+                channel.write(buffer);
+            } catch (IOException e) {
+                System.out.println("I/O error " + e);
+            }
+        } else {
+            System.out.println("Error file format");
         }
-        System.out.println(data);
     }
 
-    public static String processData(String data) {
-        writeFile("src/homework_12/Argishti_Mesropyan/assignment_1/nioFile.txt", data.toUpperCase());
-        return data;
-    }
-
-    @Override
-    public boolean accept(File dir, String name) {
-        return name.endsWith(".txt") || name.endsWith(".json") || name.endsWith(".csv");
+    public static String precessData(String data) {
+        String dataUpperCase = data.toUpperCase();
+        return dataUpperCase;
     }
 }
