f80c1098dcc0fe2b6a9d09cd70ec920b82b79661
Argisht <100313895+Argisht25@users.noreply.github.com>
2023-04-27 11:25:57 +0300

    homework_10 chapter_10 (#983)

---
+++ src/homework_10/Argishti_Mesropyan/Exc1.java
@@ -0,0 +1,12 @@
+package homework_10.Argishti_Mesropyan;
+
+public class Exc1 {
+    static void subroutine() {
+        int d = 0;
+        int a = 1 / d;
+    }
+
+    public static void main(String[] args) {
+        Exc1.subroutine();
+    }
+}
