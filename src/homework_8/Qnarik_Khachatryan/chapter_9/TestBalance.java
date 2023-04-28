8375d229bba02e869f926760692013c7386a7b0f
Varduhi Karapetyan <varduhi_karapetyan@epam.com>
2023-04-28 17:27:42 +0400

    Fix.

---
+++ src/homework_8/Qnarik_Khachatryan/chapter_9/TestBalance.java
@@ -0,0 +1,13 @@
+package homework_8.Qnarik_Khachatryan.chapter_9;
+
+import homework_8.Qnarik_Khachatryan.chapter_9.mypack.*;
+
+public class TestBalance {
+    public static void main(String[] args) {
+
+        /*Because Balance is public, we may use Balance class and call its constructor. */
+        Balance test = new Balance("J. J. Jaspers", 99.88);
+
+        test.show(); //we can also call show()
+    }
+}
