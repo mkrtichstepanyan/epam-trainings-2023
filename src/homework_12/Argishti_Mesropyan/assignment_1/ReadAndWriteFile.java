9184a524df00dad6d4e17a59c52b60b7ecc5467e
Argisht25 <argoo17@mail.ru>
2023-04-29 00:10:08 +0400

    homework12

---
+++ src/homework_12/Argishti_Mesropyan/assignment_1/ReadAndWriteFile.java
@@ -0,0 +1,48 @@
+package homework_12.Argishti_Mesropyan.assignment_1;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class ReadAndWriteFile {
+    public static String readFile(String fileName) {
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        if (fileName.endsWith(".txt") || fileName.endsWith(".csv") || fileName.endsWith(".json")) {
+            try (FileChannel channel = FileChannel.open(Paths.get(fileName), StandardOpenOption.READ)) {
+                channel.read(buffer);
+                buffer.flip();
+                String content = new String(buffer.array()).trim();
+                return "The content of file " + content;
+            } catch (IOException e) {
+                System.out.println("I/O error " + e);
+                return "";
+            }
+        } else {
+            System.out.println("Error file format");
+            return "";
+        }
+    }
+
+    public static void writeFile(String writingFileName, String readingFileName) {
+        String message = readFile(readingFileName);
+        String precessedMessage = precessData(message);
+        ByteBuffer buffer = ByteBuffer.wrap(precessedMessage.getBytes());
+        if (writingFileName.endsWith(".txt") || writingFileName.endsWith(".csv") || writingFileName.endsWith(".json")) {
+            try (FileChannel channel = FileChannel.open(Paths.get(writingFileName), StandardOpenOption.CREATE, StandardOpenOption.WRITE)) {
+                channel.write(buffer);
+            } catch (IOException e) {
+                System.out.println("I/O error " + e);
+            }
+        } else {
+            System.out.println("Error file format");
+        }
+    }
+
+    public static String precessData(String data) {
+        String dataUpperCase = data.toUpperCase();
+        return dataUpperCase;
+    }
+
+}
