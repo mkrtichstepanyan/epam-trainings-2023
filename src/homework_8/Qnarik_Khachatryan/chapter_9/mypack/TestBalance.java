78737b439d1245f4004485c07308b5959c69d4be
Argisht25 <argoo17@mail.ru>
2023-04-28 14:54:33 +0400

    Bring it back file

--- src/homework_8/Qnarik_Khachatryan/chapter_9/mypack/TestBalance.java
+++ src/homework_8/Qnarik_Khachatryan/chapter_9/mypack/TestBalance.java
@@ -1,2 +1,8 @@
-package homework_8.Qnarik_Khachatryan.chapter_9.mypack;public class TestBalace {
+package homework_8.Qnarik_Khachatryan.chapter_9.mypack;
+
+public class TestBalance {
+    public static void main(String[] args) {
+        Balance test = new Balance("J. J. Jaspers", 99.88);
+        test.show();
+    }
 }
