67d01e770e1bf39052de2f0387f6b839142627d4
Ani Kovalenko <ani.kovalenko97@gmail.com>
2023-05-10 11:40:41 +0400

    stream API homework

---
+++ src/homework_19/Ani_Kovalenko/User.java
@@ -0,0 +1,28 @@
+package homework_19.Ani_Kovalenko;
+
+public class User {
+
+    private String name;
+    private String surname;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public User(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+}
