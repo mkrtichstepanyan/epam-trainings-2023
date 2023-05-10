c6b7c74b94a36f08fe0c903030fc45e6175842ed
Diana Melkonyan <dianamelkonyanit@gmail.com>
2023-05-10 22:08:19 +0400

    homework 20

--- src/homework_20/Diana_Melkonyan/Main.java
+++ src/homework_20/Diana_Melkonyan/Main.java
@@ -1,61 +1,14 @@
 package homework_20.Diana_Melkonyan;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        BoundedBlockingBuffer queue = new BoundedBlockingBuffer();
-        Thread t = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (true) {
-                    Runnable task = queue.take();
-                    task.run();
-                }
-            }
-        });
-
-        t.start();
-        for (int i = 0; i < 10; i++) {
-            queue.put(getTask());
-        }
-    }
-
-    private static Runnable getTask() {
-        return new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Start  :" + this);
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("Finish  :" + this);
-            }
-        };
-    }
-
-}
-
-class BoundedBlockingBuffer {
-    ArrayList<Runnable> tasks = new ArrayList<>();
-
-    public synchronized Runnable take() {
-        while (tasks.isEmpty()) {
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        Runnable task = tasks.get(0);
-        tasks.remove(task);
-        return task;
-    }
-
-    public synchronized void put(Runnable task) {
-        tasks.add(task);
-        notify();
+        BoundedBlockBuffer queue=new BoundedBlockBuffer();
+        new Producer(queue);
+        new Consumer(queue);
     }
 }
+
+
---
+++ src/homework_20/Diana_Melkonyan/Producer.java
@@ -0,0 +1,22 @@
+package homework_20.Diana_Melkonyan;
+
+public class Producer implements Runnable{
+   BoundedBlockBuffer buffer;
+
+    public Producer(BoundedBlockBuffer buffer){
+        this.buffer=buffer;
+        Thread t=new Thread(this,"Producer");
+        t.start();
+    }
+
+    @Override
+    public void run() {
+        Integer i=1;
+      while (true){
+          buffer.put(i++);
+          try{Thread.sleep(1000);}catch (Exception e){}
+      }
+
+
+    }
+}
