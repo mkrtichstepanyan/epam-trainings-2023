d8cf0901ae964f04964e4652ab08be99b6f195fc
vladvanyan95 <vlovvayan95@gmail.com>
2023-05-14 01:18:02 +0400

    Homework 19 Extended List

---
+++ src/homework_19/Vladimir_Vanyan/Main.java
@@ -0,0 +1,28 @@
+package homework_19.Vladimir_Vanyan;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        ExtendedList<Integer> list = new ExtendedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        ExtendedList<String> mappedList = list.map(x -> "Value " + x);
+        System.out.println(mappedList);
+
+        list.fill(() -> (int) (Math.random() * 10), 3);
+        System.out.println(list);
+
+        boolean lessThanNumber = list.forAll(x -> x < 10);
+        System.out.println(lessThanNumber);
+
+        List<ExtendedList<Integer>> partition = list.partition(x -> x > 5);
+        System.out.println(partition);
+
+        int sum = list.reduce(Integer::sum, 0);
+        System.out.println(sum);
+    }
+}
