b5a5a46fef738612420c519e315e15d1a7817456
Knarik8 <k_armina@mail.ru>
2023-05-14 20:34:01 +0300

    Bounded blocking buffer

---
+++ src/homework_20/qnarik_khachatryan/BoundedBlockingBuffer.java
@@ -0,0 +1,41 @@
+package homework_20.qnarik_khachatryan;
+
+public class BoundedBlockingBuffer<T> {
+    T data;
+
+
+    //Put in it the value you want to transfer.
+// If the buffer already contains some value, then the thread waits until someone picks it up.
+    public synchronized void put(T data) {
+        while (this.data != null) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                System.out.println("Interrupted");
+            }
+
+        }
+        notifyAll();
+        this.data = data;
+        System.out.println("Put " + data);
+    }
+
+    //Pick up the value put there. If the buffer is empty,
+    // then the thread waits until someone puts a value into it.
+    public synchronized T take() {
+        T newData;
+        while (this.data == null) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                System.out.println("Interrupted");
+            }
+        }
+        // new Data??
+        notifyAll();
+        newData = data;
+        data = null;
+        System.out.println("Get " + newData);
+        return data;
+    }
+}
