698c092159d6c230d6f10bb2aad301a95b95d363
Eduard Eghiazaryan <egiazaryaneduard3@gmail.com>
2023-05-22 14:58:14 +0400

    homework_19_eduard_eghiazaryan

---
+++ src/homework_19/Eduard_Eghiazaryan/Main.java
@@ -0,0 +1,36 @@
+package homework_19.Eduard_Eghiazaryan;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ExtendedList<Integer> numbers = new ExtendedList<>();
+
+        //add some elements to the list
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+
+        //Test the map method
+        ExtendedList<String> stringNumbers = numbers.map(Object::toString);
+        System.out.println("After mapping: " + stringNumbers);
+
+        //Test the fill method
+        numbers.fill(() -> 2, 5);
+        System.out.println("After filling: " + numbers);
+
+        //test forAll method
+        boolean allPositive = numbers.forAll(n -> n >= 0);
+        System.out.println("All positive? " + allPositive);
+
+        //Test the partition method
+        List<ExtendedList<Integer>> partitions = numbers.partition(n -> n % 2 == 0);
+        System.out.println("Partitions list: " + partitions);
+
+        //Test the reduce method
+        int sum = numbers.reduce(Integer::sum, 0);
+        System.out.println("Sum: " + sum);
+    }
+}
