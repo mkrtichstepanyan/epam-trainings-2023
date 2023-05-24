5a4490d910e2a2ba4b5239ed891350f6674c8113
Alina <alinamkhoyan19@gmail.com>
2023-05-24 14:57:47 +0400

    Homework 21:

---
+++ src/homework_21/Alina_Mkhoyan/QuickSort.java
@@ -0,0 +1,37 @@
+package homework_21.Alina_Mkhoyan;
+
+public class QuickSort implements SortingAlgorithm {
+    @Override
+    public void sort(int[] array) {
+        quickSort(array, 0, array.length - 1);
+    }
+
+    private void quickSort(int[] array, int low, int high) {
+        if (low < high) {
+            int pi = partition(array, low, high);
+
+            quickSort(array, low, pi - 1);
+            quickSort(array, pi + 1, high);
+        }
+    }
+
+    private int partition(int[] array, int low, int high) {
+        int pivot = array[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (array[j] < pivot) {
+                i++;
+
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        int temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
+
+        return i + 1;
+    }
+}
