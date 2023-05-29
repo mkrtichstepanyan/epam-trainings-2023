6f1d98e5f19e69d981b1a9d401056efcc13b9a9a
Alina <alinamkhoyan19@gmail.com>
2023-05-29 14:03:17 +0400

    Homework 20:

--- src/homework_20/Alina_Mkhoyan/BlockingBufferTest.java
+++ src/homework_20/Alina_Mkhoyan/BlockingBufferTest.java
@@ -1,13 +1,9 @@
 package homework_20.Alina_Mkhoyan;
 
-import homework_20.araksya_ghazaryan.BoundedBlockBuffer;
-import homework_20.araksya_ghazaryan.Consumer;
-import homework_20.araksya_ghazaryan.Producer;
-
 public class BlockingBufferTest {
     public static void main(String[] args) throws InterruptedException {
 
-        BoundedBlockBuffer buffer = new BoundedBlockBuffer();
+        BoundedBlockingBuffer buffer = new BoundedBlockingBuffer();
       Producer t1 = new Producer(buffer);
       Consumer t2 = new Consumer(buffer);
 
