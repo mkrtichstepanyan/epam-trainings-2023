a0cb21090c62b222182a4112557e74fc44455440
Alina <86889072+alina-071100@users.noreply.github.com>
2023-06-07 21:05:06 +0400

    Homework 21: (#1086)

---
+++ src/homework_21/Alina_Mkhoyan/BubbleSort.java
@@ -0,0 +1,17 @@
+package homework_21.Alina_Mkhoyan;
+
+public class BubbleSort implements SortingAlgorithm {
+    @Override
+    public void sort(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
---
+++ src/homework_21/Alina_Mkhoyan/SelectionSort.java
@@ -0,0 +1,20 @@
+package homework_21.Alina_Mkhoyan;
+
+public class SelectionSort implements SortingAlgorithm {
+
+    @Override
+    public void sort(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+    }
+}
